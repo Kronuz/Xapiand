c7a23d48729849e85cf5d488207494b8b9692a4f
German M. Bravo <german.mb@deipi.com>
2015-02-22 00:31:27 -0600

    Added queue class

---
+++ queue.h
@@ -0,0 +1,140 @@
+#ifndef XAPIAND_INCLUDED_QUEUE_H
+#define XAPIAND_INCLUDED_QUEUE_H
+
+#include <queue>
+#include <cerrno>
+
+#include <sys/time.h>
+#include <pthread.h>
+
+
+template<class T>
+class Queue {
+private:
+	// The queue itself
+	std::queue<T> queue;
+	// A mutex object to control access to the std::queue
+	pthread_mutex_t qmtx;
+	// A variable condition to make threads wait on specified condition values
+	pthread_cond_t wcond;
+
+	bool finished;
+
+public:
+	Queue();
+	~Queue();
+
+	void finish();
+	void push(T& element);
+	bool pop(T& element, time_t timeout=0);
+
+	unsigned int sizeOfQueue();
+	bool empty();
+};
+
+
+template<class T>
+Queue<T>::Queue()
+{
+	pthread_cond_init(&wcond, 0);
+	pthread_mutex_init(&qmtx, 0);
+}
+
+
+template<class T>
+Queue<T>::~Queue()
+{
+	finish();
+	pthread_mutex_destroy(&qmtx);
+	pthread_cond_destroy(&wcond);
+}
+
+
+template<class T>
+void Queue<T>::finish()
+{
+	pthread_mutex_lock(&qmtx);
+
+	finished = true;
+
+	pthread_mutex_unlock(&qmtx);
+
+	// Signal the condition variable in case any threads are waiting
+	pthread_cond_broadcast(&wcond);
+}
+
+
+template<class T>
+void Queue<T>::push(T& element)
+{
+	pthread_mutex_lock(&qmtx);
+
+	if (!finished) {
+		// Insert the element in the FIFO queue
+		queue.push(element);
+	}
+
+	// Now we need to unlock the mutex otherwise waiting threads will not be able
+	// to wake and lock the mutex by time before push is locking again
+	pthread_mutex_unlock(&qmtx);
+
+	// Notifiy waiting thread they can pop/push now
+	pthread_cond_signal(&wcond);
+}
+
+
+template<class T>
+bool Queue<T>::empty()
+{
+	pthread_mutex_lock(&qmtx);
+	bool empty = queue.empty();
+	pthread_mutex_unlock(&qmtx);
+	return empty;
+}
+
+
+template<class T>
+bool Queue<T>::pop(T& element, time_t timeout)
+{
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	struct timespec ts;
+	ts.tv_sec = tv.tv_sec + timeout;
+	ts.tv_nsec = 0;
+
+	pthread_mutex_lock(&qmtx);
+
+	// While the queue is empty, make the thread that runs this wait
+	while(queue.empty()) {
+		if (!finished && timeout) {
+			if (pthread_cond_timedwait(&wcond, &qmtx, &ts) == ETIMEDOUT) {
+				pthread_mutex_unlock(&qmtx);
+				return false;
+			}
+		} else {
+			pthread_mutex_unlock(&qmtx);
+			return false;
+		}
+	}
+
+	//when the condition variable is unlocked, popped the element
+	element = queue.front();
+
+	//pop the element
+	queue.pop();
+
+	pthread_mutex_unlock(&qmtx);
+	return true;
+};
+
+
+template<class T>
+unsigned int Queue<T>::sizeOfQueue()
+{
+	pthread_mutex_lock(&qmtx);
+	unsigned int size = queue.size();
+	pthread_mutex_unlock(&qmtx);
+	return size;
+};
+
+#endif /* XAPIAND_INCLUDED_QUEUE_H */
--- threadpool.cc
+++ threadpool.cc
@@ -4,86 +4,12 @@
 // Function that retrieves a task from a queue, runs it and deletes it
 static void *getWork(void * param) {
 	Task *mw = 0;
-	WorkQueue *wq = (WorkQueue*)param;
-	while ((mw = wq->nextTask())) {
+	Queue<Task *> *wq = (Queue<Task *> *)param;
+	while (wq->pop(mw)) {
 		mw->run();
 		delete mw;
 	}
 	return 0;
-}
-
-
-WorkQueue::WorkQueue() {
-	// wcond is a condition variable that's signaled when new work arrives
-	pthread_cond_init(&wcond, 0);
-
-	// Initialize the mutex protecting the queue
-	pthread_mutex_init(&qmtx, 0);
-}
-
-WorkQueue::~WorkQueue() {
-	// Cleanup pthreads
-	pthread_mutex_destroy(&qmtx);
-
-	pthread_cond_destroy(&wcond);
-}
-
-// Retrieves the next task from the queue
-Task *WorkQueue::nextTask() {
-	// Lock the queue mutex
-	pthread_mutex_lock(&qmtx);
-
-	// The return value
-	Task *nt = NULL;
-
-	while(true) {
-		// Check if there's work
-		if (finished && tasks.size() == 0) {
-			// If not return null (0)
-			nt = 0;
-		} else {
-			// Not finished, but there are no tasks, so wait for
-			// wcond to be signalled
-			if (tasks.size() == 0) {
-				pthread_cond_wait(&wcond, &qmtx);
-				continue;
-			}
-			// get the next task
-			nt = tasks.front();
-			tasks.pop();
-		}
-		break;
-	}
-
-	// Unlock the mutex and return
-	pthread_mutex_unlock(&qmtx);
-
-	return nt;
-}
-
-// Add a task
-void WorkQueue::addTask(Task *nt) {
-	pthread_mutex_lock(&qmtx);
-
-	// Only add the task if the queue isn't marked finished
-	if (!finished) {
-		tasks.push(nt); // Add the task
-		pthread_cond_signal(&wcond); // signal there's new work
-	}
-
-	pthread_mutex_unlock(&qmtx);
-}
-
-// Mark the queue finished
-void WorkQueue::finish() {
-	pthread_mutex_lock(&qmtx);
-
-	finished = true;
-
-	// Signal the condition variable in case any threads are waiting
-	pthread_cond_broadcast(&wcond);
-
-	pthread_mutex_unlock(&qmtx);
 }
 
 
@@ -108,7 +34,7 @@ ThreadPool::~ThreadPool() {
 
 // Add a task
 void ThreadPool::addTask(Task *nt) {
-	workQueue.addTask(nt);
+	workQueue.push(nt);
 }
 
 // Tell the tasks to finish and return
