6d2d8cac51cb243f4539ee582bf4e366345e6b86
German M. Bravo <german.mb@deipi.com>
2015-02-25 15:31:02 -0600

    Loop lives working (SIGINT working)

--- src/utils.h
+++ src/utils.h
@@ -6,4 +6,6 @@
 void print_string(const std::string &string);
 void fprint_string(FILE * file, const std::string &string);
 
+void log(void *obj, const char *format, ...);
+
 #endif /* XAPIAND_INCLUDED_UTILS_H */
