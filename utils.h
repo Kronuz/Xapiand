78b58dfe734a154cc248f66c293c736b9e59e136
German M. Bravo <german.mb@deipi.com>
2015-02-26 10:35:08 -0600

    Fixed issues with repr()

--- utils.h
+++ utils.h
@@ -3,9 +3,9 @@
 
 #include <string>
 
+void log(void *obj, const char *format, ...);
+
 std::string repr(const char *p, size_t size);
 std::string repr(const std::string &string);
-
-void log(void *obj, const char *format, ...);
 
 #endif /* XAPIAND_INCLUDED_UTILS_H */
