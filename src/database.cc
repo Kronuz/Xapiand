c6251001b840be6b2e2650de8cd846c1130be890
German M. Bravo <german.mb@deipi.com>
2015-03-01 01:35:38 -0600

    Handle remote databases when reopening

--- src/database.cc
+++ src/database.cc
@@ -23,6 +23,7 @@
 #include "utils.h"
 
 #include "database.h"
+#include <xapian/dbfactory.h>
 
 
 Database::Database(Endpoints &endpoints_, bool writable_)
@@ -39,13 +40,22 @@ Database::reopen()
 {
 	// FIXME: Handle remote endpoints and figure out if the endpoint is a local database
 	if (writable) {
-		db = new Xapian::WritableDatabase(endpoints[0].path, Xapian::DB_CREATE_OR_OPEN);
+		db = new Xapian::WritableDatabase();
+		if (endpoints[0].protocol == "file") {
+			db->add_database(Xapian::WritableDatabase(endpoints[0].path, Xapian::DB_CREATE_OR_OPEN));
+		} else {
+			db->add_database(Xapian::Remote::open_writable(endpoints[0].host, endpoints[0].port, 0, 10000, endpoints[0].path));
+		}
 	} else {
 		db = new Xapian::Database();
 		if (!writable) {
 			std::vector<Endpoint>::const_iterator i(endpoints.begin());
 			for (; i != endpoints.end(); ++i) {
-				db->add_database(Xapian::Database((*i).path, Xapian::DB_CREATE_OR_OPEN));
+				if (i->protocol == "file") {
+					db->add_database(Xapian::Database((*i).path, Xapian::DB_CREATE_OR_OPEN));
+				} else {
+					db->add_database(Xapian::Remote::open(i->host, i->port, 0, 10000, i->path));
+				}
 			}
 		} else if (endpoints.size() != 1) {
 			LOG_ERR(this, "ERROR: Expecting exactly one database.");
