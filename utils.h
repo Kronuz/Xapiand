d541d715c17c1b5d7cb8ee0e93a3469a3e96fb72
German M. Bravo <german.mb@deipi.com>
2015-03-01 09:41:22 -0600

    Some more logs disabled

--- utils.h
+++ utils.h
@@ -30,15 +30,16 @@ void log(void *obj, const char *fmt, ...);
 std::string repr(const char *p, size_t size);
 std::string repr(const std::string &string);
 
+#define LOG(...) log(__VA_ARGS__)
+#define LOG_ERR(...) log(__VA_ARGS__)
+
 #define LOG_OBJ(...)
 #define LOG_DATABASE(...)
 #define LOG_HTTP_PROTO_PARSER(...)
 
-#define LOG(...) log(__VA_ARGS__)
-#define LOG_ERR(...) log(__VA_ARGS__)
-#define LOG_EV(...) log(__VA_ARGS__)
-#define LOG_CONN(...) log(__VA_ARGS__)
-#define LOG_HTTP_PROTO(...) log(__VA_ARGS__)
-#define LOG_BINARY_PROTO(...) log(__VA_ARGS__)
+#define LOG_EV(...)
+#define LOG_CONN(...)
+#define LOG_HTTP_PROTO(...)
+#define LOG_BINARY_PROTO(...)
 
 #endif /* XAPIAND_INCLUDED_UTILS_H */
