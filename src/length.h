9f2ca30d3d0cfb67829972b31613fed31edda3bb
German M. Bravo <german.mb@deipi.com>
2015-04-01 12:43:01 -0600

    Got permission by olly to include length.h and length.cc with MIT license

---
+++ src/length.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2006,2007,2008,2009,2010,2011,2012 Olly Betts
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef XAPIAND_INCLUDED_LENGTH_H
+#define XAPIAND_INCLUDED_LENGTH_H
+
+#include <string>
+
+/** Encode a length as a variable-length string.
+ *
+ *  The encoding specifies its own length.
+ *
+ *  @param len	The length to encode.
+ *
+ *  @return	The encoded length.
+ */
+std::string encode_length(size_t len);
+
+/** Decode a length encoded by encode_length.
+ *
+ *  @param p	Pointer to a pointer to the string, which will be advanced past
+ *		the encoded length.
+ *  @param end	Pointer to the end of the string.
+ *  @param check_remaining	Check the result against the amount of data
+ *				remaining after the length has been decoded.
+ *
+ *  @return	The decoded length.
+ */
+size_t decode_length(const char ** p, const char *end, bool check_remaining);
+
+#endif /* XAPIAND_INCLUDED_LENGTH_H */
