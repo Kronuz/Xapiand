8a25fa53082ed8c6ec396d99b15d88d420cfe866
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2015-04-10 16:54:35 -0500

    Variables unused

--- utils.cc
+++ utils.cc
@@ -567,8 +567,6 @@ double strtodouble(const std::string &str) {
 std::string timestamp_date(const std::string &str) {
 	int len = (int) str.size();
 	char sign;
-	const char *errptr;
-	int erroffset, ret, n[9];
 	double  timestamp;
 	group *g = NULL;
 	
