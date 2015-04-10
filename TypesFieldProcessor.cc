988440588930d1d2cd8b184d7e5ac7b81797890a
José María Valencia Ramírez <chem_valencia@hotmail.com>
2015-04-10 14:17:33 -0500

    Field Processors

--- TypesFieldProcessor.cc
+++ TypesFieldProcessor.cc
@@ -1,46 +1,125 @@
-//
-//  TypesFieldProcessor.cpp
-//  Xapiand
-//
-//  Created by Jose Madrigal on 07/04/15.
-//  Copyright (c) 2015 Germán M. Bravo. All rights reserved.
-//
+/*
+ * Copyright deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
 #include "TypesFieldProcessor.h"
+#include <xapian/query.h>
 
-NumericFieldProcessor::NumericFieldProcessor(std::string prefix_, std::string field_): prefix(prefix_), field(field_){}
 
-Xapian::Query NumericFieldProcessor::operator()(const std::string &str)
-{
-    Database *database = NULL;
-    return Xapian::Query(prefix + database->serialise(field, str));
+NumericFieldProcessor::NumericFieldProcessor(const std::string &prefix_): prefix(prefix_) {
+	LOG(this, "PREFIX NUMERIC %s\n", prefix.c_str());
 }
 
 
-LatLongFieldProcessor::LatLongFieldProcessor(std::string prefix_, std::string field_): prefix(prefix_), field(field_){ }
+Xapian::Query
+NumericFieldProcessor::operator()(const std::string &str)
+{
+	LOG(this, "Numeric FP %s!!\n", str.c_str());
+	std::string serialise = serialise_numeric(str);
+	if (serialise.size() == 0) {
+		throw Xapian::QueryParserError("Didn't understand numeric specification '" + str + "'");
+	}
+	return Xapian::Query(prefix + serialise);
+}
+
+
+LatLongFieldProcessor::LatLongFieldProcessor(const std::string &prefix_): prefix(prefix_) {}
+
 
 Xapian::Query
 LatLongFieldProcessor::operator()(const std::string &str)
 {
-    Database *database = NULL;
-    return Xapian::Query(prefix + database->serialise(field, str));
+	LOG(NULL, "LatLong FP!!\n");
+	std::string serialise = serialise_geo(str);
+	if (serialise.size() == 0) {
+		throw Xapian::QueryParserError("Didn't understand LatLongs specification '" + str + "'");
+	}
+	return Xapian::Query(prefix + serialise);
 }
 
 
-LatLongDistanceFieldProcessor::LatLongDistanceFieldProcessor(std::string prefix_, std::string field_): prefix(prefix_), field(field_) {}
+LatLongDistanceFieldProcessor::LatLongDistanceFieldProcessor(const std::string &prefix_): prefix(prefix_) {}
+
 
 Xapian::Query
 LatLongDistanceFieldProcessor::operator()(const std::string &str)
 {
-    Database *database = NULL;
-    return Xapian::Query(prefix + database->serialise(field, str));
+	return Xapian::Query("Procesing");
 }
 
 
-BooleanFieldProcessor::BooleanFieldProcessor(std::string prefix_, std::string field_): prefix(prefix_), field(field_){}
+BooleanFieldProcessor::BooleanFieldProcessor(const std::string &prefix_): prefix(prefix_) {}
+
 
 Xapian::Query BooleanFieldProcessor::operator()(const std::string &str)
 {
-	Database *database = NULL;
-	return Xapian::Query(prefix + database->serialise(field, str));
+	LOG(NULL, "Boolean FP!!\n");
+	std::string serialise = serialise_bool(str);
+	if (serialise.size() == 0) {
+		throw Xapian::QueryParserError("Didn't understand bool specification '" + str + "'");
+	}
+	return Xapian::Query(prefix + serialise);
+}
+
+
+DateFieldProcessor::DateFieldProcessor(const std::string &prefix_): prefix(prefix_) {}
+
+
+Xapian::Query DateFieldProcessor::operator()(const std::string &str)
+{
+	LOG(NULL, "Date FP!!\n");
+	std::string serialise = serialise_date(str);
+	if (serialise.size() == 0) {
+		throw Xapian::QueryParserError("Didn't understand date specification '" + str + "'");
+	}
+	return Xapian::Query(prefix + serialise);
+}
+
+
+DateTimeValueRangeProcessor::DateTimeValueRangeProcessor(Xapian::valueno slot_, const std::string prefix_): valno(slot_), prefix(prefix_) {}
+
+
+Xapian::valueno
+DateTimeValueRangeProcessor::operator()(std::string &begin, std::string &end)
+{
+	std::string buf;
+	LOG(this,"Inside of DateTimeValueRangeProcessor\n");
+	
+	if(begin.size() != 0) {
+		buf = prefix + serialise_date(begin);
+		if(buf != "") {
+			begin.assign(buf.c_str(), buf.size());
+			LOG(this,"Serialise of begin %s\n",buf.c_str());
+		}
+		else return Xapian::BAD_VALUENO;
+	}
+	buf = "";
+	
+	if(end.size() != 0) {
+		buf = prefix + serialise_date(end);
+		if(buf != "") {
+			end.assign(buf.c_str(), buf.size());
+			LOG(this,"Serialise of end %s\n",std::string(prefix + buf).c_str());
+		}
+		else return Xapian::BAD_VALUENO;
+	}
+	return valno;
 }
--- TypesFieldProcessor.h
+++ TypesFieldProcessor.h
@@ -1,10 +1,24 @@
-//
-//  TypesFieldProcessor.h
-//  Xapiand
-//
-//  Created by Jose Madrigal on 07/04/15.
-//  Copyright (c) 2015 Germán M. Bravo. All rights reserved.
-//
+/*
+ * Copyright deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
 #ifndef __Xapiand__TypesFieldProcessor__
 #define __Xapiand__TypesFieldProcessor__
@@ -12,43 +26,61 @@
 #include <string.h>
 #include <sstream>
 #include <xapian.h>
+#include "utils.h"
 
-#include "database.h"
 
-class NumericFieldProcessor: public Xapian::FieldProcessor {
-    std::string prefix;
-	std::string field;
+class NumericFieldProcessor : public Xapian::FieldProcessor {
+	std::string prefix;
 
-public:
-    Xapian::Query operator()(const std::string &str);
-	NumericFieldProcessor(std::string prefix, std::string field);
+	public:
+		NumericFieldProcessor(const std::string &prefix);
+		Xapian::Query operator()(const std::string &str);
 };
 
-class LatLongFieldProcessor: public Xapian::FieldProcessor {
-	std::string prefix;
-	std::string field;
-    
-public:
-    Xapian::Query operator()(const std::string &str);
-    LatLongFieldProcessor(std::string prefix, std::string field);
-};
 
-class LatLongDistanceFieldProcessor: public Xapian::FieldProcessor {
+class LatLongFieldProcessor : public Xapian::FieldProcessor {
 	std::string prefix;
-	std::string field;
 	
-public:
-    Xapian::Query operator()(const std::string &str);
-	LatLongDistanceFieldProcessor(std::string prefix, std::string field);
+	public:
+		LatLongFieldProcessor(const std::string &prefix);
+		Xapian::Query operator()(const std::string &str);
 };
 
-class BooleanFieldProcessor: public Xapian::FieldProcessor {
+
+class LatLongDistanceFieldProcessor : public Xapian::FieldProcessor {
 	std::string prefix;
-	std::string field;
 	
-public:
-	Xapian::Query operator()(const std::string &str);
-	BooleanFieldProcessor(std::string prefix, std::string field);
+	public:
+		LatLongDistanceFieldProcessor(const std::string &prefix);
+		Xapian::Query operator()(const std::string &str);
+};
+
+
+class BooleanFieldProcessor : public Xapian::FieldProcessor {
+	std::string prefix;
+	
+	public:
+		BooleanFieldProcessor(const std::string &prefix);
+		Xapian::Query operator()(const std::string &str);
+};
+
+
+class DateFieldProcessor : public Xapian::FieldProcessor {
+	std::string prefix;
+	
+	public:
+		DateFieldProcessor(const std::string &prefix);
+		Xapian::Query operator()(const std::string &str);
+};
+
+
+class DateTimeValueRangeProcessor: public Xapian::ValueRangeProcessor {
+	std::string prefix;
+	Xapian::valueno valno;
+
+	public:
+		DateTimeValueRangeProcessor(Xapian::valueno slot_, const std::string prefix);
+		Xapian::valueno operator()(std::string &begin, std::string &end);
 };
 
 
