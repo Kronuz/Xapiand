93732d87c10c24bc0d173f0970cf1e3e34e42cbd
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2015-04-13 14:42:29 -0500

    Added Regularexpression  NUMERIC_RE

--- utils.cc
+++ utils.cc
@@ -31,6 +31,7 @@
 #define DATE_RE "(([1-9][0-9]{3})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])(T([01][0-9]|2[0-3]):([0-5][0-9])(:([0-5][0-9])(\\.([0-9]{3}))?)?(([+-])([01][0-9]|2[0-3])(:([0-5][0-9]))?)?)?)"
 #define COORDS_RE "(\\d*\\.\\d+|\\d+)\\s?,\\s?(\\d*\\.\\d+|\\d+)"
 #define COORDS_DISTANCE_RE "(\\d*\\.\\d+|\\d+)\\s?,\\s?(\\d*\\.\\d*|\\d+)\\s?;\\s?(\\d*\\.\\d*|\\d+)"
+#define NUMERIC_RE "(\\d*\\.\\d+|\\d+)"
 #define FIND_RANGE_RE "([^ ]*\\.\\.)"
 #define FIND_ORDER_RE "([_a-zA-Z][_a-zA-Z0-9]+,[_a-zA-Z][_a-zA-Z0-9]*)"
 
@@ -39,6 +40,7 @@ pthread_mutex_t qmtx = PTHREAD_MUTEX_INITIALIZER;
 pcre *compiled_date_re = NULL;
 pcre *compiled_coords_re = NULL;
 pcre *compiled_coords_dist_re = NULL;
+pcre *compiled_numeric_re = NULL;
 pcre *compiled_find_range_re = NULL;
 
 
