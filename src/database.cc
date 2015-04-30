ce9f538c11ac53b4a377eab470a5938e4ae69a9b
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2015-04-30 18:01:23 -0500

    Added method get_similar for fuzzy and nearest

--- src/database.cc
+++ src/database.cc
@@ -911,6 +911,38 @@ Database::_search(const std::string &query, unsigned int flags, bool text, const
 }
 
 
+//Add exception for get_mset
+void
+Database::get_similar(bool is_fuzzy, Xapian::Enquire &enquire, Xapian::Query &query, similar_t *similar)
+{
+	Xapian::RSet rset;
+	std::vector<std::string>::const_iterator it;
+	
+	Xapian::Enquire renquire = get_enquire(query, NULL, NULL, NULL, NULL, NULL);
+	Xapian::MSet mset = renquire.get_mset(0, similar->n_rset);
+	
+	for (Xapian::MSetIterator m = mset.begin(); m != mset.end(); m++) {
+		rset.add_document(*m);
+	}
+	
+	std::vector<std::string>prefixes;
+	for(it = similar->type.begin(); it != similar->type.end(); it++) {
+		prefixes.push_back(DOCUMENT_CUSTOM_TERM_PREFIX + *it);
+	}
+	for(it = similar->field.begin(); it != similar->field.end(); it++) {
+		prefixes.push_back(get_prefix(*it, DOCUMENT_CUSTOM_TERM_PREFIX));
+	}
+	ExpandDeciderFilterPrefixes efp(prefixes);
+	Xapian::ESet eset = enquire.get_eset(similar->n_eset, rset, &efp);
+	
+	if (is_fuzzy) {
+		query = Xapian::Query(Xapian::Query::OP_OR, query, Xapian::Query(Xapian::Query::OP_ELITE_SET, eset.begin(), eset.end()));
+	} else {
+		query = Xapian::Query(Xapian::Query::OP_ELITE_SET, eset.begin(), eset.end());
+	}
+}
+
+
 Xapian::Enquire
 Database::get_enquire(Xapian::Query &query, Xapian::MultiValueKeyMaker *sorter, std::vector<std::pair<std::string, std::unique_ptr<MultiValueCountMatchSpy>>>&spies, query_t e)
 {
