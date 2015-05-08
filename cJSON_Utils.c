69f0392f87297820123ba88a1f6872555fa31071
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2015-05-08 15:23:02 -0500

    Add cJSON_Utils files  for compiling

---
+++ cJSON_Utils.c
@@ -0,0 +1,9 @@
+//
+//  cJSON_Utils.cpp
+//  Xapiand
+//
+//  Created by Jose Madrigal on 08/05/15.
+//  Copyright (c) 2015 Germán M. Bravo. All rights reserved.
+//
+
+#include "cJSON_Utils.h"
---
+++ cJSON_Utils.h
@@ -0,0 +1,14 @@
+//
+//  cJSON_Utils.h
+//  Xapiand
+//
+//  Created by Jose Madrigal on 08/05/15.
+//  Copyright (c) 2015 Germán M. Bravo. All rights reserved.
+//
+
+#ifndef __Xapiand__cJSON_Utils__
+#define __Xapiand__cJSON_Utils__
+
+#include <stdio.h>
+
+#endif /* defined(__Xapiand__cJSON_Utils__) */
