55b54cdd0f28da66a93bf8c95dd8f6c164310799
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2015-05-08 16:14:05 -0500

    Silencing warnings

--- src/utils.cc
+++ src/utils.cc
@@ -126,7 +126,7 @@ int32_t jump_consistent_hash(uint64_t key, int32_t num_buckets)
 		key = key * 2862933555777941757ULL + 1;
 		j = (b + 1) * (double(1LL << 31) / double((key >> 33) + 1));
 	}
-	return b;
+	return (int32_t) b;
 }
 
 
