95fc61aeedf33abc2d9ee402574c141af7b2fcad
German M. Bravo <german.mb@deipi.com>
2015-05-10 13:07:32 -0500

    Discovery server moved to it's own class

--- src/manager.cc
+++ src/manager.cc
@@ -31,15 +31,10 @@
 #include <assert.h>
 #include <sys/sysctl.h>
 #include <fcntl.h>
-#include <netinet/tcp.h> /* for TCP_NODELAY */
 #include <net/if.h> /* for IFF_LOOPBACK */
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <ifaddrs.h>
 #include <unistd.h>
-
-
-const uint16_t XAPIAND_DISCOVERY_PROTOCOL_VERSION = XAPIAND_DISCOVERY_PROTOCOL_MAJOR_VERSION | XAPIAND_DISCOVERY_PROTOCOL_MINOR_VERSION << 8;
 
 
 #define TIME_RE "((([01]?[0-9]|2[0-3])h)?([0-5]?[0-9]m)?([0-5]?[0-9]s)?)(\\.\\.(([01]?[0-9]|2[0-3])h)?([0-5]?[0-9]m)?([0-5]?[0-9]s)?)?"
@@ -50,22 +45,13 @@ pcre *XapiandManager::compiled_time_re = NULL;
 XapiandManager::XapiandManager(ev::loop_ref *loop_, const char *cluster_name_, const char *node_name_, const char *discovery_group_, int discovery_port_, int http_port_, int binary_port_)
 	: loop(loop_ ? loop_: &dynamic_loop),
 	  state(STATE_RESET),
-	  cluster_name(cluster_name_),
-	  node_name(node_name_),
-	  discovery_io(*loop),
-	  discovery_heartbeat(*loop),
 	  break_loop(*loop),
 	  shutdown_asap(0),
 	  shutdown_now(0),
 	  async_shutdown(*loop),
 	  thread_pool("W%d", 10),
-	  discovery_port(discovery_port_)
+	  discovery(this, NULL, cluster_name_, node_name_, discovery_group_, discovery_port_)
 {
-#ifdef HAVE_SRANDDEV
-	sranddev();
-#else
-	srand(time(NULL));
-#endif
 
 	this_node.http_port = http_port_;
 	this_node.binary_port = binary_port_;
@@ -88,11 +74,6 @@ XapiandManager::XapiandManager(ev::loop_ref *loop_, const char *cluster_name_, c
 
 	this_node.addr = host_address();
 
-	if (discovery_port == 0) {
-		discovery_port = XAPIAND_DISCOVERY_SERVERPORT;
-	}
-	bind_udp("discovery", discovery_sock, discovery_port, discovery_addr, 1, discovery_group_ ? discovery_group_ : XAPIAND_DISCOVERY_GROUP);
-
 	int http_tries = 1;
 	if (this_node.http_port == 0) {
 		this_node.http_port = XAPIAND_HTTP_SERVERPORT;
@@ -109,13 +90,7 @@ XapiandManager::XapiandManager(ev::loop_ref *loop_, const char *cluster_name_, c
 	bind_tcp("binary", binary_sock, this_node.binary_port, addr, binary_tries);
 #endif  /* HAVE_REMOTE_PROTOCOL */
 
-	assert(discovery_sock != -1 && http_sock != -1 && binary_sock != -1);
-
-	discovery_io.set<XapiandManager, &XapiandManager::discovery_io_cb>(this);
-	discovery_io.start(discovery_sock, ev::READ);
-
-	discovery_heartbeat.set<XapiandManager, &XapiandManager::discovery_heartbeat_cb>(this);
-	discovery_heartbeat.start(0, 1);
+	assert(http_sock != -1 && binary_sock != -1);
 
 	LOG_OBJ(this, "CREATED MANAGER!\n");
 }
@@ -134,37 +109,6 @@ XapiandManager::~XapiandManager()
 	LOG_OBJ(this, "DELETED MANAGER!\n");
 }
 
-
-void XapiandManager::check_tcp_backlog(int tcp_backlog)
-{
-#if defined(NET_CORE_SOMAXCONN)
-	int name[3] = {CTL_NET, NET_CORE, NET_CORE_SOMAXCONN};
-	int somaxconn;
-	size_t somaxconn_len = sizeof(somaxconn);
-	if (sysctl(name, 3, &somaxconn, &somaxconn_len, 0, 0) < 0) {
-		LOG_ERR(this, "ERROR: sysctl: %s\n", strerror(errno));
-		return;
-	}
-	if (somaxconn > 0 && somaxconn < tcp_backlog) {
-		LOG_ERR(this, "WARNING: The TCP backlog setting of %d cannot be enforced because "
-				"net.core.somaxconn"
-				" is set to the lower value of %d.\n", tcp_backlog, somaxconn);
-	}
-#elif defined(KIPC_SOMAXCONN)
-	int name[3] = {CTL_KERN, KERN_IPC, KIPC_SOMAXCONN};
-	int somaxconn;
-	size_t somaxconn_len = sizeof(somaxconn);
-	if (sysctl(name, 3, &somaxconn, &somaxconn_len, 0, 0) < 0) {
-		LOG_ERR(this, "ERROR: sysctl: %s\n", strerror(errno));
-		return;
-	}
-	if (somaxconn > 0 && somaxconn < tcp_backlog) {
-		LOG_ERR(this, "WARNING: The TCP backlog setting of %d cannot be enforced because "
-				"kern.ipc.somaxconn"
-				" is set to the lower value of %d.\n", tcp_backlog, somaxconn);
-	}
-#endif
-}
 
 struct sockaddr_in XapiandManager::host_address()
 {
@@ -185,119 +129,6 @@ struct sockaddr_in XapiandManager::host_address()
 		freeifaddrs(if_addr_struct);
 	}
 	return addr;
-}
-
-bool XapiandManager::bind_tcp(const char *type, int &sock, int &port, struct sockaddr_in &addr, int tries)
-{
-	int tcp_backlog = XAPIAND_TCP_BACKLOG;
-	int optval = 1;
-	struct linger ling = {0, 0};
-
-	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-		LOG_ERR(this, "ERROR: %s socket: %s\n", type, strerror(errno));
-		return false;
-	}
-
-	// use setsockopt() to allow multiple listeners connected to the same address
-	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt SO_REUSEADDR (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-#ifdef SO_NOSIGPIPE
-	if (setsockopt(sock, SOL_SOCKET, SO_NOSIGPIPE, &optval, sizeof(optval)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt SO_NOSIGPIPE (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-#endif
-	if (setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt SO_KEEPALIVE (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-
-	if (setsockopt(sock, SOL_SOCKET, SO_LINGER, &ling, sizeof(ling)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt SO_LINGER (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-
-	if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &optval, sizeof(optval)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt TCP_NODELAY (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	for (int i = 0; i < tries; i++, port++) {
-		addr.sin_port = htons(port);
-
-		if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-			LOG_DEBUG(this, "ERROR: %s bind error (sock=%d): %s\n", type, sock, strerror(errno));
-		} else {
-			fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
-			check_tcp_backlog(tcp_backlog);
-			listen(sock, tcp_backlog);
-			return true;
-		}
-	}
-
-	LOG_ERR(this, "ERROR: %s bind error (sock=%d): %s\n", type, sock, strerror(errno));
-	::close(sock);
-	sock = -1;
-	return false;
-}
-
-
-bool XapiandManager::bind_udp(const char *type, int &sock, int &port, struct sockaddr_in &addr, int tries, const char *group)
-{
-	int optval = 1;
-	unsigned char ttl = 3;
-	struct ip_mreq mreq;
-
-	if ((sock = socket(PF_INET, SOCK_DGRAM, 0)) < 0) {
-		LOG_ERR(this, "ERROR: %s socket: %s\n", type, strerror(errno));
-		return false;
-	}
-
-	// use setsockopt() to allow multiple listeners connected to the same port
-	if (setsockopt(sock, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt SO_REUSEPORT (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-
-	if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_LOOP, &optval, sizeof(optval)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt IP_MULTICAST_LOOP (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-
-	if (setsockopt(sock, IPPROTO_IP, IP_MULTICAST_TTL, &ttl, sizeof(ttl)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt IP_MULTICAST_TTL (sock=%d): %s\n", type, sock, strerror(errno));
-	}
-
-	// use setsockopt() to request that the kernel join a multicast group
-	memset(&mreq, 0, sizeof(mreq));
-	mreq.imr_multiaddr.s_addr = inet_addr(group);
-	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-	if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0) {
-		LOG_ERR(this, "ERROR: %s setsockopt IP_ADD_MEMBERSHIP (sock=%d): %s\n", type, sock, strerror(errno));
-		::close(sock);
-		sock = -1;
-		return false;
-	}
-
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);  // bind to all addresses (differs from sender)
-
-	for (int i = 0; i < tries; i++, port++) {
-		addr.sin_port = htons(port);
-
-		if (bind(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-			LOG_DEBUG(this, "ERROR: %s bind error (sock=%d): %s\n", type, sock, strerror(errno));
-		} else {
-			fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
-			addr.sin_addr.s_addr = inet_addr(group);  // setup s_addr for sender (send to group)
-			return true;
-		}
-	}
-
-	LOG_ERR(this, "ERROR: %s bind error (sock=%d): %s\n", type, sock, strerror(errno));
-	::close(sock);
-	sock = -1;
-	return false;
 }
 
 
@@ -339,16 +170,9 @@ void XapiandManager::sig_shutdown_handler(int sig)
 void XapiandManager::destroy()
 {
 	pthread_mutex_lock(&qmtx);
-	if (discovery_sock == -1 && http_sock == -1 && binary_sock == -1) {
+	if (http_sock == -1 && binary_sock == -1) {
 		pthread_mutex_unlock(&qmtx);
 		return;
-	}
-
-	discovery(DISCOVERY_BYE, this_node);
-
-	if (discovery_sock != -1) {
-		::close(discovery_sock);
-		discovery_sock = -1;
 	}
 
 	if (http_sock != -1) {
@@ -360,8 +184,6 @@ void XapiandManager::destroy()
 		::close(binary_sock);
 		binary_sock = -1;
 	}
-
-	discovery_io.stop();
 
 	pthread_mutex_unlock(&qmtx);
 
@@ -424,252 +246,6 @@ void XapiandManager::shutdown()
 }
 
 
-void XapiandManager::discovery_heartbeat_cb(ev::timer &watcher, int revents)
-{
-	if (state != STATE_READY) {
-		if (state == STATE_RESET) {
-			if (!this_node.name.empty()) {
-				nodes.erase(stringtolower(this_node.name));
-			}
-			if (node_name.empty()) {
-				this_node.name = name_generator();
-			} else {
-				this_node.name = node_name;
-			}
-		}
-		discovery(DISCOVERY_HELLO, this_node);
-	} else {
-		discovery(DISCOVERY_PING, this_node);
-	}
-	if (state && state-- == 1) {
-		discovery_heartbeat.set(0, 10);
-	}
-}
-
-
-void XapiandManager::discovery_io_cb(ev::io &watcher, int revents)
-{
-	if (EV_ERROR & revents) {
-		LOG_EV(this, "ERROR: got invalid discovery event (sock=%d): %s\n", discovery_sock, strerror(errno));
-		return;
-	}
-
-	if (discovery_sock == -1) {
-		return;
-	}
-
-	assert(discovery_sock == watcher.fd);
-
-	if (discovery_sock != -1 && revents & EV_READ) {
-		char buf[1024];
-		struct sockaddr_in addr;
-		socklen_t addrlen;
-
-		ssize_t received = ::recvfrom(discovery_sock, buf, sizeof(buf), 0, (struct sockaddr *)&addr, &addrlen);
-
-		if (received < 0) {
-			if (errno != EAGAIN && discovery_sock != -1) {
-				LOG_ERR(this, "ERROR: read error (sock=%d): %s\n", discovery_sock, strerror(errno));
-				destroy();
-			}
-		} else if (received == 0) {
-			// The peer has closed its half side of the connection.
-			LOG_CONN(this, "Received EOF (sock=%d)!\n", discovery_sock);
-			destroy();
-		} else {
-			LOG_DISCOVERY_WIRE(this, "(sock=%d) -->> '%s'\n", discovery_sock, repr(buf, received).c_str());
-
-			if (received < 4) {
-				LOG_DISCOVERY(this, "Badly formed message: Incomplete!\n");
-				return;
-			}
-
-			// LOG(this, "%s says '%s'\n", inet_ntoa(addr.sin_addr), repr(buf, received).c_str());
-			uint16_t remote_protocol_version = *(uint16_t *)(buf + 1);
-			if ((remote_protocol_version & 0xff) > XAPIAND_DISCOVERY_PROTOCOL_MAJOR_VERSION) {
-				LOG_DISCOVERY(this, "Badly formed message: Protocol version mismatch %x vs %x!\n", remote_protocol_version & 0xff, XAPIAND_DISCOVERY_PROTOCOL_MAJOR_VERSION);
-				return;
-			}
-
-			const char *ptr = buf + 3;
-			Node remote_node;
-			size_t decoded;
-
-			if ((decoded = decode_length(&ptr, buf + received, true)) == -1) {
-				LOG_DISCOVERY(this, "Badly formed message: No cluster name!\n");
-				return;
-			}
-			std::string remote_cluster_name(ptr, decoded);
-			ptr += decoded;
-			if (remote_cluster_name != cluster_name) {
-				return;
-			}
-
-			if ((decoded = decode_length(&ptr, buf + received, false)) == -1) {
-				LOG_DISCOVERY(this, "Badly formed message: No address!\n");
-				return;
-			}
-			remote_node.addr.sin_addr.s_addr = (int) decoded;
-
-			if ((decoded = decode_length(&ptr, buf + received, false)) == -1) {
-				LOG_DISCOVERY(this, "Badly formed message: No http port!\n");
-				return;
-			}
-			remote_node.http_port = (int) decoded;
-
-			if ((decoded = decode_length(&ptr, buf + received, false)) == -1) {
-				LOG_DISCOVERY(this, "Badly formed message: No binary port!\n");
-				return;
-			}
-			remote_node.binary_port = (int) decoded;
-
-			if ((decoded = decode_length(&ptr, buf + received, true)) <= 0) {
-				LOG_DISCOVERY(this, "Badly formed message: No name length!\n");
-				return;
-			}
-			remote_node.name = std::string(ptr, decoded);
-			ptr += decoded;
-
-			int remote_pid = (int) decode_length(&ptr, buf + received, false);
-
-			// LOG_DISCOVERY(this, "%s on ip:%s, tcp:%d (http), tcp:%d (xapian), at pid:%d\n", remote_node.name.c_str(), inet_ntoa(remote_node.addr.sin_addr), remote_node.http_port, remote_node.binary_port, remote_pid);
-
-			Node *node;
-			time_t now = time(NULL);
-
-			switch (buf[0]) {
-				case DISCOVERY_HELLO:
-					if (remote_node.addr.sin_addr.s_addr == this_node.addr.sin_addr.s_addr &&
-						remote_node.http_port == this_node.http_port &&
-						remote_node.binary_port == this_node.binary_port) {
-						discovery(DISCOVERY_WAVE, this_node);
-					} else {
-						try {
-							node = &nodes.at(stringtolower(remote_node.name));
-							if (remote_node.addr.sin_addr.s_addr == node->addr.sin_addr.s_addr &&
-								remote_node.http_port == node->http_port &&
-								remote_node.binary_port == node->binary_port) {
-								discovery(DISCOVERY_WAVE, this_node);
-							} else {
-								discovery(DISCOVERY_SNEER, remote_node);
-							}
-						} catch (const std::out_of_range& err) {
-							discovery(DISCOVERY_WAVE, this_node);
-						}
-					}
-					break;
-
-				case DISCOVERY_WAVE:
-					try {
-						node = &nodes.at(stringtolower(remote_node.name));
-					} catch (const std::out_of_range& err) {
-						node = &nodes[stringtolower(remote_node.name)];
-						node->name = remote_node.name;
-						node->addr.sin_addr.s_addr = remote_node.addr.sin_addr.s_addr;
-						node->http_port = remote_node.http_port;
-						node->binary_port = remote_node.binary_port;
-						INFO(this, "Node %s joined the party on ip:%s, tcp:%d (http), tcp:%d (xapian), at pid:%d!\n", remote_node.name.c_str(), inet_ntoa(remote_node.addr.sin_addr), remote_node.http_port, remote_node.binary_port, remote_pid);
-					}
-					if (remote_node.addr.sin_addr.s_addr == node->addr.sin_addr.s_addr &&
-						remote_node.http_port == node->http_port &&
-						remote_node.binary_port == node->binary_port) {
-						node->touched = now;
-					}
-					break;
-
-				case DISCOVERY_SNEER:
-					if (state != STATE_READY &&
-						remote_node.name == this_node.name &&
-						remote_node.addr.sin_addr.s_addr == this_node.addr.sin_addr.s_addr &&
-						remote_node.http_port == this_node.http_port &&
-						remote_node.binary_port == this_node.binary_port) {
-						if (node_name.empty()) {
-							state = STATE_RESET;
-							discovery_heartbeat.set(0, 1);
-							LOG_DISCOVERY(this, "Retrying other name");
-						} else {
-							LOG_ERR(this, "Cannot join the party. Node name %s already taken!\n", this_node.name.c_str());
-							this_node.name.clear();
-							shutdown_asap = time(NULL);
-							shutdown();
-						}
-					}
-					break;
-
-				case DISCOVERY_PING:
-					try {
-						node = &nodes.at(stringtolower(remote_node.name));
-						node->touched = now;
-						// Received a ping, return pong
-						discovery(DISCOVERY_PONG, this_node);
-					} catch (const std::out_of_range& err) {
-						LOG_DISCOVERY(this, "Ignoring ping from unknown peer");
-					}
-					break;
-
-				case DISCOVERY_PONG:
-					try {
-						node = &nodes.at(stringtolower(remote_node.name));
-						node->touched = now;
-					} catch (const std::out_of_range& err) {
-						LOG_DISCOVERY(this, "Ignoring pong from unknown peer");
-					}
-					break;
-
-				case DISCOVERY_BYE:
-					nodes.erase(stringtolower(remote_node.name));
-					INFO(this, "Node %s left the party!\n", remote_node.name.c_str());
-					break;
-			}
-		}
-	}
-}
-
-
-void XapiandManager::discovery(const char *buf, size_t buf_size)
-{
-	if (discovery_sock != -1) {
-		LOG_DISCOVERY_WIRE(this, "(sock=%d) <<-- '%s'\n", discovery_sock, repr(buf, buf_size).c_str());
-
-#ifdef MSG_NOSIGNAL
-		ssize_t written = ::sendto(discovery_sock, buf, buf_size, MSG_NOSIGNAL, (struct sockaddr *)&discovery_addr, sizeof(discovery_addr));
-#else
-		ssize_t written = ::sendto(discovery_sock, buf, buf_size, 0, (struct sockaddr *)&discovery_addr, sizeof(discovery_addr));
-#endif
-
-		if (written < 0) {
-			if (errno != EAGAIN && discovery_sock != -1) {
-				LOG_ERR(this, "ERROR: sendto error (sock=%d): %s\n", discovery_sock, strerror(errno));
-				destroy();
-			}
-		} else if (written == 0) {
-			// nothing written?
-		} else {
-
-		}
-	}
-}
-
-
-void XapiandManager::discovery(discovery_type type, Node &node)
-{
-	if (node.name.empty()) {
-		return;
-	}
-	std::string message((const char *)&type, 1);
-	message.append(std::string((const char *)&XAPIAND_DISCOVERY_PROTOCOL_VERSION, sizeof(uint16_t)));
-	message.append(encode_length(cluster_name.size()));
-	message.append(cluster_name);
-	message.append(encode_length(node.addr.sin_addr.s_addr));
-	message.append(encode_length(node.http_port));
-	message.append(encode_length(node.binary_port));
-	message.append(encode_length(node.name.size()));
-	message.append(node.name);
-	message.append(encode_length(getpid()));
-	discovery(message.c_str(), message.size());
-}
-
-
 void XapiandManager::run(int num_servers)
 {
 	std::string msg("Listening on ");
@@ -679,8 +255,8 @@ void XapiandManager::run(int num_servers)
 	if (this_node.binary_port != -1) {
 		msg += "tcp:" + std::to_string(this_node.binary_port) + " (xapian v" + std::to_string(XAPIAN_REMOTE_PROTOCOL_MAJOR_VERSION) + "." + std::to_string(XAPIAN_REMOTE_PROTOCOL_MINOR_VERSION) + "), ";
 	}
-	if (discovery_port != -1) {
-		msg += "udp:" + std::to_string(discovery_port) + " (discovery v" + std::to_string(XAPIAND_DISCOVERY_PROTOCOL_MAJOR_VERSION) + "." + std::to_string(XAPIAND_DISCOVERY_PROTOCOL_MINOR_VERSION) + "), ";
+	if (discovery.discovery_port != -1) {
+		msg += "udp:" + std::to_string(discovery.discovery_port) + " (discovery v" + std::to_string(XAPIAND_DISCOVERY_PROTOCOL_MAJOR_VERSION) + "." + std::to_string(XAPIAND_DISCOVERY_PROTOCOL_MINOR_VERSION) + "), ";
 	}
 	msg += "at pid:" + std::to_string(getpid()) + "...\n";
 
