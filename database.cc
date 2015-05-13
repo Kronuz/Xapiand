947ac6c9b60de670610655a5ae71f44b255a9b2c
JoseMariaVR <chem_valencia@hotmail.com>
2015-05-13 14:47:15 -0500

    New reserved words

--- database.cc
+++ database.cc
@@ -444,15 +444,27 @@ Database::patch(cJSON *patches, const std::string &_document_id, bool commit, co
 bool
 Database::is_reserved(const std::string &word)
 {
-	if (word.compare(RESERVED_WEIGHT)    != 0 &&
-		word.compare(RESERVED_POSITION)  != 0 &&
-		word.compare(RESERVED_LANGUAGE)  != 0 &&
-		word.compare(RESERVED_SPELLING)  != 0 &&
-		word.compare(RESERVED_POSITIONS) != 0 &&
-		word.compare(RESERVED_TEXTS)     != 0 &&
-		word.compare(RESERVED_VALUES)    != 0 &&
-		word.compare(RESERVED_TERMS)     != 0 &&
-		word.compare(RESERVED_DATA)      != 0) {
+	if (word.compare(RESERVED_WEIGHT)      != 0 &&
+		word.compare(RESERVED_POSITION)    != 0 &&
+		word.compare(RESERVED_LANGUAGE)    != 0 &&
+		word.compare(RESERVED_SPELLING)    != 0 &&
+		word.compare(RESERVED_POSITIONS)   != 0 &&
+		word.compare(RESERVED_TEXTS)       != 0 &&
+		word.compare(RESERVED_VALUES)      != 0 &&
+		word.compare(RESERVED_TERMS)       != 0 &&
+		word.compare(RESERVED_DATA)        != 0 &&
+		word.compare(RESERVED_ACCURACY)    != 0 &&
+		word.compare(RESERVED_STORE)       != 0 &&
+		word.compare(RESERVED_TYPE)        != 0 &&
+		word.compare(RESERVED_ANALYZER)    != 0 &&
+		word.compare(RESERVED_DYNAMIC)     != 0 &&
+		word.compare(RESERVED_D_DETECTION) != 0 &&
+		word.compare(RESERVED_N_DETECTION) != 0 &&
+		word.compare(RESERVED_G_DETECTION) != 0 &&
+		word.compare(RESERVED_B_DETECTION) != 0 &&
+		word.compare(RESERVED_S_DETECTION) != 0 &&
+		word.compare(RESERVED_VALUE)       != 0 &&
+		word.compare(RESERVED_ID)          != 0) {
 		return false;
 	}
 	return true;
