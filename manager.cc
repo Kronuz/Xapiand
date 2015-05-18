d38533468385f3f29f014886cfb1fad84d111627
German M. Bravo <german.mb@deipi.com>
2015-05-18 00:29:25 -0500

    Use xapian protocol for node endpoint

--- manager.cc
+++ manager.cc
@@ -266,7 +266,10 @@ XapiandManager::setup_node()
 	pthread_mutex_lock(&qmtx);
 
 	// Open cluster database
-	Endpoint cluster_endpoint("file://.");
+	char ip[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &this_node.addr.sin_addr, ip, INET_ADDRSTRLEN);
+
+	Endpoint cluster_endpoint("xapian://" + std::string(ip) + ":" + std::to_string(this_node.binary_port) + "/.");
 	Endpoints cluster_endpoints;
 	cluster_endpoints.insert(cluster_endpoint);
 	if(!database_pool.checkout(&cluster_database, cluster_endpoints, DB_WRITABLE|DB_PERSISTENT)) {
