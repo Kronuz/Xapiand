c64be1827ab4dfde8096687285daa948f24d3f5d
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2015-06-19 10:58:01 -0500

    Missing header in utils.cc

--- src/utils.cc
+++ src/utils.cc
@@ -35,6 +35,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <netinet/tcp.h> /* for TCP_NODELAY */
 #include <netdb.h> /* for getaddrinfo */
 #include <unistd.h>
