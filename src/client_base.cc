eb6fac1ae5a53e3584ed4a4decdccd8ecc667ff3
German M. Bravo <german.mb@deipi.com>
2015-09-17 13:52:23 -0500

    Do not receive data from closet clients

--- src/client_base.cc
+++ src/client_base.cc
@@ -269,7 +269,7 @@ void BaseClient::read_cb()
 {
 	LZ4F_errorCode_t errorCode;
 
-	if (sock != -1) {
+	if (sock != -1 && !closed) {
 		ssize_t received = ::read(sock, read_buffer, BUF_SIZE);
 		const char *buf_end = read_buffer + received;
 		const char *buf_data = read_buffer;
