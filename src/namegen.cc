dc50eba28a75665e8170be4318df0c7eab0a6312
German M. Bravo <german.mb@deipi.com>
2015-09-21 00:23:49 -0500

    Added namegen library (based on rinkworks name generator)

---
+++ src/namegen.cc
@@ -0,0 +1,452 @@
+/**
+ * Copyright (C) 2015 German M. Bravo (Kronuz). All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "namegen.h"
+
+#include <random>
+
+
+using namespace NameGen;
+
+
+static std::random_device rd;  // Random device engine, usually based on /dev/random on UNIX-like systems
+static std::mt19937 rng(rd()); // Initialize Mersennes' twister using rd to generate the seed
+
+const std::unordered_map<std::string, const std::vector<const std::string> >
+Generator::symbols = {
+	{
+		"s", {
+			"ach", "ack", "ad", "age", "ald", "ale", "an", "ang", "ar", "ard",
+			"as", "ash", "at", "ath", "augh", "aw", "ban", "bel", "bur", "cer",
+			"cha", "che", "dan", "dar", "del", "den", "dra", "dyn", "ech", "eld",
+			"elm", "em", "en", "end", "eng", "enth", "er", "ess", "est", "et",
+			"gar", "gha", "hat", "hin", "hon", "ia", "ight", "ild", "im", "ina",
+			"ine", "ing", "ir", "is", "iss", "it", "kal", "kel", "kim", "kin",
+			"ler", "lor", "lye", "mor", "mos", "nal", "ny", "nys", "old", "om",
+			"on", "or", "orm", "os", "ough", "per", "pol", "qua", "que", "rad",
+			"rak", "ran", "ray", "ril", "ris", "rod", "roth", "ryn", "sam",
+			"say", "ser", "shy", "skel", "sul", "tai", "tan", "tas", "ther",
+			"tia", "tin", "ton", "tor", "tur", "um", "und", "unt", "urn", "usk",
+			"ust", "ver", "ves", "vor", "war", "wor", "yer"
+		}
+	},
+	{
+		"v", {
+			"a", "e", "i", "o", "u", "y"
+		}
+	},
+	{
+		"V", {
+			"a", "e", "i", "o", "u", "y", "ae", "ai", "au", "ay", "ea", "ee",
+			"ei", "eu", "ey", "ia", "ie", "oe", "oi", "oo", "ou", "ui"
+		}
+	},
+	{
+		"c", {
+			"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r",
+			"s", "t", "v", "w", "x", "y", "z"
+		}
+	},
+	{
+		"B", {
+			"b", "bl", "br", "c", "ch", "chr", "cl", "cr", "d", "dr", "f", "g",
+			"h", "j", "k", "l", "ll", "m", "n", "p", "ph", "qu", "r", "rh", "s",
+			"sch", "sh", "sl", "sm", "sn", "st", "str", "sw", "t", "th", "thr",
+			"tr", "v", "w", "wh", "y", "z", "zh"
+		}
+	},
+	{
+		"C", {
+			"b", "c", "ch", "ck", "d", "f", "g", "gh", "h", "k", "l", "ld", "ll",
+			"lt", "m", "n", "nd", "nn", "nt", "p", "ph", "q", "r", "rd", "rr",
+			"rt", "s", "sh", "ss", "st", "t", "th", "v", "w", "y", "z"
+		}
+	},
+	{
+		"i", {
+			"air", "ankle", "ball", "beef", "bone", "bum", "bumble", "bump",
+			"cheese", "clod", "clot", "clown", "corn", "dip", "dolt", "doof",
+			"dork", "dumb", "face", "finger", "foot", "fumble", "goof",
+			"grumble", "head", "knock", "knocker", "knuckle", "loaf", "lump",
+			"lunk", "meat", "muck", "munch", "nit", "numb", "pin", "puff",
+			"skull", "snark", "sneeze", "thimble", "twerp", "twit", "wad",
+			"wimp", "wipe"
+		}
+	},
+	{
+		"m", {
+			"baby", "booble", "bunker", "cuddle", "cuddly", "cutie", "doodle",
+			"foofie", "gooble", "honey", "kissie", "lover", "lovey", "moofie",
+			"mooglie", "moopie", "moopsie", "nookum", "poochie", "poof",
+			"poofie", "pookie", "schmoopie", "schnoogle", "schnookie",
+			"schnookum", "smooch", "smoochie", "smoosh", "snoogle", "snoogy",
+			"snookie", "snookum", "snuggy", "sweetie", "woogle", "woogy",
+			"wookie", "wookum", "wuddle", "wuddly", "wuggy", "wunny"
+		}
+	},
+	{
+		"M", {
+			"boo", "bunch", "bunny", "cake", "cakes", "cute", "darling",
+			"dumpling", "dumplings", "face", "foof", "goo", "head", "kin",
+			"kins", "lips", "love", "mush", "pie", "poo", "pooh", "pook", "pums"
+		}
+	},
+	{
+		"D", {
+			"b", "bl", "br", "cl", "d", "f", "fl", "fr", "g", "gh", "gl", "gr",
+			"h", "j", "k", "kl", "m", "n", "p", "th", "w"
+		}
+	},
+	{
+		"d", {
+			"elch", "idiot", "ob", "og", "ok", "olph", "olt", "omph", "ong",
+			"onk", "oo", "oob", "oof", "oog", "ook", "ooz", "org", "ork", "orm",
+			"oron", "ub", "uck", "ug", "ulf", "ult", "um", "umb", "ump", "umph",
+			"un", "unb", "ung", "unk", "unph", "unt", "uzz"
+		}
+	}
+};
+
+
+Generator::Generator()
+{
+}
+
+
+Generator::Generator(const std::vector<Generator *> & generators_) :
+	generators(generators_)
+{
+}
+
+Generator::~Generator() {
+	for (auto g : generators) {
+		delete g;
+	}
+}
+
+
+size_t Generator::combinations()
+{
+	size_t total = 1;
+	for (auto g : generators) {
+		total *= g->combinations();
+	}
+	return total;
+}
+
+
+size_t Generator::min()
+{
+	size_t final = 0;
+	for (auto g : generators) {
+		final += g->min();
+	}
+	return final;
+}
+
+
+size_t Generator::max()
+{
+	size_t final = 0;
+	for (auto g : generators) {
+		final += g->max();
+	}
+	return final;
+}
+
+std::string Generator::toString() {
+	std::string str;
+	for (auto g : generators) {
+		str.append(g->toString());
+	}
+	return str;
+}
+
+
+void Generator::add(Generator *g)
+{
+	generators.push_back(g);
+}
+
+
+Random::Random()
+{
+}
+
+Random::Random(const std::vector<Generator *> & generators_) :
+	Generator(generators_)
+{
+}
+
+size_t Random::combinations()
+{
+	size_t total = 0;
+	for (auto g : generators) {
+		total += g->combinations();
+	}
+	return total ? total : 1;
+}
+
+size_t Random::min()
+{
+	size_t final = -1;
+	for (auto g : generators) {
+		size_t current = g->min();
+		if (current < final) {
+			final = current;
+		}
+	}
+	return final;
+}
+
+size_t Random::max()
+{
+	size_t final = 0;
+	for (auto g : generators) {
+		size_t current = g->max();
+		if (current > final) {
+			final = current;
+		}
+	}
+	return final;
+}
+
+
+std::string Random::toString()
+{
+	if (!generators.size()) {
+		return "";
+	}
+	std::uniform_real_distribution<double> distribution(0, generators.size() - 1);
+	int rnd = distribution(rng) + 0.5;
+	return generators[rnd]->toString();
+}
+
+
+Sequence::Sequence()
+{
+}
+
+Sequence::Sequence(const std::vector<Generator *> & generators_) :
+	Generator(generators_)
+{
+}
+
+Literal::Literal(const std::string &value_) :
+	value(value_)
+{
+}
+
+size_t Literal::combinations()
+{
+	return 1;
+}
+size_t Literal::min()
+{
+	return value.size();
+}
+size_t Literal::max()
+{
+	return value.size();
+}
+
+std::string Literal::toString()
+{
+	return value;
+}
+
+Reverser::Reverser(Generator *generator_) :
+	Generator(std::vector<Generator *>({generator_}))
+{
+}
+
+std::string Reverser::toString()
+{
+	std::string str(Generator::toString());
+	std::reverse(str.begin(), str.end());
+	return str;
+}
+
+Capitalizer::Capitalizer(Generator *generator_) :
+	Generator(std::vector<Generator *>({generator_}))
+{
+}
+
+std::string Capitalizer::toString()
+{
+	std::string str(Generator::toString());
+	str[0] = toupper(str[0]);
+	return str;
+}
+
+
+Generator::Generator(const std::string &pattern) {
+	Group *top;
+	Generator *last;
+
+	std::stack<Group *> stack;
+
+	stack.push(new GroupSymbol());
+
+	for (auto c : pattern) {
+		top = stack.top();
+		switch (c) {
+			case '<':
+				stack.push(new GroupSymbol());
+				break;
+			case '(':
+				stack.push(new GroupLiteral());
+				break;
+			case '>':
+			case ')':
+				if (stack.size() == 1) {
+					throw std::invalid_argument("Unbalanced brackets");
+				} else if (c == '>' && top->type != group_types::symbol) {
+					throw std::invalid_argument("Unexpected '>' in pattern");
+				} else if (c == ')' && top->type != group_types::literal) {
+					throw std::invalid_argument("Unexpected ')' in pattern");
+				}
+
+				last = top->emit();
+				stack.pop();
+				delete top;
+
+				top = stack.top();
+				top->add(last);
+				break;
+			case '|':
+				top->split();
+				break;
+			case '!':
+				if (top->type == group_types::symbol) {
+					top->wrap(wrappers::capitalizer);
+				} else {
+					top->add(c);
+				}
+				break;
+			case '~':
+				if (top->type == group_types::symbol) {
+					top->wrap(wrappers::reverser);
+				} else {
+					top->add(c);
+				}
+				break;
+			default:
+				top->add(c);
+				break;
+		}
+	}
+
+	if (stack.size() != 1) {
+		throw std::invalid_argument("Missing closing bracket");
+	}
+
+	top = stack.top();
+	add(top->emit());
+
+	while (!stack.empty()) {
+		delete stack.top();
+		stack.pop();
+	}
+}
+
+
+Generator::Group::Group(group_types_t type_) :
+	type(type_)
+{
+}
+
+void Generator::Group::add(Generator *g)
+{
+	while (!wrappers.empty()) {
+		switch (wrappers.top()) {
+			case reverser:
+				g = new Reverser(g);
+				break;
+			case capitalizer:
+				g = new Capitalizer(g);
+				break;
+		}
+		wrappers.pop();
+	}
+	if (set.size() == 0) {
+		set.push_back(new Sequence());
+	}
+	set.back()->add(g);
+}
+
+void Generator::Group::add(char c)
+{
+	std::string value(&c, 1);
+	Generator * g = new Random();
+	g->add(new Literal(value));
+	Group::add(g);
+}
+
+Generator * Generator::Group::emit()
+{
+	switch(set.size()) {
+		case 0:
+			return new Literal("");
+		case 1:
+			return *(set.begin());
+		default:
+			return new Random(set);
+	}
+}
+
+void Generator::Group::split()
+{
+	if (set.size() == 0) {
+		set.push_back(new Sequence());
+	}
+	set.push_back(new Sequence());
+}
+
+void Generator::Group::wrap(wrappers_t type)
+{
+	wrappers.push(type);
+}
+
+Generator::GroupSymbol::GroupSymbol() :
+	Group(group_types::symbol)
+{
+}
+
+void Generator::GroupSymbol::add(char c)
+{
+	std::string value(&c, 1);
+	Generator * g = new Random();
+	try {
+		for (auto s : Generator::symbols.at(value)) {
+			g->add(new Literal(s));
+		}
+	} catch (std::out_of_range) {
+		g->add(new Literal(value));
+	}
+	Group::add(g);
+}
+
+Generator::GroupLiteral::GroupLiteral() :
+	Group(group_types::literal)
+{
+}
---
+++ src/namegen.h
@@ -0,0 +1,270 @@
+/**
+ * Copyright (C) 2015 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * @file A fantasy name generator library.
+ * @version 1.0.0
+ * @license MIT License
+ * @author German M. Bravo (Kronuz)
+ *
+ * This library is designed after the RinkWorks Fantasy Name Generator.
+ * @see http://www.rinkworks.com/namegen/
+ *
+ * @example
+ * NameGen::Generator generator("sV'i");
+ * generator.toString();  // Emits a new name on each call
+ * // => "entheu'loaf"
+ *
+ * ## Pattern Syntax
+ *
+ *   The compile() function creates a name generator based on an input
+ * pattern. The letters s, v, V, c, B, C, i, m, M, D, and d represent
+ * different types of random replacements. Everything else is emitted
+ * literally.
+ *
+ *   s - generic syllable
+ *   v - vowel
+ *   V - vowel or vowel combination
+ *   c - consonant
+ *   B - consonant or consonant combination suitable for beginning a word
+ *   C - consonant or consonant combination suitable anywhere in a word
+ *   i - insult
+ *   m - mushy name
+ *   M - mushy name ending
+ *   D - consonant suited for a stupid person's name
+ *   d - syllable suited for a stupid person's name (begins with a vowel)
+ *
+ *   All characters between parenthesis () are emitted literally. For
+ * example, the pattern "s(dim)", emits a random generic syllable
+ * followed by "dim".
+ *
+ *   Characters between angle brackets <> emit patterns from the table
+ * above. Imagine the entire pattern is wrapped in one of these.
+ *
+ *   In both types of groupings, a vertical bar | denotes a random
+ * choice. Empty groups are allowed. For example, "(foo|bar)" emits
+ * either "foo" or "bar". The pattern "<c|v|>" emits a constant,
+ * vowel, or nothing at all.
+ *
+ *   An exclamation point ! means to capitalize the component that
+ * follows it. For example, "!(foo)" will emit "Foo" and "v!s" will
+ * emit a lowercase vowel followed by a capitalized syllable, like
+ * "eRod".
+ *
+ *   A tilde ~ means to reverse the letters of the component that
+ * follows it. For example, "~(foo)" will emit "oof". To reverse an
+ * entire template, wrap it in brackets. For example, to reverse
+ * "sV'i" as a whole use "~<sV'i>". The template "~sV'i" will only
+ * reverse the initial syllable.
+ *
+ * ## Internals
+ *
+ *   A name generator is anything with a toString() method, including,
+ * importantly, strings themselves. The generator constructors
+ * (Random, Sequence) perform additional optimizations when *not* used
+ * with the `new` keyword: they may pass through a provided generator,
+ * combine provided generators, or even return a simple string.
+ *
+ *   New pattern symbols added to Generator.symbols will automatically
+ * be used by the compiler.
+ */
+
+#include <string>
+#include <memory>
+#include <stack>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <stdexcept>
+#include <algorithm>
+
+namespace NameGen {
+
+// Japanese Names (Constrained)
+#define JAPANESE_NAMES_CONSTRAINED "(aka|aki|bashi|gawa|kawa|furu|fuku|fuji|hana|hara|haru|hashi|hira|hon|hoshi|ichi|iwa|kami|kawa|ki|kita|kuchi|kuro|marui|matsu|miya|mori|moto|mura|nabe|naka|nishi|no|da|ta|o|oo|oka|saka|saki|sawa|shita|shima|i|suzu|taka|take|to|toku|toyo|ue|wa|wara|wata|yama|yoshi|kei|ko|zawa|zen|sen|ao|gin|kin|ken|shiro|zaki|yuki|asa)(||||||||||bashi|gawa|kawa|furu|fuku|fuji|hana|hara|haru|hashi|hira|hon|hoshi|chi|wa|ka|kami|kawa|ki|kita|kuchi|kuro|marui|matsu|miya|mori|moto|mura|nabe|naka|nishi|no|da|ta|o|oo|oka|saka|saki|sawa|shita|shima|suzu|taka|take|to|toku|toyo|ue|wa|wara|wata|yama|yoshi|kei|ko|zawa|zen|sen|ao|gin|kin|ken|shiro|zaki|yuki|sa)"
+
+// Japanese Names (Diverse)
+#define JAPANESE_NAMES_DIVERSE "(a|i|u|e|o|||||)(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|ro|ro|wa|wa|wa|wa|wo|wo)(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|ro|ro|wa|wa|wa|wa|wo|wo)(|(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|ro|ro|wa|wa|wa|wa|wo|wo)|(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|ro|ro|wa|wa|wa|wa|wo|wo)(|(ka|ki|ki|ku|ku|ke|ke|ko|ko|sa|sa|sa|shi|shi|shi|su|su|se|so|ta|ta|chi|chi|tsu|te|to|na|ni|ni|nu|nu|ne|no|no|ha|hi|fu|fu|he|ho|ma|ma|ma|mi|mi|mi|mu|mu|mu|mu|me|mo|mo|mo|ya|yu|yu|yu|yo|ra|ra|ra|ri|ru|ru|ru|re|ro|ro|ro|wa|wa|wa|wa|wo|wo)))(|||n)"
+
+// Chinese Names
+#define CHINESE_NAMES "(zh|x|q|sh|h)(ao|ian|uo|ou|ia)(|(l|w|c|p|b|m)(ao|ian|uo|ou|ia)(|n)|-(l|w|c|p|b|m)(ao|ian|uo|ou|ia)(|(d|j|q|l)(a|ai|iu|ao|i)))"
+
+// Greek Names
+#define GREEK_NAMES "<s<v|V>(tia)|s<v|V>(os)|B<v|V>c(ios)|B<v|V><c|C>v(ios|os)>"
+
+// Hawaiian Names (1)
+#define HAWAIIAN_NAMES_1 "((h|k|l|m|n|p|w|')|)(a|e|i|o|u)((h|k|l|m|n|p|w|')|)(a|e|i|o|u)(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)(((h|k|l|m|n|p|w|')|)(a|e|i|o|u)|)"
+
+// Hawaiian Names (2)
+#define HAWAIIAN_NAMES_2 "((h|k|l|m|n|p|w|)(a|e|i|o|u|a'|e'|i'|o'|u'|ae|ai|ao|au|oi|ou|eu|ei)(k|l|m|n|p|)|)(h|k|l|m|n|p|w|)(a|e|i|o|u|a'|e'|i'|o'|u'|ae|ai|ao|au|oi|ou|eu|ei)(k|l|m|n|p|)"
+
+// Old Latin Place Names
+#define OLD_LATIN_PLACE_NAMES "sv(nia|lia|cia|sia)"
+
+// Dragons (Pern)
+#define DRAGONS_PERN "<<s|ss>|<VC|vC|B|BVs|Vs>><v|V|v|<v(l|n|r)|vc>>(th)"
+
+// Dragon Riders
+#define DRAGON_RIDERS "c'<s|cvc>"
+
+// Pokemon
+#define POKEMON "<i|s>v(mon|chu|zard|rtle)"
+
+// Fantasy (Vowels, R, etc.)
+#define FANTASY_VOWELS_R "(|(<B>|s|h|ty|ph|r))(i|ae|ya|ae|eu|ia|i|eo|ai|a)(lo|la|sri|da|dai|the|sty|lae|due|li|lly|ri|na|ral|sur|rith)(|(su|nu|sti|llo|ria|))(|(n|ra|p|m|lis|cal|deu|dil|suir|phos|ru|dru|rin|raap|rgue))"
+
+// Fantasy (S, A, etc.)
+#define FANTASY_S_A "(cham|chan|jisk|lis|frich|isk|lass|mind|sond|sund|ass|chad|lirt|und|mar|lis|il|<BVC>)(jask|ast|ista|adar|irra|im|ossa|assa|osia|ilsa|<vCv>)(|(an|ya|la|sta|sda|sya|st|nya))"
+
+// Fantasy (H, L, etc.)
+#define FANTASY_H_L "(ch|ch't|sh|cal|val|ell|har|shar|shal|rel|laen|ral|jh't|alr|ch|ch't|av)(|(is|al|ow|ish|ul|el|ar|iel))(aren|aeish|aith|even|adur|ulash|alith|atar|aia|erin|aera|ael|ira|iel|ahur|ishul)"
+
+// Fantasy (N, L, etc.)
+#define FANTASY_N_L "(ethr|qil|mal|er|eal|far|fil|fir|ing|ind|il|lam|quel|quar|quan|qar|pal|mal|yar|um|ard|enn|ey)(|(<vc>|on|us|un|ar|as|en|ir|ur|at|ol|al|an))(uard|wen|arn|on|il|ie|on|iel|rion|rian|an|ista|rion|rian|cil|mol|yon)"
+
+// Fantasy (K, N, etc.)
+#define FANTASY_K_N "(taith|kach|chak|kank|kjar|rak|kan|kaj|tach|rskal|kjol|jok|jor|jad|kot|kon|knir|kror|kol|tul|rhaok|rhak|krol|jan|kag|ryr)(<vc>|in|or|an|ar|och|un|mar|yk|ja|arn|ir|ros|ror)(|(mund|ard|arn|karr|chim|kos|rir|arl|kni|var|an|in|ir|a|i|as))"
+
+// Fantasy (J, G, Z, etc.)
+#define FANTASY_J_G_Z "(aj|ch|etz|etzl|tz|kal|gahn|kab|aj|izl|ts|jaj|lan|kach|chaj|qaq|jol|ix|az|biq|nam)(|(<vc>|aw|al|yes|il|ay|en|tom||oj|im|ol|aj|an|as))(aj|am|al|aqa|ende|elja|ich|ak|ix|in|ak|al|il|ek|ij|os|al|im)"
+
+// Fantasy (K, J, Y, etc.)
+#define FANTASY_K_J_Y "(yi|shu|a|be|na|chi|cha|cho|ksa|yi|shu)(th|dd|jj|sh|rr|mk|n|rk|y|jj|th)(us|ash|eni|akra|nai|ral|ect|are|el|urru|aja|al|uz|ict|arja|ichi|ural|iru|aki|esh)"
+
+// Fantasy (S, E, etc.)
+#define FANTASY_S_E "(syth|sith|srr|sen|yth|ssen|then|fen|ssth|kel|syn|est|bess|inth|nen|tin|cor|sv|iss|ith|sen|slar|ssil|sthen|svis|s|ss|s|ss)(|(tys|eus|yn|of|es|en|ath|elth|al|ell|ka|ith|yrrl|is|isl|yr|ast|iy))(us|yn|en|ens|ra|rg|le|en|ith|ast|zon|in|yn|ys)"
+
+
+class Generator
+{
+	typedef enum wrappers {
+		capitalizer,
+		reverser
+	} wrappers_t;
+
+	typedef enum group_types {
+		symbol,
+		literal
+	} group_types_t;
+
+
+	class Group {
+		std::stack<wrappers_t> wrappers;
+		std::vector<Generator *> set;
+
+	public:
+		group_types_t type;
+
+		Group(group_types_t type_);
+
+		virtual void add(char c);
+		virtual void add(Generator *g);
+		virtual Generator * emit();
+		virtual void split();
+		virtual void wrap(wrappers_t type);
+	};
+
+
+	class GroupSymbol : public Group {
+	public:
+		GroupSymbol();
+
+		virtual void add(char c);
+	};
+
+
+	class GroupLiteral : public Group {
+	public:
+		GroupLiteral();
+	};
+
+protected:
+	std::vector<Generator *> generators;
+
+public:
+	static const std::unordered_map<std::string, const std::vector<const std::string> > symbols;
+
+	Generator();
+	Generator(const std::string &pattern);
+	Generator(const std::vector<Generator *> & generators_);
+
+	virtual ~Generator();
+
+	virtual size_t combinations();
+	virtual size_t min();
+	virtual size_t max();
+	virtual std::string toString();
+
+	void add(Generator *g);
+};
+
+
+class Random : public Generator
+{
+public:
+	Random();
+	Random(const std::vector<Generator *> & generators_);
+
+	size_t combinations();
+	size_t min();
+	size_t max();
+	std::string toString();
+};
+
+
+class Sequence : public Generator
+{
+public:
+	Sequence();
+	Sequence(const std::vector<Generator *> & generators_);
+};
+
+
+class Literal : public Generator
+{
+	std::string value;
+
+public:
+	Literal(const std::string &value_);
+
+	size_t combinations();
+	size_t min();
+	size_t max();
+	std::string toString();
+};
+
+
+class Reverser : public Generator {
+public:
+	Reverser(Generator *generator_);
+
+	std::string toString();
+};
+
+
+class Capitalizer : public Generator
+{
+public:
+	Capitalizer(Generator *generator_);
+
+	std::string toString();
+};
+
+};
