e9857be1352bfd8ba3b2d9684f6a653afec96a2c
German M. Bravo <german.mb@deipi.com>
2015-09-21 19:06:35 -0500

    Haystack backend and Python client updated

--- contrib/python/haystack/backend.py
+++ contrib/python/haystack/backend.py
@@ -1,6 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015 deipi.com LLC and contributors. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
 from __future__ import absolute_import, unicode_literals
 
-from contrib.haystack.client import Xapiand
+import json
 
 from haystack import connections
 from haystack.constants import ID, DJANGO_CT, DJANGO_ID
@@ -9,11 +31,12 @@ from haystack.models import SearchResult
 from haystack.utils import get_identifier, get_model_ct
 
 from django.conf import settings
-from django.utils import six
 from django.utils.importlib import import_module
 from django.core.exceptions import ImproperlyConfigured
+from django.utils import six
 
-import json
+from .client import Xapiand
+
 
 DOCUMENT_TAGS_FIELD = 'tags'
 DOCUMENT_AC_FIELD = 'ac'
