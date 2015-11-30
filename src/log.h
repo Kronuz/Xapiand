6873533c55977b86c17ac718e5cff9ccca247de6
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2015-11-30 00:35:03 -0600

    Redefinition of ANSI color

--- src/log.h
+++ src/log.h
@@ -98,7 +98,6 @@ class LogThread {
 #define RED "\033[0;31m"
 #define GREEN "\033[0;32m"
 #define YELLOW "\033[0;33m"
-#define YELLOW "\033[0;33m"
 #define BLUE "\033[0;34m"
 #define MAGENTA "\033[0;35m"
 #define CYAN "\033[0;36m"
