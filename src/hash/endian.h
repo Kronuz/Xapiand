b164a508b6015fe61d0dc84d72d29232eeac8008
JoseMariaVR <chem_valencia@hotmail.com>
2015-12-09 17:55:20 -0600

    Defines macros for endianess (needed in sha256, md5 and serialise)

---
+++ src/hash/endian.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2015 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+
+/*
+ * Define macro for endianess.
+ */
+
+
+#if defined(__linux__)
+	#include <endian.h>
+#elif defined(__APPLE__)
+	#include <libkern/OSByteOrder.h>
+	#define __BYTE_ORDER    BYTE_ORDER
+	#define __BIG_ENDIAN    BIG_ENDIAN
+	#define __LITTLE_ENDIAN LITTLE_ENDIAN
+	#define __PDP_ENDIAN    PDP_ENDIAN
+#elif defined(__FreeBSD__)
+	#include <sys/endian.h>
+	#define __BYTE_ORDER    _BYTE_ORDER
+	#define __BIG_ENDIAN    _BIG_ENDIAN
+	#define __LITTLE_ENDIAN _LITTLE_ENDIAN
+	#define __PDP_ENDIAN    _PDP_ENDIAN
+#elif defined(_WIN16) || defined(_WIN32) || defined(_WIN64)
+	#include <winsock2.h>
+	#include <sys/param.h>
+	#define __BYTE_ORDER    BYTE_ORDER
+	#define __BIG_ENDIAN    BIG_ENDIAN
+	#define __LITTLE_ENDIAN LITTLE_ENDIAN
+	#define __PDP_ENDIAN    PDP_ENDIAN
+#endif
