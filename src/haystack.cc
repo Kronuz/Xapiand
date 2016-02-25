e8ab31ebed67d1b77c83ee3ec0d4577195bd9ec0
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2016-02-24 18:02:55 -0600

    Update the haystack file in project

---
+++ src/haystack.cc
@@ -0,0 +1,9 @@
+//
+//  haystack.cpp
+//  Xapiand
+//
+//  Created by Jose Madrigal on 24/02/16.
+//  Copyright © 2016 Germán M. Bravo. All rights reserved.
+//
+
+#include "haystack.hpp"
---
+++ src/haystack.h
@@ -0,0 +1,14 @@
+//
+//  haystack.hpp
+//  Xapiand
+//
+//  Created by Jose Madrigal on 24/02/16.
+//  Copyright © 2016 Germán M. Bravo. All rights reserved.
+//
+
+#ifndef haystack_hpp
+#define haystack_hpp
+
+#include <stdio.h>
+
+#endif /* haystack_hpp */
