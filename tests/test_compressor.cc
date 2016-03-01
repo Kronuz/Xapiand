78fc4dc54c7314d8c4afa25c6398e44256521778
JoseMariaVR <chem_valencia@hotmail.com>
2016-03-01 16:16:42 -0600

    Close files, throw and catch exceptions in test compressor

--- tests/test_compressor.cc
+++ tests/test_compressor.cc
@@ -31,7 +31,7 @@
 std::string read_file(const std::string& filename) {
 	int fd = io::open(filename.c_str(), O_RDONLY, 0644);
 	if unlikely(fd < 0) {
-		throw std::exception(/* Error in read volumen */);
+		throw MSG_Error("Cannot open file: %s", filename.c_str());
 	}
 
 	std::string ret;
@@ -43,8 +43,10 @@ std::string read_file(const std::string& filename) {
 	}
 
 	if unlikely(r < 0) {
-		throw std::exception(/* Error in read volumen */);
+		throw MSG_Error("IO error: read");
 	}
+
+	io::close(fd);
 
 	return ret;
 }
@@ -53,31 +55,42 @@ std::string read_file(const std::string& filename) {
 int compare(const std::string& filename1, const std::string& filename2) {
 	int fd1 = io::open(filename1.c_str(), O_RDONLY, 0644);
 	if unlikely(fd1 < 0) {
-		throw std::exception(/* Error in read volumen */);
+		throw MSG_Error("Cannot open file: %s", filename1.c_str());
 	}
 
 	int fd2 = io::open(filename2.c_str(), O_RDONLY, 0644);
 	if unlikely(fd2 < 0) {
-		throw std::exception(/* Error in read volumen */);
+		io::close(fd1);
+		throw MSG_Error("Cannot open file: %s", filename2.c_str());
 	}
 
 	if (lseek(fd1, 0, SEEK_END) != lseek(fd2, 0, SEEK_END)) {
+		io::close(fd1);
+		io::close(fd2);
 		L_ERR(nullptr, "%s and %s are not equal (different sizes)\n", filename1.c_str(), filename2.c_str());
 		return 1;
 	}
 
-	lseek(fd1, 0, SEEK_SET);
-	lseek(fd2, 0, SEEK_SET);
+	if (lseek(fd1, 0, SEEK_SET) != 0 && lseek(fd2, 0, SEEK_SET) != 0) {
+		io::close(fd1);
+		io::close(fd2);
+		throw MSG_Error("IO error: lseek");
+	}
 
 	size_t r1 = 0, r2 = 0;
 	char buf1[1024];
 	char buf2[1024];
 	while ((r1 = io::read(fd1, buf1, sizeof(buf1))) > 0 && (r2 = io::read(fd2, buf2, sizeof(buf2))) > 0) {
 		if (r1 != r2 || std::string(buf1, r1) != std::string(buf2, r2)) {
+			io::close(fd1);
+			io::close(fd2);
 			L_ERR(nullptr, "%s and %s are not equal\n", filename1.c_str(), filename2.c_str());
 			return 1;
 		}
 	}
+
+	io::close(fd1);
+	io::close(fd2);
 
 	return 0;
 }
@@ -88,24 +101,22 @@ int test_Compress_Decompress_Data(const std::string& orig_file, const std::strin
 		// Compress Data
 		{
 			std::string _data = read_file(orig_file);
-			L(nullptr, "Original Data Size: %zu\n", _data.size());
+			L_ERR(nullptr, "Original Data Size: %zu\n", _data.size());
 
 			int fd = io::open(cmp_file.c_str(), O_RDWR | O_CREAT | O_DSYNC, 0644);
 			if unlikely(fd < 0) {
-				L_ERR(nullptr, "Error in read volumen\n");
-				return 1;
+				throw MSG_Error("Cannot open file: %s", cmp_file.c_str());
 			}
 
 			LZ4CompressData lz4(_data.c_str(), _data.size());
 			auto it = lz4.begin();
 			while (it) {
 				if (io::write(fd, it->c_str(), it.size()) != static_cast<ssize_t>(it.size())) {
-					L_ERR(nullptr, "Error in write volumen\n");
-					return 1;
+					throw MSG_Error("IO error: write");
 				}
 				++it;
 			}
-			L(nullptr, "Size compress: %zu\n", lz4.size());
+			L_ERR(nullptr, "Size compress: %zu\n", lz4.size());
 			io::close(fd);
 		}
 
@@ -114,24 +125,25 @@ int test_Compress_Decompress_Data(const std::string& orig_file, const std::strin
 			std::string cmp_data = read_file(cmp_file.c_str());
 			int fd = io::open(dec_file.c_str(), O_RDWR | O_CREAT | O_DSYNC, 0644);
 			if unlikely(fd < 0) {
-				L_ERR(nullptr, "Error in read volumen\n");
-				return 1;
+				throw MSG_Error("Cannot open file: %s", dec_file.c_str());
 			}
 
 			LZ4DecompressData dec_lz4(cmp_data.data(), cmp_data.size());
 			auto dec_it = dec_lz4.begin();
 			while (dec_it) {
 				if (io::write(fd, dec_it->c_str(), dec_it.size()) != static_cast<ssize_t>(dec_it.size())) {
-					L_ERR(nullptr, "Error in write volumen\n");
-					return 1;
+					throw MSG_Error("IO error: write");
 				}
 				++dec_it;
 			}
-			L(nullptr, "Size decompress: %zu\n", dec_lz4.size());
+			L_ERR(nullptr, "Size decompress: %zu\n", dec_lz4.size());
 			io::close(fd);
 		}
 
 		return compare(orig_file, dec_file);
+	} catch (const Error& er) {
+		L_ERR(nullptr, "%s\n", er.get_context());
+		return 1;
 	} catch (const std::exception& er) {
 		L_ERR(nullptr, "%s\n", er.what());
 		return 1;
@@ -145,20 +157,18 @@ int test_Compress_Decompress_File(const std::string& orig_file, const std::strin
 		{
 			int fd = io::open(cmp_file.c_str(), O_RDWR | O_CREAT | O_DSYNC, 0644);
 			if unlikely(fd < 0) {
-				L_ERR(nullptr, "Error in read volumen\n");
-				return 1;
+				throw MSG_Error("Cannot open file: %s", cmp_file.c_str());
 			}
 
 			LZ4CompressFile lz4(orig_file);
 			auto it = lz4.begin();
 			while (it) {
 				if (io::write(fd, it->c_str(), it.size()) != static_cast<ssize_t>(it.size())) {
-					L_ERR(nullptr, "Error in write volumen\n");
-					return 1;
+					throw MSG_Error("IO error: write");
 				}
 				++it;
 			}
-			L(nullptr, "Size compress: %zu\n", lz4.size());
+			L_ERR(nullptr, "Size compress: %zu\n", lz4.size());
 			io::close(fd);
 		}
 
@@ -166,24 +176,25 @@ int test_Compress_Decompress_File(const std::string& orig_file, const std::strin
 		{
 			int fd = io::open(dec_file.c_str(), O_RDWR | O_CREAT | O_DSYNC, 0644);
 			if unlikely(fd < 0) {
-				L_ERR(nullptr, "Error in read volumen\n");
-				return 1;
+				throw MSG_Error("Cannot open file: %s", dec_file.c_str());
 			}
 
 			LZ4DecompressFile dec_lz4(cmp_file);
 			auto dec_it = dec_lz4.begin();
 			while (dec_it) {
 				if (io::write(fd, dec_it->c_str(), dec_it.size()) != static_cast<ssize_t>(dec_it.size())) {
-					L_ERR(nullptr, "Error in write volumen\n");
-					return 1;
+					throw MSG_Error("IO error: write");
 				}
 				++dec_it;
 			}
-			L(nullptr, "Size decompress: %zu\n", dec_lz4.size());
+			L_ERR(nullptr, "Size decompress: %zu\n", dec_lz4.size());
 			io::close(fd);
 		}
 
 		return compare(orig_file, dec_file);
+	} catch (const Error& er) {
+		L_ERR(nullptr, "%s\n", er.get_context());
+		return 1;
 	} catch (const std::exception& er) {
 		L_ERR(nullptr, "%s\n", er.what());
 		return 1;
@@ -191,113 +202,66 @@ int test_Compress_Decompress_File(const std::string& orig_file, const std::strin
 }
 
 
+static const std::string cmp = "examples/compressor/compress.lz4";
+static const std::string dec = "examples/compressor/decompress.dec";
+
+
+static const std::vector<std::string> small_files({
+	"examples/compressor/Small_File1.txt",
+	"examples/compressor/Small_File2.txt",
+	"examples/compressor/Small_File3.txt",
+	"examples/compressor/Small_File4.txt"
+});
+
+
+static const std::vector<std::string> big_files({
+	"examples/compressor/Big_File1.jpg",
+	"examples/compressor/Big_File2.pdf",
+	"examples/compressor/Big_File3.pdf",
+	"examples/compressor/Big_File4.pdf",
+	"examples/compressor/Big_File5.pdf"
+});
+
+
 int test_small_datas() {
-	std::string cmp = "examples/compressor/compress.lz4";
-	std::string dec = "examples/compressor/decompress.dec";
-
-	std::string orig = "examples/compressor/Small_File1.txt";
-	int res = test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Small_File2.txt";
-	res += test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Small_File3.txt";
-	res += test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
+	int res = 0;
+	for (const auto& file : small_files) {
+		res += test_Compress_Decompress_Data(file, cmp, dec);
+		unlink(cmp.c_str());
+		unlink(dec.c_str());
+	}
 	return res;
 }
 
 
 int test_big_datas() {
-	std::string cmp = "examples/compressor/compress.lz4";
-	std::string dec = "examples/compressor/decompress.dec";
-
-	std::string orig = "examples/compressor/Big_File1.jpg";
-	int res = test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File2.pdf";
-	res += test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File3.pdf";
-	res += test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File4.pdf";
-	res += test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File5.pdf";
-	res += test_Compress_Decompress_Data(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
+	int res = 0;
+	for (const auto& file : big_files) {
+		res += test_Compress_Decompress_Data(file, cmp, dec);
+		unlink(cmp.c_str());
+		unlink(dec.c_str());
+	}
 	return res;
 }
 
 
 int test_small_files() {
-	std::string cmp = "examples/compressor/compress.lz4";
-	std::string dec = "examples/compressor/decompress.dec";
-
-	std::string orig = "examples/compressor/Small_File1.txt";
-	int res = test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Small_File2.txt";
-	res += test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Small_File3.txt";
-	res += test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
+	int res = 0;
+	for (const auto& file : small_files) {
+		res += test_Compress_Decompress_File(file, cmp, dec);
+		unlink(cmp.c_str());
+		unlink(dec.c_str());
+	}
 	return res;
 }
 
 
 int test_big_files() {
-	std::string cmp = "examples/compressor/compress.lz4";
-	std::string dec = "examples/compressor/decompress.dec";
-
-	std::string orig = "examples/compressor/Big_File1.jpg";
-	int res = test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File2.pdf";
-	res += test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File3.pdf";
-	res += test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File4.pdf";
-	res += test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
-	orig = "examples/compressor/Big_File5.pdf";
-	res += test_Compress_Decompress_File(orig, cmp, dec);
-	unlink(cmp.c_str());
-	unlink(dec.c_str());
-
+	int res = 0;
+	for (const auto& file : big_files) {
+		res += test_Compress_Decompress_File(file, cmp, dec);
+		unlink(cmp.c_str());
+		unlink(dec.c_str());
+	}
 	return res;
 }
