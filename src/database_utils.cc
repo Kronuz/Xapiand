e3a35705515d12e8e2a0083bdc03ee3a3a96b03c
German M. Bravo <german.mb@deipi.com>
2016-03-08 08:20:49 -0600

    Better JSON parse error

--- src/database_utils.cc
+++ src/database_utils.cc
@@ -166,7 +166,7 @@ MIMEType get_mimetype(const std::string& type) {
 void json_load(rapidjson::Document& doc, const std::string& str) {
 	rapidjson::ParseResult parse_done = doc.Parse(str.data());
 	if (!parse_done) {
-		throw MSG_ClientError("JSON parse error: %s (%u)", GetParseError_En(parse_done.Code()), parse_done.Offset());
+		throw MSG_ClientError("JSON parse error at position %u: %s", parse_done.Offset(), GetParseError_En(parse_done.Code()));
 	}
 }
 
