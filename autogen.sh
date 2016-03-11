5a7815c9642675c069bf844b451cd456abaace36
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2016-03-11 11:57:33 -0800

    Temporary solution to find the xapian-#.#.m4 macro, because linux aclocal only search macro files in the aclocal installation configuration path which default is /usr/share/aclocal and sometimes xapian is installed in prefix=/user/local/ and the xapian macro is in prefix/share/aclocal and in linux seems there is not have a dirlist for search macro, and the options -I with the custom path seems not work for us

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+if [ -d "/usr/local/share/aclocal/" ]; then
+	# Try to get xapian-1.3.m4 in case xapian has been installed in /usr/local/
+	cp /usr/local/share/aclocal/xapian-1.3.m4 m4/xapian-1.3.m4
+fi
+
 aclocal -I m4 --install
 autoreconf --force --install --verbose
