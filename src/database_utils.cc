c409765553467bfd556f750a1ff5e138393e9916
JoseMariaVR <chem_valencia@hotmail.com>
2016-03-30 11:36:09 -0600

    Function is_valid was added in database_utils

--- src/database_utils.cc
+++ src/database_utils.cc
@@ -93,8 +93,8 @@ long long read_mastery(const std::string& dir, bool force) {
 }
 
 
-bool is_reserved(const std::string& word) {
-	return word.front() == '_' || word.back() == '_' || word.find(DB_OFFSPRING_UNION) != std::string::npos;
+bool is_valid(const std::string& word) {
+	return word.front() != '_' && word.back() != '_' && word.find(DB_OFFSPRING_UNION) == std::string::npos;
 }
 
 
