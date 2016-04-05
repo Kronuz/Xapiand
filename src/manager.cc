aa60316f41e05a5036268aa9e738b8f5c1750d54
German M. Bravo <german.mb@deipi.com>
2016-04-05 13:26:17 -0500

    _stats prints running tasks

--- src/manager.cc
+++ src/manager.cc
@@ -792,31 +792,30 @@ XapiandManager::server_status(MsgPack&& stats)
 	std::lock_guard<std::mutex> lk(XapiandServer::static_mutex);
 	stats["connections"] = XapiandServer::total_clients.load();
 	stats["http_connections"] = XapiandServer::http_clients.load();
-	stats["max_connections"] = XapiandServer::max_total_clients.load();
-	stats["max_http_connections"] = XapiandServer::max_http_clients.load();
-
-	stats["workers_pool_size"] = thread_pool.threadpool_size();
-	stats["workers_enqueued_tasks"] = thread_pool.size();
-	stats["workers_tasks_running"] = thread_pool.running_task.load();
-
-	stats["servers_pool_size"] = server_pool.threadpool_size();
-	stats["servers_enqueued_tasks"] = server_pool.size();
-	stats["servers_tasks_running"] = server_pool.running_task.load();
-
-	stats["committers_pool_size"] = autocommit_pool.threadpool_size();
-	stats["committers_enqueued_tasks"] = autocommit_pool.size();
-	stats["committers_tasks_running"] = autocommit_pool.running_task.load();
-
-	stats["fsync_pool_size"] = asyncfsync_pool.threadpool_size();
-	stats["fsync_enqueued_tasks"] = asyncfsync_pool.size();
-	stats["fsync_tasks_running"] = asyncfsync_pool.running_task.load();
 #ifdef XAPIAND_CLUSTERING
 	if(!solo) {
 		stats["binary_connections"] = XapiandServer::binary_clients.load();
+	}
+#endif
+
+	stats["max_connections"] = XapiandServer::max_total_clients.load();
+	stats["max_http_connections"] = XapiandServer::max_http_clients.load();
+#ifdef XAPIAND_CLUSTERING
+	if(!solo) {
 		stats["max_binary_connections"] = XapiandServer::max_binary_clients.load();
-		stats["replicators_pool_size"] = replicator_pool.threadpool_size();
-		stats["replicators_enqueued_tasks"] = replicator_pool.size();
-		stats["replicators_tasks_running"] = replicator_pool.running_task.load();
+	}
+#endif
+
+	stats["worker_tasks_running"] = thread_pool.running_task.load();
+	stats["worker_tasks_enqueued"] = thread_pool.size();
+	stats["worker_tasks_pool_size"] = thread_pool.threadpool_size();
+
+	stats["servers_threads"] = server_pool.running_task.load();
+	stats["committers_threads"] = autocommit_pool.running_task.load();
+	stats["fsync_threads"] = asyncfsync_pool.running_task.load();
+#ifdef XAPIAND_CLUSTERING
+	if(!solo) {
+		stats["replicator_threads"] = replicator_pool.running_task.load();
 	}
 #endif
 }
