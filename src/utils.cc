2e79005fe16a230a356d49ee381fb8b4dabb41e8
JoseMariaVR <chem_valencia@hotmail.com>
2016-04-07 17:42:38 -0500

    Negative numbers is correct int

--- src/utils.cc
+++ src/utils.cc
@@ -1074,7 +1074,7 @@ int copy_file(const std::string& src, const std::string& dst, bool create, const
 int strict_stoi(const std::string& str) {
 	std::string::size_type sz;
 	int res = std::stoi(str, &sz);
-	if (res < 0 || sz != str.size()) {
+	if (sz != str.size()) {
 		throw std::invalid_argument("Can not convert value: " + str);
 	}
 	return res;
