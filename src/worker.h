cb6e7535144800c7862492c2109a85d7af269216
German M. Bravo <german.mb@deipi.com>
2016-04-12 10:02:02 -0500

    Worker: Dump_tree shows if loop is running

--- src/worker.h
+++ src/worker.h
@@ -178,7 +178,7 @@ public:
 		std::lock_guard<std::mutex> lk(_mtx);
 		std::string ret;
 		for (int l = 0; l < level; ++l) ret += "    ";
-		ret += __repr__() + " - " + std::to_string(shared_from_this().use_count()) + "\n";
+		ret += __repr__() + " - " + std::to_string(shared_from_this().use_count()) + (_running ? " in a running loop\n" : "\n");
 		for (const auto& c : _children) {
 			ret += c->dump_tree(level + 1);
 		}
