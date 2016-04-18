e355b1ab097310d73a511eeaeb7aced40e924edd
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2016-04-18 16:46:37 -0500

    Fix format for Copyright

--- src/generate_terms.cc
+++ src/generate_terms.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015, 2016 deipi.com LLC and contributors. All rights reserved.
+ * Copyright (C) 2015,2016 deipi.com LLC and contributors. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
