39a3c2b4457f3d5f8e67f8e5616adb7e093fdaa0
JoseMariaVR <chem_valencia@hotmail.com>
2016-07-29 17:30:51 -0500

    Add new soundexs in phonetic.h

--- src/phonetic.h
+++ src/phonetic.h
@@ -22,6 +22,8 @@
 
 #pragma once
 
-#include "phonetic/soundex.h"
 #include "phonetic/english_soundex.h"
+#include "phonetic/french_soundex.h"
+#include "phonetic/german_soundex.h"
+#include "phonetic/soundex.h"
 #include "phonetic/spanish_soundex.h"
