e7a37c2ac44ac9dd07cbaf35cb59390fa14c8d02
JoseMariaVR <chem_valencia@hotmail.com>
2016-08-18 14:13:24 -0500

    Using reserved word

--- src/client_http.cc
+++ src/client_http.cc
@@ -974,7 +974,7 @@ HttpClient::facets_view()
 			for (auto facet = spy.second->values_begin(); facet != facet_e; ++facet) {
 				auto field_t = db_handler.get_schema()->get_slot_field(spy.first);
 				MsgPack value = {
-					{ "_value", Unserialise::MsgPack(field_t.type, *facet) },
+					{ RESERVED_VALUE, Unserialise::MsgPack(field_t.type, *facet) },
 					{ "_termfreq", facet.get_termfreq() }
 				};
 				array.push_back(std::move(value));
