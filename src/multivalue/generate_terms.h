fd9e57b92fa87a88e18526d61ce04f915c28c6ab
JoseMariaVR <chem_valencia@hotmail.com>
2016-08-18 14:35:19 -0500

    Add headers functions for new functions in generate_terms.h

--- src/multivalue/generate_terms.h
+++ src/multivalue/generate_terms.h
@@ -23,18 +23,20 @@
 #pragma once
 
 #include "../database_utils.h"
+#include "../datetime.h"
 #include "../wkt_parser.h"
-#include "datetime.h"
 
 #include <unordered_set>
 #include <vector>
+#include <xapian.h>
 
 
-#define MAX_TERMS             50
-#define MAX_SERIALISE_LENGTH  18
+constexpr size_t MAX_TERMS            = 50;
+constexpr size_t MAX_SERIALISE_LENGTH = 18;
+constexpr size_t START_POS            = SIZE_BITS_ID - 4;
 
 
-// Returns the upper bound's size given the prefix's length, number of unions and union's length.
+// Returns the upper bound's length given the prefix's length, number of unions and union's length.
 inline constexpr size_t get_upper_bound(size_t length_prefix, size_t number_unions, size_t length_union) noexcept {
 	auto max_length_term = length_prefix + MAX_SERIALISE_LENGTH;
 	return (max_length_term + length_union) * number_unions + max_length_term;
@@ -42,18 +44,43 @@ inline constexpr size_t get_upper_bound(size_t length_prefix, size_t number_unio
 
 
 namespace GenerateTerms {
-	template <typename T, typename = std::enable_if_t<std::is_arithmetic<std::decay_t<T>>::value>>
-	std::pair<std::string, std::vector<std::string>> numeric(T start_, T end_, const std::vector<double>& accuracy, const std::vector<std::string>& acc_prefix) {
-		if (accuracy.empty() || end_ < start_) {
+
+	/*
+	 * Add generated terms by accuracy for field or global values in doc.
+	 */
+
+	void integer(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix, int64_t value);
+	void positive(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix, uint64_t value);
+	void date(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix, const Datetime::tm_t& tm);
+	void geo(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix, const RangeList& ranges);
+
+
+	/*
+	 * Add generated terms by accuracy for field and global values in doc.
+	 */
+
+	void integer(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix,
+		const std::vector<std::string>& acc_global_prefix, int64_t value);
+	void positive(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix,
+		const std::vector<std::string>& acc_global_prefix, uint64_t value);
+	void date(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix,
+		const std::vector<std::string>& acc_global_prefix, const Datetime::tm_t& tm);
+	void geo(Xapian::Document& doc, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix,
+		const std::vector<std::string>& acc_global_prefix, const RangeList& ranges);
+
+
+	/*
+	 * Generate terms for numerical ranges.
+	 */
+	template <typename T, typename = std::enable_if_t<std::is_integral<std::decay_t<T>>::value>>
+	std::pair<std::string, std::vector<std::string>> numeric(T start, T end, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix) {
+		if (accuracy.empty() || end < start) {
 			return std::make_pair(std::string(), std::vector<std::string>());
 		}
 
 		std::string result_terms;
 		std::vector<std::string> used_prefixes;
 		used_prefixes.reserve(2);
-
-		auto start = static_cast<int64_t>(start_);
-		auto end = static_cast<int64_t>(end_);
 
 		uint64_t size_r = end - start;
 
@@ -65,13 +92,10 @@ namespace GenerateTerms {
 
 		// If there is a upper or equal accuracy.
 		if (pos < len) {
-			auto up_acc = static_cast<int64_t>(accuracy[pos]);
-			auto up_start = start - (start % up_acc);
-			auto up_end = end - (end % up_acc);
-
+			auto up_acc = accuracy[pos];
+			T up_start = start - (start % up_acc);
+			T up_end = end - (end % up_acc);
 			std::string prefix_dot_up;
-			std::string prefix_dot_low;
-
 			prefix_dot_up.reserve(acc_prefix[pos].length() + 1);
 			prefix_dot_up.assign(acc_prefix[pos]).push_back(':');
 
@@ -79,12 +103,16 @@ namespace GenerateTerms {
 
 			if (pos > 0) {
 				--pos;
-				auto low_acc = static_cast<int64_t>(accuracy[pos]);
-				auto low_start = start - start % low_acc;
-				auto low_end = end - end % low_acc;
-				used_prefixes.push_back(acc_prefix[pos]);
+				auto low_acc = accuracy[pos];
+				T low_start = start - start % low_acc;
+				T low_end = end - end % low_acc;
+
+				std::string prefix_dot_low;
 				prefix_dot_low.reserve(acc_prefix[pos].length() + 1);
 				prefix_dot_low.assign(acc_prefix[pos]).push_back(':');
+
+				used_prefixes.push_back(acc_prefix[pos]);
+
 				if (up_start == up_end) {
 					size_t num_unions = (low_end - low_start) / low_acc;
 					if (num_unions == 0) {
@@ -144,13 +172,13 @@ namespace GenerateTerms {
 				result_terms.append(prefix_dot_up).append(to_query_string(up_end));
 			}
 		} else {
-			// If there is a lower accuracy.
+			// If only there is a lower accuracy.
 			if (pos > 0) {
 				--pos;
-				auto _acc = static_cast<int64_t>(accuracy[pos]);
-				start -= start % _acc;
-				end -= end % _acc;
-				size_t num_unions = (end - start) / _acc;
+				auto low_acc = accuracy[pos];
+				start -= start % low_acc;
+				end -= end % low_acc;
+				size_t num_unions = (end - start) / low_acc;
 				// If terms are not too many terms (num_unions + 1).
 				if (num_unions < MAX_TERMS) {
 					std::string prefix_dot, lower_terms;
@@ -163,7 +191,7 @@ namespace GenerateTerms {
 
 					while (start != end) {
 						lower_terms.append(prefix_dot).append(to_query_string(start)).append(" OR ");
-						start +=_acc;
+						start += low_acc;
 					}
 					lower_terms.append(prefix_dot).append(to_query_string(end));
 
@@ -181,7 +209,15 @@ namespace GenerateTerms {
 		return std::make_pair(std::move(result_terms), std::move(used_prefixes));
 	}
 
-	std::pair<std::string, std::vector<std::string>> date(double start_, double end_, const std::vector<double>& accuracy, const std::vector<std::string>& acc_prefix);
+	/*
+	 * Generate Terms for date ranges.
+	 */
+	std::pair<std::string, std::vector<std::string>> date(double start_, double end_, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix);
+
+	/*
+	 * Auxiliar functions for date ranges.
+	 */
+
 	std::string millennium(Datetime::tm_t& tm_s, Datetime::tm_t& tm_e, const std::string& prefix);
 	std::string century(Datetime::tm_t& tm_s, Datetime::tm_t& tm_e, const std::string& prefix);
 	std::string decade(Datetime::tm_t& tm_s, Datetime::tm_t& tm_e, const std::string& prefix);
@@ -198,5 +234,8 @@ namespace GenerateTerms {
 		return (year > 0) ? year : accuracy;
 	}
 
-	std::pair<std::string, std::unordered_set<std::string>> geo(const std::vector<range_t>& ranges, const std::vector<double>& accuracy, const std::vector<std::string>& acc_prefix);
+	/*
+	 * Generate ters for geospatial ranges.
+	 */
+	std::pair<std::string, std::unordered_set<std::string>> geo(const std::vector<range_t>& ranges, const std::vector<uint64_t>& accuracy, const std::vector<std::string>& acc_prefix);
 };
