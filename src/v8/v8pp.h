4164e9484bd58b7db6749efe757ec6b8af7514cb
German M. Bravo <german.mb@deipi.com>
2016-10-03 16:00:29 -0500

    small cleanups

--- src/v8/v8pp.h
+++ src/v8/v8pp.h
@@ -106,9 +106,6 @@ class ArrayBufferAllocator : public v8::ArrayBuffer::Allocator {
 public:
 	void* AllocateUninitialized(size_t length) {
 		void* data = malloc(length);
-		if (data == nullptr) {
-			fprintf(stderr, "ERROR!!!\n");
-		}
 		return data;
 	}
 	void* Allocate(size_t length) {
@@ -147,7 +144,8 @@ class Processor {
 	};
 
 	struct PropertyHandler {
-		Processor* processor;
+		v8::Isolate* isolate;
+		v8::Global<v8::ObjectTemplate> obj_template;
 		wrap<MsgPack> wapped_type;
 
 		static void property_getter_cb(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info) {
@@ -197,23 +195,21 @@ class Processor {
 
 		v8::Local<v8::Value> property_getter(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Value>& info) {
 			if (property.IsEmpty() || property->IsNull() || property->IsUndefined()) {
-				return v8::Undefined(processor->isolate);
+				return v8::Undefined(isolate);
 			}
 
 			auto name_str = convert<std::string>()(property);
 
 			if (name_str == "toString") {
 				auto str = wapped_type.to_string(info);
-				return v8::FunctionTemplate::New(processor->isolate, to_string, v8::String::NewFromUtf8(processor->isolate, str.data(), v8::NewStringType::kNormal, str.size()).ToLocalChecked())->GetFunction();
+				return v8::FunctionTemplate::New(isolate, to_string, v8::String::NewFromUtf8(isolate, str.data(), v8::NewStringType::kNormal, str.size()).ToLocalChecked())->GetFunction();
 			}
 
-			v8::Local<v8::ObjectTemplate> obj_template_ = v8::Local<v8::ObjectTemplate>::New(processor->isolate, processor->obj_template);
-			return  wapped_type.getter(name_str, info, obj_template_);
+			return  wapped_type.getter(name_str, info, obj_template.Get(isolate));
 		}
 
 		v8::Local<v8::Value> index_getter(uint32_t index, const v8::PropertyCallbackInfo<v8::Value>& info) {
-			v8::Local<v8::ObjectTemplate> obj_template_ = v8::Local<v8::ObjectTemplate>::New(processor->isolate, processor->obj_template);
-			return wapped_type.getter(index, info, obj_template_);
+			return wapped_type.getter(index, info, obj_template.Get(isolate));
 		}
 
 		v8::Local<v8::Value> property_setter(v8::Local<v8::String> property, v8::Local<v8::Value> value_obj, const v8::PropertyCallbackInfo<v8::Value>& info) {
@@ -238,71 +234,78 @@ class Processor {
 
 		v8::Local<v8::Boolean> property_deleter(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Boolean>& info) {
 			if (property.IsEmpty() || property->IsNull() || property->IsUndefined()) {
-				return v8::Boolean::New(processor->isolate, false);
+				return v8::Boolean::New(isolate, false);
 			}
 
 			auto name_str = convert<std::string>()(property);
 
 			if (name_str == "toString" || name_str == "valueOf") {
-				return v8::Boolean::New(processor->isolate, false);
+				return v8::Boolean::New(isolate, false);
 			}
 
 			wapped_type.deleter(name_str, info);
-			return v8::Boolean::New(processor->isolate, true);
+			return v8::Boolean::New(isolate, true);
 		}
 
 		v8::Local<v8::Boolean> index_deleter(uint32_t index, const v8::PropertyCallbackInfo<v8::Boolean>& info) {
 			wapped_type.deleter(index, info);
-			return v8::Boolean::New(processor->isolate, true);
+			return v8::Boolean::New(isolate, true);
 		}
 
 		v8::Local<v8::Integer> property_query(v8::Local<v8::String> property, const v8::PropertyCallbackInfo<v8::Integer>&) {
 			if (property.IsEmpty() || property->IsNull() || property->IsUndefined()) {
-				return v8::Integer::New(processor->isolate, v8::None);
+				return v8::Integer::New(isolate, v8::None);
 			}
 
 			auto name_str = convert<std::string>()(property);
 
 			if (name_str == "toString" || name_str == "valueOf") {
-				return v8::Integer::New(processor->isolate, v8::ReadOnly | v8::DontDelete | v8::DontEnum);
+				return v8::Integer::New(isolate, v8::ReadOnly | v8::DontDelete | v8::DontEnum);
 			}
 
-			return v8::Integer::New(processor->isolate, v8::None);
+			return v8::Integer::New(isolate, v8::None);
 		}
 
 		v8::Local<v8::Integer> index_query(uint32_t, const v8::PropertyCallbackInfo<v8::Integer>&) {
-			return v8::Integer::New(processor->isolate, v8::None);
+			return v8::Integer::New(isolate, v8::None);
 		}
 
 		v8::Local<v8::Array> enumerator(const v8::PropertyCallbackInfo<v8::Array>& info) {
 			const auto& obj = convert<MsgPack>()(info);
 			switch (obj.getType()) {
 				case MsgPack::Type::MAP: {
-					v8::Local<v8::Array> result = v8::Array::New(processor->isolate, obj.size());
+					v8::Local<v8::Array> result = v8::Array::New(isolate, obj.size());
 					int i = 0;
 					for (const auto& key : obj) {
-						result->Set(i++, v8::String::NewFromUtf8(processor->isolate, key.as_string().c_str(), v8::NewStringType::kNormal).ToLocalChecked());
+						result->Set(i++, v8::String::NewFromUtf8(isolate, key.as_string().c_str(), v8::NewStringType::kNormal).ToLocalChecked());
 					}
 					return result;
 				}
 				case MsgPack::Type::ARRAY: {
 					auto size = obj.size();
-					v8::Local<v8::Array> result = v8::Array::New(processor->isolate, size);
+					v8::Local<v8::Array> result = v8::Array::New(isolate, size);
 					for (size_t i = 0; i < size; ++i) {
-						result->Set(i, v8::Integer::New(processor->isolate, i));
+						result->Set(i, v8::Integer::New(isolate, i));
 					}
 					return result;
 				}
 				default:
-					return v8::Array::New(processor->isolate, 0);
+					return v8::Array::New(isolate, 0);
 			}
 		}
 
-		PropertyHandler(Processor* processor_) : processor(processor_) { }
+		PropertyHandler(v8::Isolate* isolate_) : isolate(isolate_) { }
 
 		v8::Local<v8::Value> operator()(MsgPack& arg) const {
-			v8::Local<v8::ObjectTemplate> obj_template_ = v8::Local<v8::ObjectTemplate>::New(v8::Isolate::GetCurrent(), processor->obj_template);
-			return wapped_type.toValue(processor->isolate, arg, obj_template_);
+			return wapped_type.toValue(isolate, arg, obj_template.Get(isolate));
+		}
+
+		void Initialize() {
+			v8::Local<v8::ObjectTemplate> obj_template_ = v8::ObjectTemplate::New(isolate);
+			obj_template_->SetInternalFieldCount(1);
+			obj_template_->SetNamedPropertyHandler(property_getter_cb, property_setter_cb, property_query_cb, property_deleter_cb, enumerator_cb, v8::External::New(isolate, this));
+			obj_template_->SetIndexedPropertyHandler(index_getter_cb, index_setter_cb, index_query_cb, index_deleter_cb, enumerator_cb, v8::External::New(isolate, this));
+			obj_template.Reset(isolate, obj_template_);
 		}
 	};
 
@@ -318,9 +321,17 @@ public:
 			: processor(processor_),
 			  function(std::move(function_)) { }
 
+		Function(Function&& o)
+			: processor(std::move(o.processor)),
+			  function(std::move(o.function))
+		{
+			processor = nullptr;
+		}
+
 		~Function() {
 			function.Reset();
 		}
+
 
 		template <typename... Args>
 		MsgPack operator()(Args&&... args) {
@@ -330,11 +341,9 @@ public:
 
 private:
 	v8::Isolate* isolate;
-	bool initialized;
 
 	PropertyHandler property_handler;
 	v8::Global<v8::Context> context;
-	v8::Global<v8::ObjectTemplate> obj_template;
 	std::map<std::string, Function> functions;
 
 	// Auxiliar variables for kill a script.
@@ -345,48 +354,28 @@ private:
 	void Initialize(const std::string& script_name_, const std::string& script_source_) {
 		v8::Locker locker(isolate);
 		v8::Isolate::Scope isolate_scope(isolate);
-
-		// Create a handle scope to hold the temporary references.
 		v8::HandleScope handle_scope(isolate);
 
-		v8::Local<v8::ObjectTemplate> global = v8::ObjectTemplate::New(isolate);
-		global->Set(v8::String::NewFromUtf8(isolate, "print", v8::NewStringType::kNormal).ToLocalChecked(), v8::FunctionTemplate::New(isolate, print));
+		property_handler.Initialize();
 
-		v8::Local<v8::Context> context_ = v8::Context::New(isolate, nullptr, global);
+		v8::Local<v8::ObjectTemplate> global_template = v8::ObjectTemplate::New(isolate);
+
+		global_template->Set(v8::String::NewFromUtf8(isolate, "print", v8::NewStringType::kNormal).ToLocalChecked(), v8::FunctionTemplate::New(isolate, print));
+
+		auto context_ = v8::Context::New(isolate, nullptr, global_template);
 		context.Reset(isolate, context_);
 
 		// Enter the new context so all the following operations take place within it.
 		v8::Context::Scope context_scope(context_);
+		v8::TryCatch try_catch;
 
 		v8::Local<v8::String> script_name = v8::String::NewFromUtf8(isolate, script_name_.data(), v8::NewStringType::kNormal, script_name_.size()).ToLocalChecked();
 		v8::Local<v8::String> script_source = v8::String::NewFromUtf8(isolate, script_source_.data(), v8::NewStringType::kNormal, script_source_.size()).ToLocalChecked();
-
-		v8::TryCatch try_catch;
-
 		v8::Local<v8::Script> script = v8::Script::Compile(script_source, script_name);
 
-		initialized = !script.IsEmpty();
+		script->Run();
 
-		if (initialized) {
-			script->Run();
-			v8::Local<v8::ObjectTemplate> obj_template_ = v8::ObjectTemplate::New(isolate);
-			obj_template_->SetInternalFieldCount(1);
-			obj_template_->SetNamedPropertyHandler(
-				v8pp::Processor::PropertyHandler::property_getter_cb,
-				v8pp::Processor::PropertyHandler::property_setter_cb,
-				v8pp::Processor::PropertyHandler::property_query_cb,
-				v8pp::Processor::PropertyHandler::property_deleter_cb,
-				v8pp::Processor::PropertyHandler::enumerator_cb,
-				v8::External::New(isolate, &property_handler));
-			obj_template_->SetIndexedPropertyHandler(
-				v8pp::Processor::PropertyHandler::index_getter_cb,
-				v8pp::Processor::PropertyHandler::index_setter_cb,
-				v8pp::Processor::PropertyHandler::index_query_cb,
-				v8pp::Processor::PropertyHandler::index_deleter_cb,
-				v8pp::Processor::PropertyHandler::enumerator_cb,
-				v8::External::New(isolate, &property_handler));
-			obj_template.Reset(isolate, obj_template_);
-		} else {
+		if (try_catch.HasCaught()) {
 			throw ScriptSyntaxError(std::string("ScriptSyntaxError: ").append(report_exception(&try_catch)));
 		}
 	}
@@ -395,22 +384,16 @@ private:
 		v8::Locker locker(isolate);
 		v8::Isolate::Scope isolate_scope(isolate);
 		v8::HandleScope handle_scope(isolate);
-		v8::Local<v8::Context> context_ = v8::Local<v8::Context>::New(isolate, context);
-		v8::Context::Scope context_scope(context_);
 
 		// The script compiled and ran correctly.  Now we fetch out the function from the global object.
-		v8::Local<v8::Value> function_val = context_->Global()->Get(v8::String::NewFromUtf8(v8::Isolate::GetCurrent(), name.c_str(), v8::NewStringType::kNormal).ToLocalChecked());
+		auto global = context.Get(isolate)->Global();
+		auto name_value = v8::String::NewFromUtf8(isolate, name.data(), v8::NewStringType::kNormal, name.size()).ToLocalChecked();
+		v8::Local<v8::Value> function_val = global->Get(name_value);
 
 		// If there is no function, or if it is not a function, ignore
 		if (function_val->IsFunction()) {
-			// It is a function; cast it to a Function
-			v8::Local<v8::Function> function_fun = v8::Local<v8::Function>::Cast(function_val);
-
-			// Store the function in a Global handle, since we also want
-			// that to remain after this call returns
-			v8::Global<v8::Function> function;
-			function.Reset(v8::Isolate::GetCurrent(), function_fun);
-			return std::move(function);
+			// It is a function; cast it to a Function and make it Global
+			return std::move(v8::Global<v8::Function>(isolate, v8::Local<v8::Function>::Cast(function_val)));
 		}
 
 		throw ReferenceError(std::string("Reference error to function: ").append(name));
@@ -429,8 +412,6 @@ private:
 		v8::Locker locker(isolate);
 		v8::Isolate::Scope isolate_scope(isolate);
 		v8::HandleScope handle_scope(isolate);
-		v8::Local<v8::Context> context_ = v8::Local<v8::Context>::New(isolate, context);
-		v8::Context::Scope context_scope(context_);
 
 		std::array<v8::Local<v8::Value>, sizeof...(arguments)> args{{ property_handler(std::forward<Args>(arguments))... }};
 		v8::TryCatch try_catch;
@@ -440,8 +421,7 @@ private:
 		t_kill.detach();
 
 		// Invoke the function, giving the global object as 'this' and one args
-		v8::Local<v8::Function> function_ = v8::Local<v8::Function>::New(isolate, function);
-		v8::Local<v8::Value> result = function_->Call(context_->Global(), args.size(), args.data());
+		v8::Local<v8::Value> result = function.Get(isolate)->Call(context.Get(isolate)->Global(), args.size(), args.data());
 
 		if (finished) {
 			throw TimeOutError();
@@ -465,48 +445,27 @@ private:
 public:
 	Processor(const std::string& script_name, const std::string& script_source)
 		: isolate(v8::Isolate::New(CreateParams())),
-		  property_handler(this)
+		  property_handler(isolate)
 	{
 		Initialize(script_name, script_source);
 	}
 
 	Processor(Processor&& o)
 		: isolate(std::move(o.isolate)),
-		  initialized(std::move(o.initialized)),
 		  property_handler(std::move(o.property_handler)),
 		  context(std::move(o.context)),
-		  obj_template(std::move(o.obj_template)),
 		  functions(std::move(o.functions))
-	{ }
-
-	Processor& operator=(Processor&& o) {
-		if (this != &o) {
-			reset();
-			isolate = std::move(o.isolate);
-			initialized = std::move(o.initialized);
-			property_handler = std::move(o.property_handler);
-			context = std::move(o.context);
-			obj_template = std::move(o.obj_template);
-			functions = std::move(o.functions);
-		}
-		return *this;
+	{
+		isolate = nullptr;
 	}
-
-	Processor(const Processor&) = delete;
-	Processor& operator=(const Processor&) = delete;
 
 	~Processor() {
-		reset();
-	}
-
-	void reset() {
 		if (isolate) {
 			{
 				v8::Locker locker(isolate);
 				v8::Isolate::Scope isolate_scope(isolate);
 				context.Reset();
 			}
-
 			isolate->Dispose();
 		}
 	}
@@ -515,7 +474,7 @@ public:
 		try {
 			return functions.at(name);
 		} catch (const std::out_of_range&) {
-			functions.emplace(std::piecewise_construct, std::forward_as_tuple(name), std::forward_as_tuple(this, extract_function(name)));
+			functions.emplace(name, Function(this, extract_function(name)));
 			return functions.at(name);
 		}
 	}
