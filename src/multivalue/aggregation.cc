638da64e9f2e103698d24f609980338498b91a68
JoseMariaVR <chem_valencia@hotmail.com>
2016-10-07 17:10:56 -0500

    Add Aggregations

---
+++ src/multivalue/aggregation.cc
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2016 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "aggregation.h"
+
+#include "../exception.h"
+#include "../length.h"
+#include "../msgpack.h"
+#include "../stl_serialise.h"
+
+#include "aggregation_bucket.h"
+
+#include <cassert>
+
+
+const std::unordered_map<std::string, dispatch_aggregations> map_dispatch_aggregations({
+	{ AGGREGATION_COUNT,            &Aggregation::add_count           },
+	{ AGGREGATION_SUM,              &Aggregation::add_sum             },
+	{ AGGREGATION_AVG,              &Aggregation::add_avg             },
+	{ AGGREGATION_MIN,              &Aggregation::add_min             },
+	{ AGGREGATION_MAX,              &Aggregation::add_max             },
+	{ AGGREGATION_VARIANCE,         &Aggregation::add_variance        },
+	{ AGGREGATION_STD,              &Aggregation::add_std             },
+	{ AGGREGATION_MEDIAN,           &Aggregation::add_median          },
+	{ AGGREGATION_MODE,             &Aggregation::add_mode            },
+	{ AGGREGATION_STATS,            &Aggregation::add_stats           },
+	{ AGGREGATION_EXT_STATS,        &Aggregation::add_ext_stats       },
+	{ AGGREGATION_GEO_BOUNDS,       &Aggregation::add_geo_bounds      },
+	{ AGGREGATION_GEO_CENTROID,     &Aggregation::add_geo_centroid    },
+	{ AGGREGATION_PERCENTILE,       &Aggregation::add_percentile      },
+	{ AGGREGATION_FILTER,           &Aggregation::add_filter          }
+});
+
+
+Aggregation::Aggregation(MsgPack& result, const MsgPack& o_aggs, const std::shared_ptr<Schema>& schema)
+{
+	try {
+		const auto& aggs = o_aggs.at(AGGREGATION_AGGS);
+		for (const auto& agg : aggs) {
+			auto sub_agg_name = agg.as_string();
+			if (is_valid(sub_agg_name)) {
+				const auto& sub_agg = aggs.at(sub_agg_name);
+				auto sub_agg_type = (*sub_agg.begin()).as_string();
+				try {
+					auto func = map_dispatch_aggregations.at(sub_agg_type);
+					(this->*func)(result[sub_agg_name], sub_agg, schema);
+				} catch (const std::out_of_range&) {
+					throw MSG_AggregationError("Aggregation type: %s is not valid", sub_agg_name.c_str());
+				}
+			} else {
+				throw MSG_AggregationError("Aggregation sub_agg_name: %s is not valid", sub_agg_name.c_str());
+			}
+		}
+	} catch (const std::out_of_range&) {
+		throw MSG_AggregationError("'%s' must be defined", AGGREGATION_AGGS);
+	} catch (const msgpack::type_error) {
+		throw MSG_AggregationError("Aggregations must be a json");
+	}
+}
+
+
+void
+Aggregation::operator()(const Xapian::Document& doc)
+{
+	for (auto& sub_agg : _sub_aggregations) {
+		(*sub_agg)(doc);
+	}
+};
+
+
+void
+Aggregation::update()
+{
+	for (auto& sub_agg : _sub_aggregations) {
+		sub_agg->update();
+	}
+}
+
+
+inline void
+Aggregation::add_filter(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema)
+{
+	_sub_aggregations.push_back(std::make_shared<FilterAggregation>(result, data, schema));
+}
+
+
+void
+AggregationMatchSpy::operator()(const Xapian::Document& doc, double)
+{
+	++_total;
+	_aggregation(doc);
+}
+
+
+Xapian::MatchSpy*
+AggregationMatchSpy::clone() const
+{
+	return new AggregationMatchSpy(_aggs, _schema);
+}
+
+
+std::string
+AggregationMatchSpy::name() const
+{
+	return "AggregationMatchSpy";
+}
+
+
+std::string
+AggregationMatchSpy::serialise() const
+{
+	StringList l;
+	l.push_back(_aggs.serialise());
+	l.push_back(_schema->get_const_schema()->serialise());
+	return l.serialise();
+}
+
+
+Xapian::MatchSpy*
+AggregationMatchSpy::unserialise(const std::string& s, const Xapian::Registry&) const
+{
+	StringList l;
+	l.unserialise(s);
+	std::shared_ptr<const MsgPack> internal_schema = std::make_shared<const MsgPack>(MsgPack::unserialise(l.at(1)));
+	return new AggregationMatchSpy(MsgPack::unserialise(l.at(0)), std::make_shared<Schema>(internal_schema));
+}
+
+
+std::string
+AggregationMatchSpy::get_description() const
+{
+	std::string desc("AggregationMatchSpy(");
+	desc.append(_aggs.to_string()).push_back(')');
+	return desc;
+}
---
+++ src/multivalue/aggregation.h
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2016 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "aggregation_metric.h"
+
+#include <xapian.h>
+
+#include <string>
+#include <vector>
+
+
+class Aggregation {
+	std::vector<std::shared_ptr<SubAggregation>> _sub_aggregations;
+
+public:
+	Aggregation() = default;
+
+	Aggregation(MsgPack& result, const MsgPack& aggs, const std::shared_ptr<Schema>& schema);
+
+	void operator()(const Xapian::Document& doc);
+
+	void update();
+
+	inline void add_count(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>&) {
+		_sub_aggregations.push_back(std::make_shared<MetricCount>(result[AGGREGATION_COUNT], data[AGGREGATION_COUNT]));
+	}
+
+	inline void add_sum(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricSum>(result[AGGREGATION_SUM], data[AGGREGATION_SUM], schema));
+	}
+
+	inline void add_avg(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricAvg>(result[AGGREGATION_AVG], data[AGGREGATION_AVG], schema));
+	}
+
+	inline void add_min(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricMin>(result[AGGREGATION_MIN], data[AGGREGATION_MIN], schema));
+	}
+
+	inline void add_max(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricMax>(result[AGGREGATION_MAX], data[AGGREGATION_MAX], schema));
+	}
+
+	inline void add_variance(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricVariance>(result[AGGREGATION_VARIANCE], data[AGGREGATION_VARIANCE], schema));
+	}
+
+	inline void add_std(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricSTD>(result[AGGREGATION_STD], data[AGGREGATION_STD], schema));
+	}
+
+	inline void add_median(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricMedian>(result[AGGREGATION_MEDIAN], data[AGGREGATION_MEDIAN], schema));
+	}
+
+	inline void add_mode(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricMode>(result[AGGREGATION_MODE], data[AGGREGATION_MODE], schema));
+	}
+
+	inline void add_stats(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricStats>(result, data[AGGREGATION_STATS], schema));
+	}
+
+	inline void add_ext_stats(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) {
+		_sub_aggregations.push_back(std::make_shared<MetricExtendedStats>(result, data[AGGREGATION_EXT_STATS], schema));
+	}
+
+	inline void add_geo_bounds(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) { }
+
+	inline void add_geo_centroid(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) { }
+
+	inline void add_percentile(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema) { }
+
+	inline void add_filter(MsgPack& result, const MsgPack& data, const std::shared_ptr<Schema>& schema);
+};
+
+
+using dispatch_aggregations = void (Aggregation::*)(MsgPack&, const MsgPack&, const std::shared_ptr<Schema>&);
+
+
+extern const std::unordered_map<std::string, dispatch_aggregations> map_dispatch_aggregations;
+
+
+// Class for calculating aggregations in the matching documents.
+class AggregationMatchSpy : public Xapian::MatchSpy {
+	// Total number of documents seen by the match spy.
+	Xapian::doccount _total;
+
+	// Result for aggregations.
+	MsgPack _result;
+
+	MsgPack _aggs;
+	std::shared_ptr<Schema> _schema;
+
+	// Aggregation seen so far.
+	Aggregation _aggregation;
+
+public:
+	// Construct an empty AggregationMatchSpy.
+	AggregationMatchSpy()
+		: _total(0) { }
+
+	/*
+	 * Construct a AggregationMatchSpy which aggregates the values.
+	 *
+	 * Further Aggregations can be added by calling add_aggregation().
+	 */
+	template <typename T, typename = std::enable_if_t<std::is_same<MsgPack, std::decay_t<T>>::value>>
+	AggregationMatchSpy(T&& aggs, const std::shared_ptr<Schema>& schema)
+		: _total(0),
+		  _result(),
+		  _aggs(std::forward<T>(aggs)),
+		  _schema(schema),
+		  _aggregation(_result[AGGREGATION_AGGS], _aggs, _schema) { }
+
+	/*
+	 * Implementation of virtual operator().
+	 *
+	 * This implementation aggregates values for a matching document.
+	 */
+	void operator()(const Xapian::Document &doc, double wt) override;
+
+	Xapian::MatchSpy* clone() const override;
+	std::string name() const override;
+	std::string serialise() const override;
+	Xapian::MatchSpy* unserialise(const std::string& serialised, const Xapian::Registry& context) const override;
+	std::string get_description() const override;
+
+	const auto& get_aggregation() noexcept {
+		_aggregation.update();
+		return _result;
+	}
+};
