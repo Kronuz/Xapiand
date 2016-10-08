5ba14937805ffa77a1f4e19559613ad6d7856353
German M. Bravo <german.mb@deipi.com>
2016-10-07 19:11:54 -0500

    Added missing MSG_AggregationError

--- src/exception.h
+++ src/exception.h
@@ -163,6 +163,13 @@ public:
 };
 
 
+class AggregationError : public ClientError {
+public:
+	template<typename... Args>
+	AggregationError(Args&&... args) : ClientError(std::forward<Args>(args)...) { }
+};
+
+
 #define MSG_Error(...) Error(__FILE__, __LINE__, __VA_ARGS__)
 #define MSG_ClientError(...) ClientError(__FILE__, __LINE__, __VA_ARGS__)
 #define MSG_LimitError(...) LimitError(__FILE__, __LINE__, __VA_ARGS__)
@@ -176,3 +183,4 @@ public:
 #define MSG_DocNotFoundError(...) DocNotFoundError(__FILE__, __LINE__, __VA_ARGS__)
 #define MSG_CheckoutError(...) CheckoutError(__FILE__, __LINE__, __VA_ARGS__)
 #define MSG_MissingTypeError(...) MissingTypeError(__FILE__, __LINE__, __VA_ARGS__)
+#define MSG_AggregationError(...) AggregationError(__FILE__, __LINE__, __VA_ARGS__)
