89d953a0139208e5278753cb740df6a87ebc48ef
German M. Bravo <german.mb@deipi.com>
2016-11-03 13:29:14 -0600

    Lowercase hex for JSON \uNNNN and \xNN

--- src/rapidjson/writer.h
+++ src/rapidjson/writer.h
@@ -242,7 +242,7 @@ protected:
     }
 
     bool WriteString(const Ch* str, SizeType length)  {
-        static const char hexDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        static const char hexDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
         static const char escape[256] = {
 #define X 'x'
 #define X16 X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X
