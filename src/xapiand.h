4764f3865a4008f51f06d6f6ffee7e440259e196
German M. Bravo <german.mb@deipi.com>
2016-11-05 09:53:11 -0600

    Fix likely and unlikely macros (when __builtin_expect works)

--- src/xapiand.h
+++ src/xapiand.h
@@ -59,8 +59,8 @@
 
 
 #ifdef HAVE___BUILTIN_EXPECT
-#define likely(x) __builtin_expect(!!(x), 1)
-#define unlikely(x) __builtin_expect(!!(x), 0)
+#define likely(x) (__builtin_expect(!!(x), 1))
+#define unlikely(x) (__builtin_expect(!!(x), 0))
 #else
 #define likely(x) (x)
 #define unlikely(x) (x)
