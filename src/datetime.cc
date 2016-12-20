afc65accbfa628cdf882b05389eeec010feac3c1
JoseMariaVR <chem_valencia@hotmail.com>
2016-12-20 10:25:33 -0600

    Using gmtime_r instead of std::gmtime, because std::gmtime is not threadsafe

--- src/datetime.cc
+++ src/datetime.cc
@@ -517,7 +517,8 @@ Datetime::computeDateMath(tm_t& tm, const std::string& op, char unit)
 					break;
 				case 'w': {
 					auto dateGMT = timegm(tm);
-					auto timeinfo = std::gmtime(&dateGMT);
+					struct tm* timeinfo = nullptr;
+					gmtime_r(&dateGMT, timeinfo);
 					if (op.length() == 1) {
 						tm.day += 6 - timeinfo->tm_wday;
 						tm.hour = 23;
@@ -579,7 +580,8 @@ Datetime::computeDateMath(tm_t& tm, const std::string& op, char unit)
 
 	// Update date.
 	auto dateGMT = timegm(tm);
-	auto timeinfo = std::gmtime(&dateGMT);
+	struct tm* timeinfo = nullptr;
+	gmtime_r(&dateGMT, timeinfo);
 	tm.year = timeinfo->tm_year + _START_YEAR;
 	tm.mon = timeinfo->tm_mon + 1;
 	tm.day = timeinfo->tm_mday;
@@ -688,7 +690,8 @@ Datetime::tm_t
 Datetime::to_tm_t(double timestamp)
 {
 	auto _time = static_cast<std::time_t>(timestamp);
-	auto timeinfo = std::gmtime(&_time);
+	struct tm* timeinfo = nullptr;
+	gmtime_r(&_time, timeinfo);
 	return tm_t(
 		timeinfo->tm_year + _START_YEAR, timeinfo->tm_mon + 1,
 		timeinfo->tm_mday, timeinfo->tm_hour, timeinfo->tm_min,
