bc380fd8d13c360448ff07570d149c5661d26546
German M. Bravo <german.mb@deipi.com>
2017-01-17 12:38:50 -0600

    Add prefixes to all endpoints

--- contrib/python/xapiand/__init__.py
+++ contrib/python/xapiand/__init__.py
@@ -140,7 +140,7 @@ class Xapiand(object):
         self.host = host
         self.port = port
         self.commit = commit
-        self.prefix = prefix
+        self.prefix = '{}/'.format(prefix) if prefix else None
         if default_accept is None:
             default_accept = 'application/json' if msgpack is None else 'application/x-msgpack'
         self.default_accept = default_accept
@@ -157,11 +157,11 @@ class Xapiand(object):
             port = self.port
         host = '{}:{}'.format(host, port)
 
-        prefix = '{}/'.format(self.prefix) if self.prefix else ''
-
         if not isinstance(index, (tuple, list)):
-            index = [index]
-        index = ','.join(prefix + i for i in index)
+            index = index.split(',')
+        if self.prefix:
+            index = [self.prefix + i for i in index]
+        index = ','.join(index)
 
         nodename = '@{}'.format(nodename) if nodename else ''
 
