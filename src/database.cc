5fc9f3413f71cbdc9fc372c5c28c96d8753a346c
JoseMariaVR <chem_valencia@hotmail.com>
2017-02-02 11:16:25 -0600

    Delete flag DB_VOLATILE in WAL Database

--- src/database.cc
+++ src/database.cc
@@ -2127,7 +2127,7 @@ DatabasePool::checkout(std::shared_ptr<Database>& database, const Endpoints& end
 								Endpoints e;
 								e.add(endpoint);
 								std::shared_ptr<Database> d;
-								if (checkout(d, e, DB_WRITABLE | DB_VOLATILE)) {
+								if (checkout(d, e, DB_WRITABLE)) {
 									// Checkout executes any commands from the WAL
 									reopen = true;
 									checkin(d);
