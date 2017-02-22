4f18843356e8e557f3266ad1bb52a229b9a23146
JoseMariaVR <chem_valencia@hotmail.com>
2017-02-21 18:58:36 -0600

    Add Circle class in geo

---
+++ src/geo/circle.cc
@@ -0,0 +1,374 @@
+/*
+ * Copyright (C) 2017 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "circle.h"
+
+
+bool
+Circle::operator==(const Circle& c) const noexcept
+{
+	return constraint == c.constraint;
+}
+
+
+bool
+Circle::operator!=(const Circle& c) const noexcept
+{
+	return !operator==(c);
+}
+
+
+bool
+Circle::operator<(const Circle& c) const noexcept
+{
+	return constraint < c.constraint;
+}
+
+
+bool
+Circle::operator>(const Circle& c) const noexcept
+{
+	return constraint > c.constraint;
+}
+
+
+TypeTrixel
+Circle::verifyTrixel(const Cartesian& v0, const Cartesian& v1, const Cartesian& v2) const
+{
+	int sum = HTM::insideVertex_Constraint(v0, constraint) + HTM::insideVertex_Constraint(v1, constraint) + HTM::insideVertex_Constraint(v2, constraint);
+
+	switch (sum) {
+		case 0: {
+			// Get bounding circle of trixel (v0, v1, v2).
+			auto bounding_circle = HTM::getBoundingCircle(v0, v1, v2);
+			// If  constraint intersect with the bounding circle.
+			if (HTM::intersectConstraints(constraint, bounding_circle)) {
+				// The constraint intersect with a edge of trixel (v0, v1, v2).
+				if (HTM::intersectConstraint_EdgeTrixel(constraint, v0, v1, v2)) {
+					return TypeTrixel::PARTIAL;
+				}
+				// Center of constraint inside trixel (v0, v1, v2).
+				if (HTM::insideVertex_Trixel(constraint.center, v0, v1, v2)) {
+					return TypeTrixel::PARTIAL;
+				}
+			}
+			return TypeTrixel::OUTSIDE;
+		}
+		case 1:
+		case 2:
+			return TypeTrixel::PARTIAL;
+		case 3:
+			/*
+			 * All corners are inside.
+			 *   For Negative constraints we need to test if there is a hole or there are intersections with the trixel.
+			 */
+			if (constraint.sign == Constraint::Sign::NEG) {
+				// If there is a hole the trixel is flag to TypeTrixel::PARTIAL.
+				if (HTM::thereisHole(constraint, v0, v1, v2)) return TypeTrixel::PARTIAL;
+				// Test whether one of the negative halfspace’s boundary circles intersects with one of the edges of the triangle.
+				if (HTM::intersectConstraint_EdgeTrixel(constraint, v0, v1, v2)) return TypeTrixel::PARTIAL;
+			}
+			return TypeTrixel::FULL;
+		default:
+			return TypeTrixel::OUTSIDE;
+	}
+}
+
+
+void
+Circle::lookupTrixel(const Cartesian& v0, const Cartesian& v1, const Cartesian& v2, std::string name, trixel_data& data, uint8_t level) const
+{
+	// Finish the recursion.
+	if (level == data.max_level) {
+		data.aux_trixels.push_back(std::move(name));
+		return;
+	}
+
+	auto w2 = HTM::midPoint(v0, v1);
+	auto w0 = HTM::midPoint(v1, v2);
+	auto w1 = HTM::midPoint(v2, v0);
+
+	TypeTrixel type_trixels[4] = {
+		verifyTrixel(v0, w2, w1),
+		verifyTrixel(v1, w0, w2),
+		verifyTrixel(v2, w1, w0),
+		verifyTrixel(w0, w1, w2)
+	};
+
+	// Number of full and partial subtrixels.
+	int F = (type_trixels[0] == TypeTrixel::FULL) + (type_trixels[1] == TypeTrixel::FULL) + (type_trixels[2] == TypeTrixel::FULL) + (type_trixels[3] == TypeTrixel::FULL);
+
+	if (F == 4) {
+		data.trixels.push_back(std::move(name));
+		return;
+	}
+
+	++level;
+
+	switch (type_trixels[0]) {
+		case TypeTrixel::FULL:
+			data.trixels.push_back(name + "0");
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(v0, w2, w1, name + "0", data, level);
+			break;
+		default:
+			break;
+	}
+
+	switch (type_trixels[1]) {
+		case TypeTrixel::FULL:
+			data.trixels.push_back(name + "1");
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(v1, w0, w2, name + "1", data, level);
+			break;
+		default:
+			break;
+	}
+
+	switch (type_trixels[2]) {
+		case TypeTrixel::FULL:
+			data.trixels.push_back(name + "2");
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(v2, w1, w0, name + "2", data, level);
+			break;
+		default:
+			break;
+	}
+
+	switch (type_trixels[3]) {
+		case TypeTrixel::FULL:
+			data.trixels.push_back(name + "3");
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(w0, w1, w2, name + "3", data, level);
+			break;
+		default:
+			break;
+	}
+}
+
+
+void
+Circle::lookupTrixel(const Cartesian& v0, const Cartesian& v1, const Cartesian& v2, uint64_t id, range_data& data, uint8_t level) const
+{
+	// Finish the recursion.
+	if (level == data.max_level) {
+		HTM::insertRange(data.aux_ranges, HTM::getRange(id, level));
+		return;
+	}
+
+	auto w2 = HTM::midPoint(v0, v1);
+	auto w0 = HTM::midPoint(v1, v2);
+	auto w1 = HTM::midPoint(v2, v0);
+
+	TypeTrixel type_trixels[4] = {
+		verifyTrixel(v0, w2, w1),
+		verifyTrixel(v1, w0, w2),
+		verifyTrixel(v2, w1, w0),
+		verifyTrixel(w0, w1, w2)
+	};
+
+	// Number of full and partial subtrixels.
+	int F = (type_trixels[0] == TypeTrixel::FULL) + (type_trixels[1] == TypeTrixel::FULL) + (type_trixels[2] == TypeTrixel::FULL) + (type_trixels[3] == TypeTrixel::FULL);
+
+	if (F == 4) {
+		HTM::insertRange(data.ranges, HTM::getRange(id, level));
+		return;
+	}
+
+	++level;
+	id <<= 2;
+
+	switch (type_trixels[0]) {
+		case TypeTrixel::FULL:
+			HTM::insertRange(data.ranges, HTM::getRange(id, level));
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(v0, w2, w1, id, data, level);
+			break;
+		default:
+			break;
+	}
+
+	switch (type_trixels[1]) {
+		case TypeTrixel::FULL:
+			HTM::insertRange(data.ranges, HTM::getRange(id + 1, level));
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(v1, w0, w2, id + 1, data, level);
+			break;
+		default:
+			break;
+	}
+
+	switch (type_trixels[2]) {
+		case TypeTrixel::FULL:
+			HTM::insertRange(data.ranges, HTM::getRange(id + 2, level));
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(v2, w1, w0, id + 2, data, level);
+			break;
+		default:
+			break;
+	}
+
+	switch (type_trixels[3]) {
+		case TypeTrixel::FULL:
+			HTM::insertRange(data.ranges, HTM::getRange(id + 3, level));
+			break;
+		case TypeTrixel::PARTIAL:
+			lookupTrixel(w0, w1, w2, id + 3, data, level);
+			break;
+		default:
+			break;
+	}
+}
+
+
+std::string
+Circle::toWKT() const
+{
+	std::string wkt("CIRCLE Z ");
+	wkt.append(to_string());
+	return wkt;
+}
+
+
+std::string
+Circle::to_string() const
+{
+	char result[128];
+	snprintf(result, 128, "(%.6f %.6f %.6f, %.6f)",
+		constraint.center.x * constraint.center.scale,
+		constraint.center.y * constraint.center.scale,
+		constraint.center.z * constraint.center.scale,
+		constraint.radius
+	);
+	return std::string(result);
+}
+
+
+std::vector<std::string>
+Circle::getTrixels(bool partials, double error) const
+{
+	if (error < HTM_MIN_ERROR || error > HTM_MAX_ERROR) {
+		THROW(HTMError, "Error must be in [%f, %f]", HTM_MIN_ERROR, HTM_MAX_ERROR);
+	}
+
+	trixel_data data(partials, HTM_MAX_LEVEL);
+	error = error * constraint.radius;
+	for (size_t i = 0; i < HTM_MAX_LEVEL; ++i) {
+		if (ERROR_NIVEL[i] < error) {
+			data.max_level = i;
+			break;
+		}
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[5].v0], start_vertices[start_trixels[5].v1], start_vertices[start_trixels[5].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[5].v0], start_vertices[start_trixels[5].v1], start_vertices[start_trixels[5].v2], start_trixels[5].name, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[1].v0], start_vertices[start_trixels[1].v1], start_vertices[start_trixels[1].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[1].v0], start_vertices[start_trixels[1].v1], start_vertices[start_trixels[1].v2], start_trixels[1].name, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[3].v0], start_vertices[start_trixels[3].v1], start_vertices[start_trixels[3].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[3].v0], start_vertices[start_trixels[3].v1], start_vertices[start_trixels[3].v2], start_trixels[3].name, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[7].v0], start_vertices[start_trixels[7].v1], start_vertices[start_trixels[7].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[7].v0], start_vertices[start_trixels[7].v1], start_vertices[start_trixels[7].v2], start_trixels[7].name, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[6].v0], start_vertices[start_trixels[6].v1], start_vertices[start_trixels[6].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[6].v0], start_vertices[start_trixels[6].v1], start_vertices[start_trixels[6].v2], start_trixels[6].name, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[2].v0], start_vertices[start_trixels[2].v1], start_vertices[start_trixels[2].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[2].v0], start_vertices[start_trixels[2].v1], start_vertices[start_trixels[2].v2], start_trixels[2].name, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[0].v0], start_vertices[start_trixels[0].v1], start_vertices[start_trixels[0].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[0].v0], start_vertices[start_trixels[0].v1], start_vertices[start_trixels[0].v2], start_trixels[0].name, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[4].v0], start_vertices[start_trixels[4].v1], start_vertices[start_trixels[4].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[4].v0], start_vertices[start_trixels[4].v1], start_vertices[start_trixels[4].v2], start_trixels[4].name, data, 0);
+	}
+
+	return data.getTrixels();
+}
+
+
+std::vector<range_t>
+Circle::getRanges(bool partials, double error) const
+{
+	if (error < HTM_MIN_ERROR || error > HTM_MAX_ERROR) {
+		THROW(HTMError, "Error must be in [%f, %f]", HTM_MIN_ERROR, HTM_MAX_ERROR);
+	}
+
+	range_data data(partials, HTM_MAX_LEVEL);
+	error = error * constraint.radius;
+	for (size_t i = 0; i < HTM_MAX_LEVEL; ++i) {
+		if (ERROR_NIVEL[i] < error) {
+			data.max_level = i;
+			break;
+		}
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[5].v0], start_vertices[start_trixels[5].v1], start_vertices[start_trixels[5].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[5].v0], start_vertices[start_trixels[5].v1], start_vertices[start_trixels[5].v2], start_trixels[5].id, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[1].v0], start_vertices[start_trixels[1].v1], start_vertices[start_trixels[1].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[1].v0], start_vertices[start_trixels[1].v1], start_vertices[start_trixels[1].v2], start_trixels[1].id, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[3].v0], start_vertices[start_trixels[3].v1], start_vertices[start_trixels[3].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[3].v0], start_vertices[start_trixels[3].v1], start_vertices[start_trixels[3].v2], start_trixels[3].id, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[7].v0], start_vertices[start_trixels[7].v1], start_vertices[start_trixels[7].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[7].v0], start_vertices[start_trixels[7].v1], start_vertices[start_trixels[7].v2], start_trixels[7].id, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[6].v0], start_vertices[start_trixels[6].v1], start_vertices[start_trixels[6].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[6].v0], start_vertices[start_trixels[6].v1], start_vertices[start_trixels[6].v2], start_trixels[6].id, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[2].v0], start_vertices[start_trixels[2].v1], start_vertices[start_trixels[2].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[2].v0], start_vertices[start_trixels[2].v1], start_vertices[start_trixels[2].v2], start_trixels[2].id, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[0].v0], start_vertices[start_trixels[0].v1], start_vertices[start_trixels[0].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[0].v0], start_vertices[start_trixels[0].v1], start_vertices[start_trixels[0].v2], start_trixels[0].id, data, 0);
+	}
+
+	if (verifyTrixel(start_vertices[start_trixels[4].v0], start_vertices[start_trixels[4].v1], start_vertices[start_trixels[4].v2]) != TypeTrixel::OUTSIDE) {
+		lookupTrixel(start_vertices[start_trixels[4].v0], start_vertices[start_trixels[4].v1], start_vertices[start_trixels[4].v2], start_trixels[4].id, data, 0);
+	}
+
+	return data.getRanges();
+}
---
+++ src/geo/circle.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2017 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "geometry.h"
+
+
+class Convex;
+class MultiCircle;
+
+
+struct range_data {
+	bool partials;
+	uint8_t max_level;
+	std::vector<range_t> ranges;
+	std::vector<range_t> partial_ranges;
+	std::vector<range_t>& aux_ranges;
+
+	range_data(bool partials_, uint8_t max_level_)
+		: partials(partials_),
+		  max_level(max_level_),
+		  ranges(),
+		  partial_ranges(),
+		  aux_ranges(partials ? ranges : partial_ranges) { }
+
+	std::vector<range_t> getRanges() {
+		if (!partials && ranges.empty()) {
+			ranges.reserve(partial_ranges.size());
+			ranges.insert(ranges.begin(), std::make_move_iterator(partial_ranges.begin()), std::make_move_iterator(partial_ranges.end()));
+		}
+		return std::move(ranges);
+	}
+};
+
+
+struct trixel_data {
+	bool partials;
+	uint8_t max_level;
+	std::vector<std::string> trixels;
+	std::vector<std::string> partial_trixels;
+	std::vector<std::string>& aux_trixels;
+
+	trixel_data(bool partials_, uint8_t max_level_)
+		: partials(partials_),
+		  max_level(max_level_),
+		  trixels(),
+		  partial_trixels(),
+		  aux_trixels(partials ? trixels : partial_trixels) { }
+
+	std::vector<std::string> getTrixels() {
+		if (!partials && trixels.empty()) {
+			trixels.reserve(partial_trixels.size());
+			trixels.insert(trixels.begin(), std::make_move_iterator(partial_trixels.begin()), std::make_move_iterator(partial_trixels.end()));
+		}
+		return std::move(trixels);
+	}
+};
+
+
+class Circle : public Geometry {
+	friend class Convex;
+	friend class MultiCircle;
+
+	Constraint constraint;
+
+	TypeTrixel verifyTrixel(const Cartesian&, const Cartesian&, const Cartesian&) const;
+	void lookupTrixel(const Cartesian& v0, const Cartesian& v1, const Cartesian& v2, std::string name, trixel_data& data, uint8_t level) const;
+	void lookupTrixel(const Cartesian& v0, const Cartesian& v1, const Cartesian& v2, uint64_t id, range_data& data, uint8_t level) const;
+
+public:
+	template <typename T, typename = std::enable_if_t<std::is_same<Cartesian, std::decay_t<T>>::value>>
+	Circle(T&& _center, double radius)
+		: Geometry(Type::CIRCLE),
+		  constraint(std::forward<T>(_center), radius) { }
+
+	Circle(Circle&& circle) noexcept
+		: Geometry(std::move(circle)),
+		  constraint(std::move(circle.constraint)) { }
+
+	Circle(const Circle& circle)
+		: Geometry(circle),
+		  constraint(circle.constraint) { }
+
+	~Circle() = default;
+
+	Circle& operator=(Circle&& circle) noexcept {
+		Geometry::operator=(std::move(circle));
+		constraint = std::move(circle.constraint);
+		return *this;
+	}
+
+	Circle& operator=(const Circle& circle) {
+		Geometry::operator=(circle);
+		constraint = circle.constraint;
+		return *this;
+	}
+
+	bool operator==(const Circle& c) const noexcept;
+	bool operator!=(const Circle& c) const noexcept;
+	bool operator<(const Circle& c) const noexcept;
+	bool operator>(const Circle& c) const noexcept;
+
+	const Constraint& getConstraint() const noexcept {
+		return constraint;
+	}
+
+	std::string toWKT() const override;
+	std::string to_string() const override;
+	std::vector<std::string> getTrixels(bool partials, double error) const override;
+	std::vector<range_t> getRanges(bool partials, double error) const override;
+};
