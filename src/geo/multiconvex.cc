cf0c40a3c827244c86dc500d0354dd546cd079bc
JoseMariaVR <chem_valencia@hotmail.com>
2017-03-07 16:47:45 -0600

    Add MultiConvex in geo folder

---
+++ src/geo/multiconvex.cc
@@ -0,0 +1,107 @@
+/*
+ * Copyright (C) 2017 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "multiconvex.h"
+
+#include <cmath>
+
+
+void
+MultiConvex::simplify()
+{
+	if (!simplified) {
+		// Simplify and sort convexs.
+		std::sort(convexs.begin(), convexs.end(), [](Convex& c1, Convex& c2) {
+			c1.simplify();
+			c2.simplify();
+			return c1 < c2;
+		});
+
+		// Deleting redundant and empty convexs.
+		for (auto it = convexs.begin(); it != convexs.end(); ) {
+			if (it->empty()) {
+				it = convexs.erase(it);
+			} else {
+				auto n_it = it + 1;
+				if (n_it != convexs.end() && *it == *n_it) {
+					it = convexs.erase(it);
+				} else {
+					++it;
+				}
+			}
+		}
+
+		simplified = true;
+	}
+}
+
+
+std::string
+MultiConvex::toWKT() const
+{
+	std::string wkt("MULTICONVEX Z ");
+	wkt.append(to_string());
+	return wkt;
+}
+
+
+std::string
+MultiConvex::to_string() const
+{
+	if (convexs.empty()) {
+		return "()";
+	}
+
+	std::string str("(");
+	for (const auto& convex : convexs) {
+		auto str_convex = convex.to_string();
+		str.reserve(str.length() + str_convex.length() + 2);
+		str.append(str_convex).append(", ");
+	}
+	str.pop_back();
+	str.back() = ')';
+	return str;
+}
+
+
+std::vector<std::string>
+MultiConvex::getTrixels(bool partials, double error) const
+{
+	std::vector<std::string> trixels;
+	for (const auto& convex : convexs) {
+		trixels = HTM::trixel_union(std::move(trixels), convex.getTrixels(partials, error));
+	}
+
+	return trixels;
+}
+
+
+std::vector<range_t>
+MultiConvex::getRanges(bool partials, double error) const
+{
+	std::vector<range_t> ranges;
+	for (const auto& convex : convexs) {
+		ranges = HTM::range_union(std::move(ranges), convex.getRanges(partials, error));
+	}
+
+	return ranges;
+}
---
+++ src/geo/multiconvex.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2017 deipi.com LLC and contributors. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "convex.h"
+
+
+class MultiConvex : public Geometry {
+	std::vector<Convex> convexs;
+	bool simplified;
+
+public:
+	MultiConvex()
+		: Geometry(Type::MULTICONVEX),
+		  simplified(true) { }
+
+	MultiConvex(MultiConvex&& multiconvex) noexcept
+		: Geometry(std::move(multiconvex)),
+		  convexs(std::move(multiconvex.convexs)),
+		  simplified(std::move(multiconvex.simplified)) { }
+
+	MultiConvex(const MultiConvex& multiconvex)
+		: Geometry(multiconvex),
+		  convexs(multiconvex.convexs),
+		  simplified(multiconvex.simplified) { }
+
+	~MultiConvex() = default;
+
+	MultiConvex& operator=(MultiConvex&& multiconvex) noexcept {
+		Geometry::operator=(std::move(multiconvex));
+		convexs = std::move(multiconvex.convexs);
+		simplified = std::move(multiconvex.simplified);
+		return *this;
+	}
+
+	MultiConvex& operator=(const MultiConvex& multiconvex) {
+		Geometry::operator=(multiconvex);
+		convexs = multiconvex.convexs;
+		simplified = multiconvex.simplified;
+		return *this;
+	}
+
+	template <typename T, typename = std::enable_if_t<std::is_same<Convex, std::decay_t<T>>::value>>
+	void add(T&& convex) {
+		convexs.push_back(std::forward<T>(convex));
+		simplified = false;
+	}
+
+	bool empty() const noexcept {
+		return convexs.empty();
+	}
+
+	const std::vector<Convex>& getConvexs() const noexcept {
+		return convexs;
+	}
+
+	void simplify();
+
+	std::string toWKT() const override;
+	std::string to_string() const override;
+	std::vector<std::string> getTrixels(bool partials, double error) const override;
+	std::vector<range_t> getRanges(bool partials, double error) const override;
+};
