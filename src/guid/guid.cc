cc579ed643a23be78fe6b83dc338f6e56fa78b68
JoseMariaVR <chem_valencia@hotmail.com>
2017-04-05 17:14:52 -0500

    Update license

--- src/guid/guid.cc
+++ src/guid/guid.cc
@@ -3,7 +3,7 @@ The MIT License (MIT)
 
 Copyright (c):
  2014 Graeme Hill (http://graemehill.ca)
- 2016 deipi.com LLC and contributors
+ 2016,2017 deipi.com LLC and contributors
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
