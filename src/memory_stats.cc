a2c77d3cc8a2f4377f4a4ac00a5c9a0b317c2815
German M. Bravo <german.mb@deipi.com>
2017-05-18 11:06:38 -0500

    Formatting

--- src/memory_stats.cc
+++ src/memory_stats.cc
@@ -55,10 +55,10 @@ std::pair<int64_t, int64_t> get_current_ram()
 	count = sizeof(vm_stats) / sizeof(natural_t);
 	if (KERN_SUCCESS == host_page_size(mach_port, &page_size) &&
 		KERN_SUCCESS == host_statistics64(mach_port, HOST_VM_INFO, (host_info64_t)&vm_stats, &count)) {
-		int64_t free_memory = (int64_t)vm_stats.free_count * (int64_t)page_size;
+		int64_t free_memory = ((int64_t)vm_stats.free_count * (int64_t)page_size);
 		int64_t used_memory = ((int64_t)vm_stats.active_count +
-								 (int64_t)vm_stats.inactive_count +
-								 (int64_t)vm_stats.wire_count) *  (int64_t)page_size;
+							   (int64_t)vm_stats.inactive_count +
+							   (int64_t)vm_stats.wire_count) * (int64_t)page_size;
 		return std::make_pair(used_memory, free_memory);
 	}
 #endif
