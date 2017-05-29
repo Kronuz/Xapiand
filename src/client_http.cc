9b0c879895d7b500d349465624596f14e97e108e
German M. Bravo <german.mb@deipi.com>
2017-05-29 14:18:36 -0500

    Allow .search to have selectors

--- src/client_http.cc
+++ src/client_http.cc
@@ -1185,7 +1185,7 @@ HttpClient::meta_view(enum http_method method, Command)
 
 	std::string key = path_parser.get_pmt();
 	std::string selector;
-	auto needle = key.find_first_of(".{");
+	auto needle = key.find_first_of(".{", 1);
 	if (needle != std::string::npos) {
 		selector = key.substr(needle);
 		key = key.substr(0, needle);
@@ -1390,10 +1390,18 @@ HttpClient::search_view(enum http_method method, Command)
 
 	std::string id = path_parser.get_id();
 	std::string selector;
-	auto needle = id.find_first_of(".{");
-	if (needle != std::string::npos) {
-		selector = id.substr(needle);
-		id = id.substr(0, needle);
+	if (id.empty()) {
+		auto cmd = path_parser.get_cmd();
+		auto needle = cmd.find_first_of(".{", 1);
+		if (needle != std::string::npos) {
+			selector = cmd.substr(needle);
+		}
+	} else {
+		auto needle = id.find_first_of(".{", 1);
+		if (needle != std::string::npos) {
+			selector = id.substr(needle);
+			id = id.substr(0, needle);
+		}
 	}
 
 	endpoints_maker(1s);
