dafb9d193b3985a98f66d3cd006434dab9af0e96
German M. Bravo <german.mb@deipi.com>
2017-11-27 17:58:36 -0600

    base-x updated (le)

--- src/base_x.hh
+++ src/base_x.hh
@@ -54,10 +54,11 @@ class BaseX {
 	}
 
 public:
-	static constexpr int ignore_case = 1;
-	static constexpr int with_checksum = 2;
-	static constexpr int with_check = 4;
-	static constexpr int block_padding = 8;
+	static constexpr int ignore_case =   (1 << 0);
+	static constexpr int little_endian = (1 << 1);
+	static constexpr int with_checksum = (1 << 2);
+	static constexpr int with_check =    (1 << 3);
+	static constexpr int block_padding = (1 << 4);
 
 	template <std::size_t alphabet_size1, std::size_t extended_size1, std::size_t padding_size1, std::size_t translate_size1>
 	constexpr BaseX(int flags, const char (&alphabet)[alphabet_size1], const char (&extended)[extended_size1], const char (&padding_string)[padding_size1], const char (&translate)[translate_size1]) :
@@ -171,7 +172,9 @@ public:
 					quotient = std::move(r.first);
 				} while (quotient);
 			}
-			std::reverse(result.begin(), result.end());
+			if (!(flags & BaseX::little_endian)) {
+				std::reverse(result.begin(), result.end());
+			}
 			if (padding_size) {
 				Result p;
 				p.resize((padding_size - (result.size() % padding_size)) % padding_size, padding);
@@ -240,6 +243,9 @@ public:
 	void decode(uinteger_t& result, const char* encoded, std::size_t encoded_size) const {
 		result = 0;
 		int sum = 0;
+		int direction = 1;
+		const char* tmple;
+
 		if (flags & BaseX::with_checksum) {
 			auto sz = encoded_size - 1;
 			sz = (sz + sz / size) % size;
@@ -251,10 +257,17 @@ public:
 			--encoded_size;
 		}
 
+		if (flags & BaseX::little_endian) {
+			direction = -1;
+			tmple = encoded + encoded_size;
+			encoded = tmple - 1;
+			for (; encoded_size && *encoded == padding; --encoded_size, --encoded);
+		}
+
 		int bp = 0;
 
 		if (base_bits) {
-			for (; encoded_size; --encoded_size, ++encoded) {
+			for (; encoded_size; --encoded_size, encoded += direction) {
 				auto c = *encoded;
 				if (c == padding) break;
 				auto d = ord(static_cast<int>(c));
@@ -268,7 +281,7 @@ public:
 			}
 		} else {
 			uinteger_t uint_base = base;
-			for (; encoded_size; --encoded_size, ++encoded) {
+			for (; encoded_size; --encoded_size, encoded += direction) {
 				auto c = *encoded;
 				if (c == padding) break;
 				auto d = ord(static_cast<int>(c));
@@ -282,7 +295,11 @@ public:
 			}
 		}
 
-		for (; encoded_size && *encoded == padding; --encoded_size, ++encoded);
+		if (flags & BaseX::little_endian) {
+			encoded = tmple;
+		} else {
+			for (; encoded_size && *encoded == padding; --encoded_size, ++encoded);
+		}
 
 		result >>= (bp & 7);
 
@@ -392,6 +409,14 @@ struct Base2 {
 		return encoder;
 	}
 
+	static const BaseX& base2_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "01", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base2chk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "01", "", "", "");
+		return encoder;
+	}
 };
 
 // base8
@@ -404,7 +429,15 @@ struct Base8 {
 		static constexpr BaseX encoder(BaseX::with_checksum, "01234567", "", "", "");
 		return encoder;
 	}
-};
+
+	static const BaseX& base8_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "01234567", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base8chk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "01234567", "", "", "");
+		return encoder;
+	}};
 
 // base11
 struct Base11 {
@@ -414,6 +447,15 @@ struct Base11 {
 	}
 	static const BaseX& base11chk() {
 		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::with_checksum, "0123456789a", "", "", "");
+		return encoder;
+	}
+
+	static const BaseX& base11_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::little_endian, "0123456789a", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base11chk_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::with_checksum | BaseX::little_endian, "0123456789a", "", "", "");
 		return encoder;
 	}
 };
@@ -430,6 +472,15 @@ struct Base16 {
 	}
 	static const BaseX& rfc4648() {
 		static constexpr BaseX encoder(0, "0123456789ABCDEF", "", "", "");
+		return encoder;
+	}
+
+	static const BaseX& base16_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::little_endian, "0123456789abcdef", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base16chk_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::with_checksum | BaseX::little_endian, "0123456789abcdef", "", "", "");
 		return encoder;
 	}
 };
@@ -460,6 +511,23 @@ struct Base32 {
 		static constexpr BaseX encoder(BaseX::block_padding, "0123456789ABCDEFGHIJKLMNOPQRSTUV", "", "========", "\n\r");
 		return encoder;
 	}
+
+	static const BaseX& base32_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::little_endian, "0123456789abcdefghijklmnopqrstuv", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base32chk_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::with_checksum | BaseX::little_endian, "0123456789abcdefghijklmnopqrstuv", "", "", "");
+		return encoder;
+	}
+	static const BaseX& crockford_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::little_endian, "0123456789ABCDEFGHJKMNPQRSTVWXYZ", "", "", "-0O1IL");
+		return encoder;
+	}
+	static const BaseX& crockfordchk_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::with_check | BaseX::little_endian, "0123456789ABCDEFGHJKMNPQRSTVWXYZ", "*~$=U", "", "-0O1IL");
+		return encoder;
+	}
 };
 
 // base36
@@ -470,6 +538,15 @@ struct Base36 {
 	}
 	static const BaseX& base36chk() {
 		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::with_checksum, "0123456789abcdefghijklmnopqrstuvwxyz", "", "", "");
+		return encoder;
+	}
+
+	static const BaseX& base36_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::little_endian, "0123456789abcdefghijklmnopqrstuvwxyz", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base36chk_le() {
+		static constexpr BaseX encoder(BaseX::ignore_case | BaseX::with_checksum | BaseX::little_endian, "0123456789abcdefghijklmnopqrstuvwxyz", "", "", "");
 		return encoder;
 	}
 };
@@ -508,6 +585,39 @@ struct Base58 {
 		static constexpr BaseX encoder(BaseX::with_checksum, "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", "", "", "");
 		return encoder;
 	}
+
+	static const BaseX& base58_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base58chk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv", "", "", "");
+		return encoder;
+	}
+	static const BaseX& bitcoin_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz", "", "", "");
+		return encoder;
+	}
+	static const BaseX& bitcoinchk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz", "", "", "");
+		return encoder;
+	}
+	static const BaseX& ripple_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz", "", "", "");
+		return encoder;
+	}
+	static const BaseX& ripplechk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz", "", "", "");
+		return encoder;
+	}
+	static const BaseX& flickr_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", "", "", "");
+		return encoder;
+	}
+	static const BaseX& flickrchk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ", "", "", "");
+		return encoder;
+	}
 };
 
 // base62
@@ -526,6 +636,23 @@ struct Base62 {
 	}
 	static const BaseX& invertedchk() {
 		static constexpr BaseX encoder(BaseX::with_checksum, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "", "", "");
+		return encoder;
+	}
+
+	static const BaseX& base62_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base62chk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", "", "", "");
+		return encoder;
+	}
+	static const BaseX& inverted_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "", "", "");
+		return encoder;
+	}
+	static const BaseX& invertedchk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ", "", "", "");
 		return encoder;
 	}
 };
@@ -556,6 +683,23 @@ struct Base64 {
 		static constexpr BaseX encoder(BaseX::block_padding, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_", "", "====", "\n\r");
 		return encoder;
 	}
+
+	static const BaseX& base64_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base64chk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/", "", "", "");
+		return encoder;
+	}
+	static const BaseX& url_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_", "", "", "");
+		return encoder;
+	}
+	static const BaseX& urlchk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_", "", "", "");
+		return encoder;
+	}
 };
 
 // base66
@@ -566,6 +710,15 @@ struct Base66 {
 	}
 	static const BaseX& base66chk() {
 		static constexpr BaseX encoder(BaseX::with_checksum, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.!~", "", "", "");
+		return encoder;
+	}
+
+	static const BaseX& base66_le() {
+		static constexpr BaseX encoder(BaseX::little_endian, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.!~", "", "", "");
+		return encoder;
+	}
+	static const BaseX& base66chk_le() {
+		static constexpr BaseX encoder(BaseX::with_checksum | BaseX::little_endian, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.!~", "", "", "");
 		return encoder;
 	}
 };
