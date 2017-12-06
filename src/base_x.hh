2456e5ca17fbc5549549dd79aad3c1ed23564ae5
German M. Bravo <german.mb@deipi.com>
2017-12-05 18:35:10 -0600

    Anonymous UUID is 'notmet'

--- src/base_x.hh
+++ src/base_x.hh
@@ -540,7 +540,7 @@ struct Base59 {
 		return encoder;
 	}
 	static const BaseX& dubaluchk() {
-		static constexpr BaseX encoder(BaseX::with_checksum, "zy9MalDxwpKLdnW2APJscgbYUq6jht7Ee3TiX5vZRkVCr4uBHoGSQf8FNmO", "", "", "-l1IO0");
+		static constexpr BaseX encoder(BaseX::with_checksum, "zy9MalDxwpKLdvW2AtmscgbYUq6jhP7E53TiXenZRkVCrouBH4GSQf8FNJO", "", "", "-l1IO0");
 		return encoder;
 	}
 };
