5c4fd83cb244fc0926202c34aaff00ebd1c52a6b
Kronuz <german.mb@gmail.com>
2018-02-15 12:12:04 -0600

    Added memory allocator for tracking memory usage

---
+++ src/allocator.cc
@@ -0,0 +1,224 @@
+/*
+ * Copyright (C) 2015-2018 dubalu.com LLC. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "xapiand.h"
+
+#include "allocator.h"
+
+#ifdef XAPIAND_TRACKED_MEM
+
+#include <atomic>         // for std::atomic_llong
+#include <cassert>        // for assert
+#include <cstdlib>        // for std::malloc, std::free
+#include <functional>     // for std::hash
+#include <mutex>          // for std::mutex
+#include <new>            // for std::nothrow_t, std::new_handler
+#include <thread>         // for std::this_thread
+#include <unordered_map>  // for std::unordered_map
+#include <utility>        // for std::make_pair
+#include <utility>        // for std::pair
+
+
+#if defined(_MSC_VER) || (defined(__GNUC__) && __GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 8) || (defined(__llvm__) && !defined(_LIBCPP_VERSION))
+# define HAS_THREAD_LOCAL
+#elif defined(__clang__)
+# if __has_feature(cxx_thread_local)
+#   define HAS_THREAD_LOCAL
+# endif
+#endif
+#if defined(__FreeBSD__) && (__FreeBSD__ < 11)  //Bug in freeBSD lower that 11v. Use of thread_local produces linking error
+# undef HAS_THREAD_LOCAL
+#endif
+
+
+namespace allocator {
+	// allocate/deallocate using ::malloc/::free
+
+	void* allocate(std::size_t size) noexcept {
+    	if (size == 0) {
+        	size = 1;
+    	}
+    	void* p;
+    	while ((p = std::malloc(size)) == 0) {
+        	// If malloc fails and there is a new_handler, call it to try free up memory.
+        	std::new_handler nh = std::get_new_handler();
+        	if (nh) {
+        		nh();
+        	} else {
+        		break;
+        	}
+    	}
+    	return p;
+    }
+
+    void deallocate(void* p) noexcept {
+    	if (p) {
+    		std::free(p);
+    	}
+    }
+
+
+    // VanillaAllocator
+
+	void* VanillaAllocator::allocate(std::size_t size) noexcept {
+		void* p = ::allocator::allocate(size + sizeof(std::size_t));
+		if (!p) return nullptr;
+		// fprintf(stderr, "[allocated %zu bytes]\n", size);
+		return p;
+	}
+
+	void VanillaAllocator::deallocate(void* p) noexcept {
+		// fprintf(stderr, "[freed]\n");
+		::allocator::deallocate(p);
+	}
+
+
+	// TrackedAllocator
+
+	std::atomic_llong& _total_allocated() noexcept {
+		static std::atomic_llong t_allocated;
+		return t_allocated;
+	}
+
+#ifdef HAS_THREAD_LOCAL
+	static long long& _local_allocated() noexcept {
+		thread_local static long long l_allocated;
+		return l_allocated;
+	}
+
+	long long local_allocated() noexcept {
+		return _local_allocated();
+	}
+
+#else
+	static std::mutex tracked_mutex;
+	static std::unordered_map<
+		std::thread::id,
+		long long,
+		std::hash<std::thread::id>,
+		std::equal_to<std::thread::id>,
+		allocator<std::pair<const std::thread::id, long long>, VanillaAllocator>
+	> tracked_sizes;
+
+	static long long& __local_allocated() noexcept {
+		const auto id = std::this_thread::get_id();
+		auto it = tracked_sizes.find(id);
+		if (it != tracked_sizes.end()) return it->second;
+		auto& l_allocated = tracked_sizes.emplace(id, 0).first->second;
+		return l_allocated;
+	}
+
+	static long long& _local_allocated() noexcept {
+		std::lock_guard<std::mutex> lock(tracked_mutex);
+		return __local_allocated();
+	}
+
+	long long local_allocated() noexcept {
+		std::lock_guard<std::mutex> lock(tracked_mutex);
+		std::size_t l_allocated = __local_allocated();
+		return l_allocated;
+	}
+#endif
+
+	long long total_allocated() noexcept {
+		return _total_allocated();
+	}
+
+	void* TrackedAllocator::allocate(std::size_t size) noexcept {
+		auto& t_allocated = _total_allocated();
+		auto& l_allocated = _local_allocated();
+		void* p = ::allocator::allocate(size + sizeof(std::size_t));
+		if (!p) return nullptr;
+		t_allocated += size;
+		l_allocated += size;
+		// fprintf(stderr, "[allocated %zu bytes, %lld [%lld] are now allocated]\n", size, l_allocated, t_allocated.load());
+		*static_cast<std::size_t*>(p) = size;
+		return static_cast<char*>(p) + sizeof(std::size_t);
+	}
+
+	void TrackedAllocator::deallocate(void* p) noexcept {
+		assert(p);
+		auto& t_allocated = _total_allocated();
+		auto& l_allocated = _local_allocated();
+		p = static_cast<char*>(p) - sizeof(std::size_t);
+		std::size_t size = *static_cast<std::size_t*>(p);
+		t_allocated -= size;
+		l_allocated -= size;
+		// fprintf(stderr, "[freed %zu bytes, %lld [%lld] remain allocated]\n", size, l_allocated, t_allocated.load());
+		::allocator::deallocate(p);
+	}
+}
+
+// Operators overload for tracking
+
+void* operator new(std::size_t size) throw(std::bad_alloc) {
+	void* p = allocator::TrackedAllocator::allocate(size);
+	if (!p) {
+		static const std::bad_alloc nomem;
+		throw nomem;
+	}
+	return p;
+}
+
+
+void* operator new(std::size_t size, const std::nothrow_t&) noexcept {
+	return allocator::TrackedAllocator::allocate(size);
+}
+
+
+void* operator new[](std::size_t size) throw(std::bad_alloc) {
+	void* p = allocator::TrackedAllocator::allocate(size);
+	if (!p) {
+		static const std::bad_alloc nomem;
+		throw nomem;
+	}
+	return p;
+}
+
+
+void* operator new[](std::size_t size, const std::nothrow_t&) noexcept {
+	return allocator::TrackedAllocator::allocate(size);
+}
+
+
+void operator delete(void* p) noexcept {
+	return allocator::TrackedAllocator::deallocate(p);
+}
+
+
+void operator delete[](void* p) noexcept {
+	return allocator::TrackedAllocator::deallocate(p);
+}
+
+#else  /* XAPIAND_TRACKED_MEM */
+
+namespace allocator {
+	long long total_allocated() noexcept {
+		return 0;
+	}
+
+	long long local_allocated() noexcept {
+		return 0;
+	}
+}
+
+#endif
---
+++ src/allocator.h
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2015-2018 dubalu.com LLC. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <cstddef>        // for std::size_t
+#include <limits>         // for std::numeric_limits
+#include <new>            // for std::bad_alloc
+
+
+namespace allocator {
+	long long total_allocated() noexcept;
+	long long local_allocated() noexcept;
+
+	class VanillaAllocator {
+	public:
+		static void* allocate(std::size_t size) noexcept;
+		static void deallocate(void *p) noexcept;
+	};
+
+	class TrackedAllocator {
+	public:
+		static void* allocate(std::size_t size) noexcept;
+		static void deallocate(void *p) noexcept;
+	};
+
+	template <typename _Tp, typename _Allocator>
+	class allocator {
+	public:
+		// type definitions
+		typedef size_t     size_type;
+		typedef ptrdiff_t  difference_type;
+		typedef _Tp*       pointer;
+		typedef const _Tp* const_pointer;
+		typedef _Tp&       reference;
+		typedef const _Tp& const_reference;
+		typedef _Tp        value_type;
+
+		// rebind allocator to type _Tp1
+		template<typename _Tp1>
+		struct rebind { typedef allocator<_Tp1, _Allocator> other; };
+
+		// constructors and destructor - nothing to do because the allocator has no state
+		allocator() throw() { }
+		allocator(const allocator&) throw() { }
+		template<typename _Tp1>
+		allocator(const allocator<_Tp1, _Allocator>&) throw() { }
+		~allocator() throw() { }
+
+		// return address of values
+		pointer address(reference __x) const { return &__x; }
+		const_pointer address(const_reference __x) const { return &__x; }
+
+		// allocate but don't initialize num elements of type _Tp
+		pointer allocate(size_type __n, const void* = static_cast<const void*>(0))
+		{
+			if (__n > max_size()) {
+				static const std::bad_alloc nomem;
+				throw nomem;
+			}
+			return static_cast<_Tp*>(_Allocator::allocate(__n * sizeof(_Tp)));
+		}
+
+		// deallocate storage __p of deleted elements
+		void deallocate(pointer __p, size_type) {
+			_Allocator::deallocate(__p);
+		}
+
+		// return maximum number of elements that can be allocated
+		size_type max_size() const throw() {
+			return std::numeric_limits<std::size_t>::max() / sizeof(_Tp);
+		}
+
+		// initialize elements of allocated storage __p with value value
+		void construct(pointer __p, const _Tp& __val) {
+			::new(__p) _Tp(__val);
+		}
+
+		// destroy elements of initialized storage __p
+		void destroy(pointer __p) {
+			__p->~_Tp();
+		}
+	};
+
+	template<typename _Tp, typename _Allocator>
+	inline bool operator==(const allocator<_Tp, _Allocator>&, const allocator<_Tp, _Allocator>&) { return true; }
+	template<typename _Tp, typename _Allocator>
+	inline bool operator!=(const allocator<_Tp, _Allocator>&, const allocator<_Tp, _Allocator>&) { return false; }
+}
