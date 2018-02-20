fa319d0356c920f486cade1b5f3273e0f6ac9dd5
Kronuz <german.mb@gmail.com>
2018-02-20 15:01:39 -0600

    Traceback: Try using atos to get filename and line numbers

--- src/exception.cc
+++ src/exception.cc
@@ -32,6 +32,93 @@
 
 #define BUFFER_SIZE 1024
 
+#ifdef __APPLE__
+#include <util.h>
+#include <dlfcn.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/select.h>
+/* Use `atos` to do symbol lookup, can lookup non-dynamic symbols and also line
+ * numbers. This function is more complicated than you'd expect because `atos`
+ * doesn't flush after each line, so plain pipe() or socketpair() won't work
+ * until we close the write side. But the whole point is we want to keep `atos`
+ * around so we don't have to reprocess the symbol table over and over. What we
+ * wind up doing is using `forkpty()` to make a new pseudoterminal for atos to
+ * run in, and thus will use line-buffering for stdout, and then we can get
+ * each line.
+ */
+inline static std::string atos(string_view address) {
+	static int fd = -1;
+	if (fd == -1) {
+		Dl_info info;
+		memset(&info, 0, sizeof(Dl_info));
+		if (!dladdr((const void*)&atos, &info)) {
+			perror("Could not get base address for `atos`.");
+			return "";
+		}
+
+		struct termios term_opts;
+		cfmakeraw(&term_opts);  // have to set this first, otherwise queries echo until child kicks in
+		pid_t childpid;
+		if unlikely((childpid = forkpty(&fd, NULL, &term_opts, NULL)) < 0) {
+			perror("Could not forkpty for `atos` call.");
+			return "";
+		}
+
+		if (childpid == 0) {
+			char base[20];
+			snprintf(base, 20, "%p", info.dli_fbase);
+			execlp("/usr/bin/atos", "atos", "-o", info.dli_fname, "-l", base, (char*)0);
+			fprintf(stderr,"Could not exec `atos` for stack trace!\n");
+			exit(1);
+		}
+
+		write(fd, address.data(), address.size());
+		write(fd, "\n", 1);
+
+		// atos can take a while to parse symbol table on first request, which
+		// is why we leave it running if we see a delay, explain what's going on...
+		fd_set fds;
+		FD_ZERO(&fds);
+		FD_SET(fd, &fds);
+		struct timeval tv = {3, 0};
+		int err = select(fd + 1, &fds, nullptr, nullptr, &tv);
+		if unlikely(err < 0) {
+			perror("Generating... first call takes some time for `atos` to cache the symbol table.");
+		} else if (err == 0) {  // timeout
+			fprintf(stderr, "Generating... first call takes some time for `atos` to cache the symbol table.\n");
+		}
+	} else {
+		write(fd, address.data(), address.size());
+		write(fd, "\n", 1);
+	}
+
+	const unsigned int MAXLINE = 1024;
+	char line[MAXLINE];
+	char c = '\0';
+	size_t nread = 0;
+	while (c != '\n' && nread < MAXLINE) {
+		if unlikely(read(fd, &c, 1) <= 0) {
+			perror("Lost `atos` connection.");
+			close(fd);
+			fd = -1;
+			return "";
+		}
+		if (c != '\n') {
+			line[nread++] = c;
+		}
+	}
+	if (nread < MAXLINE) {
+		return std::string(line, nread);
+	}
+	return "";
+}
+#else
+inline static std::string atos(string_view address) {
+	return "";
+}
+#endif
+
 
 std::string
 traceback(string_view filename, int line, void *const * callstack, int frames)
@@ -54,23 +141,32 @@ traceback(string_view filename, int line, void *const * callstack, int frames)
 	// iterate over the returned symbol lines. skip the first, it is the
 	// address of this function.
 	for (int i = 1; i < frames; ++i) {
-		int status = 0;
 		const char *sep = "\t ()+";
 		char *mangled, *lasts;
 		std::string result;
 		for (mangled = strtok_r(strs[i], sep, &lasts); mangled; mangled = strtok_r(nullptr, sep, &lasts)) {
+			int status = 0;
 			char* unmangled = abi::__cxa_demangle(mangled, nullptr, 0, &status);
 			if (!result.empty()) {
-				result += " ";
+				result.push_back(' ');
 			}
-			if (status == 0) {
-				result += unmangled;
+			if (status) {
+				string_view address(mangled);
+				if (address.size() > 2 && address.compare(0, 2, "0x") == 0) {
+					auto tmp = atos(address);
+					if (tmp.size() > 2 && tmp.compare(0, 2, "0x") != 0) {
+						result.append(tmp);
+						break;
+					}
+				}
+				result.append(mangled);
 			} else {
-				result += mangled;
+				result.append(unmangled);
+				free(unmangled);
 			}
-			free(unmangled);
 		}
-		tb += "\n    " + result;
+		tb.append("\n    ");
+		tb.append(result);
 	}
 
 	free(strs);
