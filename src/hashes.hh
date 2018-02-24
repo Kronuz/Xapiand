d080728d8face80b67b250ebf439fecdc38e1a65
German M. Bravo <german.mb@deipi.com>
2018-02-24 15:26:53 -0600

    Code styling

--- src/hashes.hh
+++ src/hashes.hh
@@ -40,92 +40,94 @@
  * /_/\_\/_/\_\ |_| |_|\__,_|___/_| |_|
  */
 class xxh64 {
-	static constexpr uint64_t PRIME1 = 11400714785074694791ULL;
-	static constexpr uint64_t PRIME2 = 14029467366897019727ULL;
-	static constexpr uint64_t PRIME3 =  1609587929392839161ULL;
-	static constexpr uint64_t PRIME4 =  9650029242287828579ULL;
-	static constexpr uint64_t PRIME5 =  2870177450012600261ULL;
+	static constexpr std::uint64_t PRIME1 = 11400714785074694791ULL;
+	static constexpr std::uint64_t PRIME2 = 14029467366897019727ULL;
+	static constexpr std::uint64_t PRIME3 =  1609587929392839161ULL;
+	static constexpr std::uint64_t PRIME4 =  9650029242287828579ULL;
+	static constexpr std::uint64_t PRIME5 =  2870177450012600261ULL;
 
-	static constexpr uint64_t rotl(uint64_t x, int r) {
+	static constexpr std::uint64_t rotl(std::uint64_t x, int r) {
 		return ((x << r) | (x >> (64 - r)));
 	}
-	static constexpr uint64_t mix1(const uint64_t h, const uint64_t prime, int rshift) {
+	static constexpr std::uint64_t mix1(const std::uint64_t h, const std::uint64_t prime, int rshift) {
 		return (h ^ (h >> rshift)) * prime;
 	}
-	static constexpr uint64_t mix2(const uint64_t p, const uint64_t v = 0) {
+	static constexpr std::uint64_t mix2(const std::uint64_t p, const std::uint64_t v = 0) {
 		return rotl (v + p * PRIME2, 31) * PRIME1;
 	}
-	static constexpr uint64_t mix3(const uint64_t h, const uint64_t v) {
+	static constexpr std::uint64_t mix3(const std::uint64_t h, const std::uint64_t v) {
 		return (h ^ mix2 (v)) * PRIME1 + PRIME4;
 	}
 #ifdef XXH64_BIG_ENDIAN
-	static constexpr uint32_t endian32(const char *v) {
-		return uint32_t(uint8_t(v[3]))|(uint32_t(uint8_t(v[2]))<<8)
-			   |(uint32_t(uint8_t(v[1]))<<16)|(uint32_t(uint8_t(v[0]))<<24);
+	static constexpr std::uint32_t endian32(const char *v) {
+		return std::uint32_t(std::uint8_t(v[3]))|(std::uint32_t(std::uint8_t(v[2]))<<8)
+			   |(std::uint32_t(std::uint8_t(v[1]))<<16)|(std::uint32_t(std::uint8_t(v[0]))<<24);
 	}
-	static constexpr uint64_t endian64(const char *v) {
-		return uint64_t(uint8_t(v[7]))|(uint64_t(uint8_t(v[6]))<<8)
-			   |(uint64_t(uint8_t(v[5]))<<16)|(uint64_t(uint8_t(v[4]))<<24)
-			   |(uint64_t(uint8_t(v[3]))<<32)|(uint64_t(uint8_t(v[2]))<<40)
-			   |(uint64_t(uint8_t(v[1]))<<48)|(uint64_t(uint8_t(v[0]))<<56);
+	static constexpr std::uint64_t endian64(const char *v) {
+		return std::uint64_t(std::uint8_t(v[7]))|(std::uint64_t(std::uint8_t(v[6]))<<8)
+			   |(std::uint64_t(std::uint8_t(v[5]))<<16)|(std::uint64_t(std::uint8_t(v[4]))<<24)
+			   |(std::uint64_t(std::uint8_t(v[3]))<<32)|(std::uint64_t(std::uint8_t(v[2]))<<40)
+			   |(std::uint64_t(std::uint8_t(v[1]))<<48)|(std::uint64_t(std::uint8_t(v[0]))<<56);
 	}
 #else
-	static constexpr uint32_t endian32(const char *v) {
-		return uint32_t(uint8_t(v[0]))|(uint32_t(uint8_t(v[1]))<<8)
-			   |(uint32_t(uint8_t(v[2]))<<16)|(uint32_t(uint8_t(v[3]))<<24);
+	static constexpr std::uint32_t endian32(const char *v) {
+		return std::uint32_t(std::uint8_t(v[0]))|(std::uint32_t(std::uint8_t(v[1]))<<8)
+			   |(std::uint32_t(std::uint8_t(v[2]))<<16)|(std::uint32_t(std::uint8_t(v[3]))<<24);
 	}
-	static constexpr uint64_t endian64 (const char *v) {
-		return uint64_t(uint8_t(v[0]))|(uint64_t(uint8_t(v[1]))<<8)
-			   |(uint64_t(uint8_t(v[2]))<<16)|(uint64_t(uint8_t(v[3]))<<24)
-			   |(uint64_t(uint8_t(v[4]))<<32)|(uint64_t(uint8_t(v[5]))<<40)
-			   |(uint64_t(uint8_t(v[6]))<<48)|(uint64_t(uint8_t(v[7]))<<56);
+	static constexpr std::uint64_t endian64 (const char *v) {
+		return std::uint64_t(std::uint8_t(v[0]))|(std::uint64_t(std::uint8_t(v[1]))<<8)
+			   |(std::uint64_t(std::uint8_t(v[2]))<<16)|(std::uint64_t(std::uint8_t(v[3]))<<24)
+			   |(std::uint64_t(std::uint8_t(v[4]))<<32)|(std::uint64_t(std::uint8_t(v[5]))<<40)
+			   |(std::uint64_t(std::uint8_t(v[6]))<<48)|(std::uint64_t(std::uint8_t(v[7]))<<56);
 	}
 #endif
-	static constexpr uint64_t fetch64(const char *p, const uint64_t v = 0) {
+	static constexpr std::uint64_t fetch64(const char *p, const std::uint64_t v = 0) {
 		return mix2 (endian64 (p), v);
 	}
-	static constexpr uint64_t fetch32(const char *p) {
-		return uint64_t (endian32 (p)) * PRIME1;
+	static constexpr std::uint64_t fetch32(const char *p) {
+		return std::uint64_t (endian32 (p)) * PRIME1;
 	}
-	static constexpr uint64_t fetch8(const char *p) {
-		return uint8_t (*p) * PRIME5;
+	static constexpr std::uint64_t fetch8(const char *p) {
+		return std::uint8_t (*p) * PRIME5;
 	}
-	static constexpr uint64_t finalize(const uint64_t h, const char *p, uint64_t len) {
+	static constexpr std::uint64_t finalize(const std::uint64_t h, const char *p, std::uint64_t len) {
 		return (len >= 8) ? (finalize (rotl (h ^ fetch64 (p), 27) * PRIME1 + PRIME4, p + 8, len - 8)) :
 			   ((len >= 4) ? (finalize (rotl (h ^ fetch32 (p), 23) * PRIME2 + PRIME3, p + 4, len - 4)) :
 				((len > 0) ? (finalize (rotl (h ^ fetch8 (p), 11) * PRIME1, p + 1, len - 1)) :
 				 (mix1 (mix1 (mix1 (h, PRIME2, 33), PRIME3, 29), 1, 32))));
 	}
-	static constexpr uint64_t h32bytes(const char *p, uint64_t len, const uint64_t v1,const uint64_t v2, const uint64_t v3, const uint64_t v4) {
+	static constexpr std::uint64_t h32bytes(const char *p, std::uint64_t len, const std::uint64_t v1,const std::uint64_t v2, const std::uint64_t v3, const std::uint64_t v4) {
 		return (len >= 32) ? h32bytes (p + 32, len - 32, fetch64 (p, v1), fetch64 (p + 8, v2), fetch64 (p + 16, v3), fetch64 (p + 24, v4)) :
 			   mix3 (mix3 (mix3 (mix3 (rotl (v1, 1) + rotl (v2, 7) + rotl (v3, 12) + rotl (v4, 18), v1), v2), v3), v4);
 	}
-	static constexpr uint64_t h32bytes(const char *p, uint64_t len, const uint64_t seed) {
+	static constexpr std::uint64_t h32bytes(const char *p, std::uint64_t len, const std::uint64_t seed) {
 		return h32bytes (p, len, seed + PRIME1 + PRIME2, seed + PRIME2, seed, seed - PRIME1);
 	}
 
 public:
-	static constexpr uint64_t hash(const char *p, uint64_t len, uint64_t seed = 0) {
+	static constexpr std::uint64_t hash(const char *p, std::uint64_t len, std::uint64_t seed = 0) {
 		return finalize((len >= 32 ? h32bytes(p, len, seed) : seed + PRIME5) + len, p + (len & ~0x1F), len & 0x1F);
 	}
 
 	template <size_t N>
-	static constexpr uint64_t hash(const char(&s)[N], uint64_t seed = 0) {
+	static constexpr std::uint64_t hash(const char(&s)[N], std::uint64_t seed = 0) {
 		return hash(s, N - 1, seed);
 	}
 
 #ifdef STATIC_STRING_H
 	template <std::size_t SN, typename ST>
-	static constexpr uint64_t hash(const static_string::static_string<SN, ST>& str, uint64_t seed = 0) {
+	static constexpr std::uint64_t hash(const static_string::static_string<SN, ST>& str, std::uint64_t seed = 0) {
 		return hash(str.data(), str.size(), seed);
 	}
 #endif
 
 #ifdef STRING_VIEW_HH
-	static uint64_t hash(string_view str, uint64_t seed = 0) {
+	static std::uint64_t hash(string_view str, std::uint64_t seed = 0) {
+#else
+	static std::uint64_t hash(const std::string& str, std::uint64_t seed = 0) {
+#endif
 		return XXH64(str.data(), str.size(), seed);
 	}
-#endif
 };
 
 
@@ -135,20 +137,22 @@ class xxh32 {
 public:
 #ifdef STATIC_STRING_H
 	template <std::size_t SN, typename ST>
-	static constexpr uint32_t hash(const static_string::static_string<SN, ST>& str, uint32_t seed = 0) {
+	static constexpr std::uint32_t hash(const static_string::static_string<SN, ST>& str, std::uint32_t seed = 0) {
 		return hash(str.data(), str.size(), seed);
 	}
 #endif
 
 #ifdef STRING_VIEW_HH
-	static uint32_t hash(string_view str, uint32_t seed = 0) {
+	static std::uint32_t hash(string_view str, std::uint32_t seed = 0) {
+#else
+	static std::uint32_t hash(const std::string& str, std::uint32_t seed = 0) {
+#endif
 		return XXH32(str.data(), str.size(), seed);
 	}
-#endif
 };
 
 
-constexpr uint32_t operator"" _XXH(const char* s, size_t size) {
+constexpr std::uint32_t operator"" _XXH(const char* s, size_t size) {
 	return xxh64::hash(s, size, 0);
 }
 
@@ -179,13 +183,15 @@ struct fnv1ah {
 
 #ifdef STRING_VIEW_HH
 	static T hash(string_view str, T seed = offset) {
+#else
+	static T hash(const std::string& str, T seed = offset) {
+#endif
         T hash = seed;
 		for (char c : str) {
             hash = (hash ^ static_cast<unsigned char>(c)) * prime;
         }
         return hash;
     }
-#endif
 };
 using fnv1ah16 = fnv1ah<std::uint16_t, 0x21dU, 51363UL>;  // shouldn't exist, figured out the prime and offset
 using fnv1ah32 = fnv1ah<std::uint32_t, 0x1000193UL, 2166136261UL>;
@@ -220,13 +226,15 @@ struct djb2h {
 
 #ifdef STRING_VIEW_HH
 	static T hash(string_view str, T seed = offset) {
+#else
+	static T hash(const std::string& str, T seed = offset) {
+#endif
 		T hash = seed;
 		for (char c : str) {
 			hash = (hash * mul) + static_cast<unsigned char>(c);
 		}
 		return hash;
 	}
-#endif
 };
 
 // from https://stackoverflow.com/a/41849998/167522, used primeth with bits size
