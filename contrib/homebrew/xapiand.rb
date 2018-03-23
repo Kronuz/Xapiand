e38b7b368c05fc2321856151856445954a4a70b0
German M. Bravo <german.mb@dubalu.com>
2018-03-23 08:41:33 -0600

    Homebrew formula updated for v1.0.0

--- contrib/homebrew/xapiand.rb
+++ contrib/homebrew/xapiand.rb
@@ -1,11 +1,8 @@
-# Homebrew Formula for Xapiand
-# Usage: brew install --HEAD https://github.com/Kronuz/Xapiand/raw/master/contrib/homebrew/xapiand.rb
-
 class Xapiand < Formula
   desc "Xapiand: A RESTful Search Engine"
   homepage "http://kronuz.io/Xapiand"
   url "https://github.com/Kronuz/Xapiand/archive/v1.0.0.tar.gz"
-  sha256 "c2aba1b5a0c677193bb4fcb2013b6f87f2fe43d0117f7cfcc0e885bf109a7676"
+  sha256 "0cf3b349e99b48882e11ae0cc2985a7ea159dd24b82e7fd03b7d6072ce38b5e4"
   head "https://github.com/Kronuz/Xapiand.git"
 
   depends_on "cmake" => :build
@@ -17,5 +14,9 @@ class Xapiand < Formula
       system "cmake", "..", "-DCCACHE_FOUND=CCACHE_FOUND-NOTFOUND", *std_cmake_args
       system "make", "install"
     end
+  end
+
+  test do
+    system bin/"xapiand", "--version"
   end
 end
