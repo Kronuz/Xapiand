8cc67578b2febc6b234cae1bf62a7e97c04b4016
German M. Bravo <german.mb@dubalu.com>
2018-03-23 08:30:08 -0600

    Homebrew formula updated

--- contrib/homebrew/xapiand.rb
+++ contrib/homebrew/xapiand.rb
@@ -1,12 +1,12 @@
 # Homebrew Formula for Xapiand
 # Usage: brew install --HEAD https://github.com/Kronuz/Xapiand/raw/master/contrib/homebrew/xapiand.rb
 
-require 'formula'
-
 class Xapiand < Formula
   desc "Xapiand: A RESTful Search Engine"
-  homepage "https://github.com/Kronuz/Xapiand"
-  head "git://github.com/Kronuz/Xapiand.git", :using => :git
+  homepage "http://kronuz.io/Xapiand"
+  url "https://github.com/Kronuz/Xapiand/archive/v1.0.0.tar.gz"
+  sha256 "c2aba1b5a0c677193bb4fcb2013b6f87f2fe43d0117f7cfcc0e885bf109a7676"
+  head "https://github.com/Kronuz/Xapiand.git"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
