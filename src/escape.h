a09bf1a81031c3bf97043be0e8ef8e29e1f6ab86
German M. Bravo <german.mb@gmail.com>
2018-04-09 09:18:54 -0500

    Utils: escape split out

---
+++ src/escape.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2015-2018 Dubalu LLC. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "xapiand.h"
+
+#include <cstddef>            // for std::size_t
+#include <string>             // for std::string
+#include "string_view.hh"     // for std::string_view
+
+
+std::string repr(const void* p, std::size_t size, bool friendly = true, char quote = '\'', std::size_t max_size = 0);
+
+inline std::string repr(const void* p, const void* e, bool friendly = true, char quote = '\'', std::size_t max_size = 0) {
+	return repr(p, static_cast<const char*>(e) - static_cast<const char*>(p), friendly, quote, max_size);
+}
+
+inline std::string repr(std::string_view string, bool friendly = true, char quote = '\'', std::size_t max_size = 0) {
+	return repr(string.data(), string.size(), friendly, quote, max_size);
+}
+
+template<typename T, std::size_t N>
+inline std::string repr(T (&s)[N], bool friendly = true, char quote = '\'', std::size_t max_size = 0) {
+	return repr(s, N - 1, friendly, quote, max_size);
+}
+
+
+std::string escape(const void* p, std::size_t size, char quote = '\'');
+
+inline std::string escape(std::string_view string, char quote = '\'') {
+	return escape(string.data(), string.size(), quote);
+}
+
+template<typename T, std::size_t N>
+inline std::string escape(T (&s)[N], char quote = '\'') {
+	return escape(s, N - 1, quote);
+}
