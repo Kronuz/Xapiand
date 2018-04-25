3cb86ad6b577053abb674e1cd45a8835a1c89d0b
German M. Bravo <german.mb@gmail.com>
2018-04-25 16:03:50 -0500

    Utils: Added reversed() for reversing iterables
    Ex.:
      for (auto& item : reversed(items)) {...}

--- src/utils.h
+++ src/utils.h
@@ -42,6 +42,11 @@
 #include "escape.h"           // for repr() and escape()
 #include "string.hh"
 
+// Reversed iterable
+template <typename T> struct reversion_wrapper { T& iterable; };
+template <typename T> auto begin (reversion_wrapper<T> w) { return std::rbegin(w.iterable); }
+template <typename T> auto end (reversion_wrapper<T> w) { return std::rend(w.iterable); }
+template <typename T> reversion_wrapper<T> reversed(T&& iterable) { return { iterable }; }
 
 struct File_ptr {
 	struct dirent *ent;
