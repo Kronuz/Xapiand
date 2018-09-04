20e5383563bdccb1d808dee7deea218c7874a439
German M. Bravo <german.mb@gmail.com>
2018-09-04 13:04:25 -0500

    Docker: Fix xapiand entrypoint

--- contrib/docker/xapiand/entrypoint.sh
+++ contrib/docker/xapiand/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-XAPIAND_USER=${XAPIAND_USER:-xapiand}
-XAPIAND_DATABASE=${XAPIAND_DATABASE:-/var/run/xapiand}
+XAPIAND_USER=${XAPIAND_USER:-$(id -u -n)}
+XAPIAND_DATABASE=${XAPIAND_DATABASE:-/var/db/xapiand}
 
 # if the first argument starts with '-', prepend xapiand
 if [ "${1:0:1}" = '-' ]; then
@@ -11,14 +11,18 @@ if [ "${1:0:1}" = '-' ]; then
 fi
 
 # allow the container to be started with `--user`
-if [ "$1" = 'xapiand' ] && [ "$XAPIAND_USER" != '0' ]; then
-	XAPIAND_USER="$(id -u)"
+if [ "$1" = 'xapiand' ]; then
+	if [ "$XAPIAND_USER" == 'root' ]; then
+		XAPIAND_USER="xapiand"
+	fi
+
+	if [ ! -d "$XAPIAND_DATABASE" ]; then
+		mkdir -p "$XAPIAND_DATABASE"
+		chown -R "$XAPIAND_USER" "$XAPIAND_DATABASE" 2>/dev/null || :
+		chmod 700 "$XAPIAND_DATABASE" 2>/dev/null || :
+	fi
+
+	set -- "$@" --uid="$XAPIAND_USER" --database="$XAPIAND_DATABASE"
 fi
 
-if [ ! -d "$XAPIAND_DATABASE" ]; then
-	mkdir -p "$XAPIAND_DATABASE"
-	chown -R "$XAPIAND_USER" "$XAPIAND_DATABASE" 2>/dev/null || :
-	chmod 700 "$XAPIAND_DATABASE" 2>/dev/null || :
-fi
-
-exec "$@" --uid="$XAPIAND_USER" --database="$XAPIAND_DATABASE"
+exec "$@"
