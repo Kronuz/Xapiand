45214b7fa4989828a3a12decc17e2353b4372c81
German M. Bravo <german.mb@gmail.com>
2018-09-10 10:39:09 -0500

    Entrypoint updated to accept --user

--- contrib/docker/xapiand/entrypoint.sh
+++ contrib/docker/xapiand/entrypoint.sh
@@ -2,27 +2,37 @@
 
 set -e
 
-XAPIAND_USER=${XAPIAND_USER:-$(id -u -n)}
-XAPIAND_DATABASE=${XAPIAND_DATABASE:-/var/db/xapiand}
+# Setup user/group (allow the container to be started with `--user`):
+UID="${UID:-$(id -u -n)}"
+if [ "$UID" == 'root' ]; then
+	UID='xapiand'
+fi
+
+GID="${GID:-$(id -g -n)}"
+if [ "$GID" == 'root' ]; then
+	GID='xapiand'
+fi
 
 # if the first argument starts with '-', prepend xapiand
 if [ "${1:0:1}" = '-' ]; then
 	set -- xapiand "$@"
 fi
 
-# allow the container to be started with `--user`
+# Add defaults to command
 if [ "$1" = 'xapiand' ]; then
-	if [ "$XAPIAND_USER" == 'root' ]; then
-		XAPIAND_USER="xapiand"
-	fi
+	XAPIAND_DATABASE="${XAPIAND_DATABASE:-/var/db/xapiand}"
 
 	if [ ! -d "$XAPIAND_DATABASE" ]; then
 		mkdir -p "$XAPIAND_DATABASE"
-		chown -R "$XAPIAND_USER" "$XAPIAND_DATABASE" 2>/dev/null || :
+		chown -R "$UID":$GID "$XAPIAND_DATABASE" 2>/dev/null || :
 		chmod 700 "$XAPIAND_DATABASE" 2>/dev/null || :
 	fi
 
-	set -- "$@" --uid="$XAPIAND_USER" --database="$XAPIAND_DATABASE"
+	set -- "$@" \
+		--uid="$UID" \
+		--gid="$GID" \
+		--database="$XAPIAND_DATABASE"
 fi
 
+echo "$@"
 exec "$@"
