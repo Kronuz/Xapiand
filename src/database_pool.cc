8bfb4115f08a565c037056e601abc615fff02ed2
German M. Bravo <german.mb@gmail.com>
2018-12-06 00:18:29 -0600

    DatabasePool: Not so immediate cleanup()

--- src/database_pool.cc
+++ src/database_pool.cc
@@ -774,7 +774,7 @@ DatabasePool::cleanup(bool immediate)
 			return lru::DropAction::leave;
 		}
 		if (size > max_size) {
-			if (immediate || database_endpoint->renew_time < now - 60s) {
+			if (database_endpoint->renew_time < now - (immediate ? 10s : 60s)) {
 				lk.unlock();
 				database_endpoint->clear();
 				lk.lock();
@@ -789,7 +789,7 @@ DatabasePool::cleanup(bool immediate)
 			L_DATABASE("Leave recently used endpoint: %s", repr(database_endpoint->to_string()));
 			return lru::DropAction::leave;
 		}
-		if (immediate || database_endpoint->renew_time < now - 3600s) {
+		if (database_endpoint->renew_time < now - (immediate ? 10s : 3600s)) {
 			lk.unlock();
 			database_endpoint->clear();
 			lk.lock();
