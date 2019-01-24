963dde944f0d1a2f09c04b7407a438b34eb71519
German M. Bravo <german.mb@gmail.com>
2019-01-24 13:11:43 -0600

    Docs: Assets: Updated accounts_update script to generate ndjson file

--- docs/assets/accounts_update.py
+++ docs/assets/accounts_update.py
@@ -4,14 +4,18 @@ import json
 import msgpack
 
 if __name__ == '__main__':
-    accounts = json.load(open('accounts.json'))
-    msgpack.dump(accounts, open('accounts.msgpack', 'w'))
-    with open('accounts.http', 'w') as fp:
-        for i, a in enumerate(accounts, 1):
-            aj = json.dumps(a)
-            print("""PUT /bank/{} HTTP/1.1
+    accounts_json = open('accounts.json')
+    accounts_msgpack = open('accounts.msgpack', 'w')
+    accounts_ndjson = open('accounts.ndjson', 'w')
+    accounts_http = open('accounts.http', 'w')
+    accounts = json.load(accounts_json)
+    msgpack.dump(accounts, accounts_msgpack)
+    for i, a in enumerate(accounts, 1):
+        aj = json.dumps(a)
+        print(aj, file=accounts_ndjson)
+        print("""PUT /bank/{} HTTP/1.1
 Host: localhost:8880
 Content-Type: application/json
 Content-Length: {}
 
-{}""".format(i, len(aj), aj), file=fp)
+{}""".format(i, len(aj), aj), file=accounts_http)
