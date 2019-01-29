b30c6b1d52d159479fb3d99c1d7de49e3d80dda8
Jose Madrigal Cardenas <mac.yosef@gmail.com>
2019-01-29 16:14:53 -0600

    QueryDsl treat string like text type, comment the stopper and added partial flag when wildcard is used

--- src/query_dsl.cc
+++ src/query_dsl.cc
@@ -826,6 +826,7 @@ QueryDSL::get_term_query(const required_spc_t& field_spc, std::string_view seria
 	L_CALL("QueryDSL::get_term_query(<field_spc>, %s, <wqf>, <q_flags>, %s)", repr(serialised_term), is_wildcard ? "true" : "false");
 
 	switch (field_spc.get_type()) {
+		case FieldType::STRING:
 		case FieldType::TEXT: {
 			Xapian::QueryParser parser;
 			if (field_spc.flags.bool_term) {
@@ -833,22 +834,28 @@ QueryDSL::get_term_query(const required_spc_t& field_spc, std::string_view seria
 			} else {
 				parser.add_prefix("_", field_spc.prefix() + field_spc.get_ctype());
 			}
-			const auto& stopper = getStopper(field_spc.language);
-			parser.set_stopper(stopper.get());
+			if (is_wildcard) {
+				q_flags |= Xapian::QueryParser::FLAG_PARTIAL;
+			}
+			// const auto& stopper = getStopper(field_spc.language);
+			// parser.set_stopper(stopper.get());
 			parser.set_stemming_strategy(getQueryParserStemStrategy(field_spc.stem_strategy));
 			parser.set_stemmer(Xapian::Stem(field_spc.stem_language));
 			return parser.parse_query("_:" + std::string(serialised_term), q_flags);
 		}
 
-		case FieldType::STRING: {
-			Xapian::QueryParser parser;
-			if (field_spc.flags.bool_term) {
-				parser.add_boolean_prefix("_", field_spc.prefix() + field_spc.get_ctype());
-			} else {
-				parser.add_prefix("_", field_spc.prefix() + field_spc.get_ctype());
-			}
-			return parser.parse_query("_:" + std::string(serialised_term), q_flags);
-		}
+		// case FieldType::STRING: {
+		// 	Xapian::QueryParser parser;
+		// 	if (field_spc.flags.bool_term) {
+		// 		parser.add_boolean_prefix("_", field_spc.prefix() + field_spc.get_ctype());
+		// 	} else {
+		// 		parser.add_prefix("_", field_spc.prefix() + field_spc.get_ctype());
+		// 	}
+		// 	if (is_wildcard) {
+		// 		q_flags |= Xapian::QueryParser::FLAG_PARTIAL;
+		// 	}
+		// 	return parser.parse_query("_:" + std::string(serialised_term), q_flags);
+		// }
 
 		case FieldType::KEYWORD: {
 			std::string lower;
