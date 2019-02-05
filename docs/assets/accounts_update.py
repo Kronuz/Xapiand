f9cda42cece08b48f280ef94d1c6f2cd92f6d420
German M. Bravo <german.mb@gmail.com>
2019-02-05 11:28:36 -0600

    Docs: Assets: Updated accounts_update script to generate msgpack without list

--- docs/assets/accounts_update.py
+++ docs/assets/accounts_update.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 
 import json
@@ -5,12 +7,12 @@ import msgpack
 
 if __name__ == '__main__':
     accounts_json = open('accounts.json')
-    accounts_msgpack = open('accounts.msgpack', 'w')
+    accounts_msgpack = open('accounts.msgpack', 'wb')
     accounts_ndjson = open('accounts.ndjson', 'w')
     accounts_http = open('accounts.http', 'w')
     accounts = json.load(accounts_json)
-    msgpack.dump(accounts, accounts_msgpack)
     for i, a in enumerate(accounts, 1):
+        accounts_msgpack.write(msgpack.dumps(a))
         aj = json.dumps(a)
         print(aj, file=accounts_ndjson)
         print("""PUT /bank/{} HTTP/1.1
