59ab1c9ae05fb6384d3c7f04da3f752071147ee8
German M. Bravo <german.mb@gmail.com>
2019-02-06 15:10:24 -0600

    QueryDSL: There cannot be bool_term for TEXT or STRING (fix prefix)

--- src/query_dsl.cc
+++ src/query_dsl.cc
@@ -829,20 +829,16 @@ QueryDSL::get_term_query(const required_spc_t& field_spc, std::string_view seria
 	switch (field_spc.get_type()) {
 		case FieldType::STRING:
 		case FieldType::TEXT: {
-			Xapian::QueryParser parser;
-			if (field_spc.flags.bool_term) {
-				parser.add_boolean_prefix("_", field_spc.prefix() + field_spc.get_ctype());
-			} else {
-				parser.add_prefix("_", field_spc.prefix() + field_spc.get_ctype());
-			}
+			// There cannot be non-keyword fields with bool_term
 			if (is_wildcard) {
 				q_flags |= Xapian::QueryParser::FLAG_PARTIAL;
 			}
+			Xapian::QueryParser parser;
 			const auto& stopper = getStopper(field_spc.language);
 			parser.set_stopper(stopper.get());
 			parser.set_stemming_strategy(getQueryParserStemStrategy(field_spc.stem_strategy));
 			parser.set_stemmer(Xapian::Stem(field_spc.stem_language));
-			return parser.parse_query("_:" + std::string(serialised_term), q_flags);
+			return parser.parse_query(std::string(serialised_term), q_flags, field_spc.prefix() + field_spc.get_ctype());
 		}
 
 		case FieldType::KEYWORD: {
