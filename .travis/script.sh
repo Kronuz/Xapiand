61a3974ab87587e68e28a7347d916a175a7806a9
German M. Bravo <german.mb@gmail.com>
2019-03-27 14:00:49 -0600

    TravisCI: Pull again after build

--- .travis/script.sh
+++ .travis/script.sh
@@ -46,31 +46,33 @@ if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
 	sed -i '' "s#^  url .*#  url \"${PACKAGE_URL}\"#" Formula/xapiand.rb
 	sed -i '' "s#^  sha256 .*#  sha256 \"${PACKAGE_SHA256}\"#" Formula/xapiand.rb
 
-	# Build bottle
+	# Build the Bottle
 	HOMEBREW_PACKAGE_HASH=$PACKAGE_HASH brew install --build-bottle --verbose Kronuz/tap/xapiand
-	PACKAGE_SHA256=$(brew bottle --no-rebuild Kronuz/tap/xapiand | grep sha256)
-	PACKAGE_NL_SHA256=$(echo '\'; echo "$PACKAGE_SHA256")
-	PACKAGE_TYPE=$(echo $PACKAGE_SHA256 | awk '{ print $4 }')
+	PACKAGE_BOTTLE_SHA256=$(brew bottle --no-rebuild Kronuz/tap/xapiand | grep sha256)
+	PACKAGE_BOTTLE_SHA256_NL=$(echo '\'; echo "$PACKAGE_BOTTLE_SHA256")
+	PACKAGE_TYPE=$(echo $PACKAGE_BOTTLE_SHA256 | awk '{ print $4 }')
 	PACKAGE_TYPE_EXT=$(echo $PACKAGE_TYPE | tr ':' '.')
 	PACKAGE_BOTTLE="xapiand-${PACKAGE_VERSION}${PACKAGE_TYPE_EXT}.bottle.tar.gz"
 
-	# Remove double slash from bottle filename
-	mv "xapiand--${PACKAGE_VERSION}${PACKAGE_TYPE_EXT}.bottle.tar.gz" "${PACKAGE_BOTTLE}"
-
-	# Update Formula with new bottle
+	# The building could have taken several minutes, so we reset the modified
+	# file, pull changes and modify again adding the new Bottle
+	git checkout Formula/xapiand.rb
+	git pull
+	sed -i '' "s#^  url .*#  url \"${PACKAGE_URL}\"#" Formula/xapiand.rb
+	sed -i '' "s#^  sha256 .*#  sha256 \"${PACKAGE_SHA256}\"#" Formula/xapiand.rb
 	grep -v $PACKAGE_TYPE Formula/xapiand.rb > Formula/xapiand.rb.tmp
-	sed "s#^    cellar :any#    cellar :any${PACKAGE_NL_SHA256}#" Formula/xapiand.rb.tmp > Formula/xapiand.rb
+	sed "s#^    cellar :any#    cellar :any${PACKAGE_BOTTLE_SHA256_NL}#" Formula/xapiand.rb.tmp > Formula/xapiand.rb
 	rm -f Formula/xapiand.rb.tmp
 
-	# Commit and push formula
-	git checkout mater
+	# Commit and push the Formula
 	git add -u
 	git commit --message "Xapiand updated to v${PACKAGE_VERSION} via Travis build: ${TRAVIS_BUILD_NUMBER}"
 	git push --quiet master
 
-	# Add, commit and push bottle
+	# Add, commit and push Bottle
 	git fetch --depth 1 origin gh-pages:gh-pages
 	git checkout gh-pages
+	mv "xapiand--${PACKAGE_VERSION}${PACKAGE_TYPE_EXT}.bottle.tar.gz" "${PACKAGE_BOTTLE}"
 	git add "${PACKAGE_BOTTLE}"
 	git commit --message "${PACKAGE_BOTTLE}"
 	git push --quiet gh-pages
