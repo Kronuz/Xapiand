4bea0f55beb9090dc33dcbebd70dcf370b11e07e
German M. Bravo <german.mb@gmail.com>
2019-04-01 10:40:07 -0600

    Replication Protocol: Destroy and detach on errors

--- src/server/replication_protocol_client.cc
+++ src/server/replication_protocol_client.cc
@@ -437,15 +437,24 @@ ReplicationProtocolClient::replication_client(ReplicationReplyType type, const s
 			}
 		}
 	} catch (const BaseException& exc) {
-		L_EXC("ERROR: Replicating database");
+		ASSERT(lk_shard_ptr);
+		L_EXC("ERROR: Replicating database: {}", (*lk_shard_ptr)->endpoint.path);
 	} catch (const Xapian::DatabaseNotFoundError& exc) {
+		ASSERT(lk_shard_ptr);
+		L_WARNING("WARNING: Replicating non-existent database: {}", (*lk_shard_ptr)->endpoint.path);
 	} catch (const Xapian::Error& exc) {
-		L_EXC("ERROR: Replicating database");
+		ASSERT(lk_shard_ptr);
+		L_EXC("ERROR: Replicating database: {}", (*lk_shard_ptr)->endpoint.path);
 	} catch (const std::exception& exc) {
-		L_EXC("ERROR: Replicating database");
+		ASSERT(lk_shard_ptr);
+		L_EXC("ERROR: Replicating database: {}", (*lk_shard_ptr)->endpoint.path);
 	} catch (...) {
-		L_EXC("ERROR: Replicating database");
+		ASSERT(lk_shard_ptr);
+		L_EXC("ERROR: Replicating database: {}", (*lk_shard_ptr)->endpoint.path);
 	}
+
+	destroy();
+	detach();
 }
 
 
