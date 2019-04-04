df44497704a9004396ed8b3b6e89904f7925b929
German M. Bravo <german.mb@gmail.com>
2019-04-04 10:56:21 -0600

    RapidJSON: Lowercase hex

--- src/rapidjson/writer.h
+++ src/rapidjson/writer.h
@@ -374,7 +374,7 @@ protected:
     }
 
     bool WriteString(const Ch* str, SizeType length)  {
-        static const typename OutputStream::Ch hexDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        static const typename OutputStream::Ch hexDigits[16] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
         static const char escape[256] = {
 #define Z16 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
             //0    1    2    3    4    5    6    7    8    9    A    B    C    D    E    F
